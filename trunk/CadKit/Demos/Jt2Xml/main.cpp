51b4fc20f67be50774252ed58ef71c6ed7901053
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2002-08-23 19:57:25 +0000

    Adding the Jt2Xml demo program.

---
+++ trunk/CadKit/Demos/Jt2Xml/main.cpp
@@ -0,0 +1,98 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  BSD License
+//  http://www.opensource.org/licenses/bsd-license.html
+//
+//  Copyright (c) 2002, Perry L. Miller IV
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without 
+//  modification, are permitted provided that the following conditions are met:
+//
+//  - Redistributions of source code must retain the above copyright notice, 
+//    this list of conditions and the following disclaimer. 
+//
+//  - Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution. 
+//
+//  - Neither the name of the CAD Toolkit nor the names of its contributors may
+//    be used to endorse or promote products derived from this software without
+//    specific prior written permission. 
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+//  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  A Jupiter to XML translator.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifdef _WIN32
+# pragma warning(disable:4251) // See note 3ce3a140-2edf-11d4-98a4-0040054c86c7.
+# pragma warning(disable:4786) // Truncated debug names.
+#endif
+
+#include "Translators/Jupiter2Xml/TrJt2XmlClientHeaders.h"
+#include <iostream>
+
+using namespace CadKit;
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Main.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+int main ( int argc, char **argv )
+{
+  // There should be at least one argument.
+  if ( argc < 2 )
+  {
+    std::cout << "Usage: " << SlAPathname ( argv[0] ).getFilename().c_str() << "<filename1.jt> [filename2.jt] ..." << std::endl;
+    return 0;
+  }
+
+  // Declare an instance of the Jupiter to XML translator.
+  TrJt2Xml jt2xml;
+
+  // Initialize.
+  if ( false == jt2xml.init() )
+  {
+    std::cout << "Failed to initialize translator" << std::endl;
+    return 0;
+  }
+
+  // Loop through all the input files.
+  for ( int i = 1; i < argc; ++i )
+  {
+    std::cout << "Translating: " << argv[i] << std::endl;
+
+    // Read the jupiter file.
+    if ( false == jt2xml.translate ( argv[i] ) )
+    {
+      std::cout << jt2xml.getLastError() << std::endl;
+      std::cout << "Failed to translate: " << argv[i] << std::endl;
+      return 0;
+    }
+
+    std::cout << "Done with:   " << argv[i] << std::endl;
+  }
+
+  // It worked.
+  return 1;
+}

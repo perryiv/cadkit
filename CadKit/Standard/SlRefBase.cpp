62372d5b159f6ed867877b5d4839f200c1948d07
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2002-09-16 20:04:31 +0000

    Fixed small compile error in Release build with SL_PRINT.

--- CadKit/Standard/SlRefBase.cpp
+++ CadKit/Standard/SlRefBase.cpp
@@ -80,7 +80,7 @@ SlRefBase::SlRefBase ( const unsigned long &refCount ) : SlBase(), _refCount ( r
 
 SlRefBase::~SlRefBase()
 {
-  SL_PRINT3 ( "SlRefBase::~SlRefBase(), this = %X, _refCount = %d, class name = %s\n", this, _refCount, this->getClassName() );
+  SL_PRINT4 ( "SlRefBase::~SlRefBase(), this = %X, _refCount = %d, class name = %s\n", this, _refCount, this->getClassName() );
 }
 
 
@@ -92,7 +92,7 @@ SlRefBase::~SlRefBase()
 
 void SlRefBase::_incrementReferenceCount()
 {
-  SL_PRINT3 ( "SlRefBase::_incrementReferenceCount(), this = %X, _refCount = %d, class name = %s\n", this, _refCount, this->getClassName() );
+  SL_PRINT4 ( "SlRefBase::_incrementReferenceCount(), this = %X, _refCount = %d, class name = %s\n", this, _refCount, this->getClassName() );
   SL_ASSERT ( this );
 
   // Increment the reference count.
@@ -108,7 +108,7 @@ void SlRefBase::_incrementReferenceCount()
 
 void SlRefBase::_decrementReferenceCount()
 {
-  SL_PRINT3 ( "SlRefBase::_decrementReferenceCount(), this = %X, _refCount = %d, class name = %s\n", this, _refCount, this->getClassName() );
+  SL_PRINT4 ( "SlRefBase::_decrementReferenceCount(), this = %X, _refCount = %d, class name = %s\n", this, _refCount, this->getClassName() );
   SL_ASSERT ( this );
 
   // Decrement the reference count.

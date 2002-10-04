56d6b608cb40baf880e0cc87551eebe87f2636bf
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2002-10-04 22:42:51 +0000

    Moving away from typecasting when converting to/from DMDTk enum values. Instead using a switch/case approach.

--- trunk/CadKit/Standard/SlMessageIds.h
+++ trunk/CadKit/Standard/SlMessageIds.h
@@ -52,6 +52,7 @@ const unsigned long OK                                = 1033011600;
 const unsigned long FAILED                            = 1033010419;
 const unsigned long NO_INTERFACE                      = 1032730909;
 const unsigned long UNKNOWN_ENTITY                    = 1033242734;
+const unsigned long UNSUPPORTED                       = 1033749917;
 };
 
 

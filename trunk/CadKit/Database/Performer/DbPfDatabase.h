d1854717e2a73b5f3f5d25ee7f84e84feb8ae012
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2002-10-15 22:48:54 +0000

    Forgot to add Database/Performer with last commit.

---
+++ trunk/CadKit/Database/Performer/DbPfDatabase.h
@@ -0,0 +1,179 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2002, Perry L. Miller IV
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  DbPfDatabase: Performer database wrapper class.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _CADKIT_DATABASE_PERFORMER_LIBRARY_DATABASE_H_
+#define _CADKIT_DATABASE_PERFORMER_LIBRARY_DATABASE_H_
+
+#include "DbPfApi.h"
+
+#include "Database/Base/DbBaseTarget.h"
+
+#include "Interfaces/IEntityNotify.h"
+#include "Interfaces/IInstanceQuery.h"
+
+#include "Standard/SlStack.h"
+
+#ifndef _CADKIT_USE_PRECOMPILED_HEADERS
+# include <string>
+# include <map>
+#endif
+
+class pfGroup;
+class pfLOD;
+class pfGeoSet;
+class pfGeoState;
+
+
+namespace CadKit
+{
+class DB_PERFORMER_API DbPfDatabase : public DbBaseTarget,
+                                      public IAssemblyNotify,
+                                      public IPartNotify,
+                                      public IInstanceNotify,
+                                      public ILodNotify,
+                                      public IShapeNotify,
+                                      public ISetNotify
+{
+public:
+
+  DbPfDatabase();
+
+  // IUnknown interface.
+  CADKIT_DECLARE_IUNKNOWN_MEMBERS;
+
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  IDataTarget interface.
+  //
+  /////////////////////////////////////////////////////////////////////////////
+
+  // Tell the target it is about to receive data.
+  virtual bool            dataTransferStart ( IUnknown *caller );
+
+  // Tell the target it is done receiving data.
+  virtual bool            dataTransferEnd ( IUnknown *caller );
+
+  // Get the file extension.
+  virtual std::string     getFileExtension() const;
+
+  // Store the data.
+  virtual bool            storeData ( const std::string &filename );
+
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  IAssemblyNotify interface.
+  //
+  /////////////////////////////////////////////////////////////////////////////
+
+  // End the assembly.
+  virtual bool            endEntity ( AssemblyHandle assembly, IUnknown *caller );
+
+  // Start the assembly.
+  virtual bool            startEntity ( AssemblyHandle assembly, IUnknown *caller );
+
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  IPartNotify interface.
+  //
+  /////////////////////////////////////////////////////////////////////////////
+
+  // End the part.
+  virtual bool            endEntity ( PartHandle assembly, IUnknown *caller );
+
+  // Start the part.
+  virtual bool            startEntity ( PartHandle assembly, IUnknown *caller );
+
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  IInstanceNotify interface.
+  //
+  /////////////////////////////////////////////////////////////////////////////
+
+  // End the instance.
+  virtual bool            endEntity ( InstanceHandle assembly, IUnknown *caller );
+
+  // Start the instance.
+  virtual bool            startEntity ( InstanceHandle assembly, IUnknown *caller );
+
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  ILodNotify interface.
+  //
+  /////////////////////////////////////////////////////////////////////////////
+
+  // End the lod.
+  virtual bool            endEntity ( LodHandle lod, IUnknown *caller );
+
+  // Start the lod.
+  virtual bool            startEntity ( LodHandle lod, IUnknown *caller );
+
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  IShapeNotify interface.
+  //
+  /////////////////////////////////////////////////////////////////////////////
+
+  // End the shape.
+  virtual bool            endEntity ( ShapeHandle shape, IUnknown *caller );
+
+  // Start the shape.
+  virtual bool            startEntity ( ShapeHandle shape, IUnknown *caller );
+
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  ISetNotify interface.
+  //
+  /////////////////////////////////////////////////////////////////////////////
+
+  // End the set.
+  virtual bool            endEntity ( SetHandle set, IUnknown *caller );
+
+  // Start the set.
+  virtual bool            startEntity ( SetHandle set, IUnknown *caller );
+
+protected:
+
+  typedef SlStack<pfGroup *> GroupStack;
+  std::auto_ptr<GroupStack> _groupStack;
+
+  virtual ~DbPfDatabase();
+
+  bool                    _addAttributes ( IUnknown *caller, ShapeHandle shape, pfGeoState *state );
+  bool                    _addDataSets   ( IUnknown *caller, ShapeHandle shape, pfGeoSet *geometry );
+  bool                    _addVertices   ( IUnknown *caller, ShapeHandle shape, pfGeoSet *geometry );
+  bool                    _addNormals    ( IUnknown *caller, ShapeHandle shape, pfGeoSet *geometry );
+  bool                    _addColors     ( IUnknown *caller, ShapeHandle shape, pfGeoSet *geometry );
+  bool                    _addTexCoords  ( IUnknown *caller, ShapeHandle shape, pfGeoSet *geometry );
+
+  void                    _clearGroupStack();
+
+  pfGroup *               _findGroup ( InstanceHandle instance, IInstanceQueryFloat *query ) const;
+
+  pfGroup *               _getRoot() const;
+
+  bool                    _hasColorAttribute ( IUnknown *caller, ShapeHandle shape ) const;
+
+  void                    _pushGroup ( pfGroup *group );
+  void                    _popGroup();
+
+  void                    _setLodParameters ( pfLOD *lod ) const;
+
+  SL_DECLARE_REFERENCE_POINTER ( DbPfDatabase );
+  SL_DECLARE_DYNAMIC_CLASS ( DbPfDatabase, 1032736074 );
+};
+
+}; // namespace CadKit
+
+#endif // _CADKIT_DATABASE_PERFORMER_LIBRARY_DATABASE_H_
---
+++ trunk/CadKit/Database/Performer/DbPfInline.h
@@ -0,0 +1,79 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2002, Perry L. Miller IV
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  DbPfInline: Inline functions.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _CADKIT_DATABASE_PERFORMER_LIBRARY_INLINE_FUNCTIONS_DATABASE_H_
+#define _CADKIT_DATABASE_PERFORMER_LIBRARY_INLINE_FUNCTIONS_DATABASE_H_
+
+#include "Standard/SlMatrix4.h"
+#include "Standard/SlRefPtr.h"
+
+#include "Interfaces/IEntityQuery.h"
+
+#ifndef _CADKIT_USE_PRECOMPILED_HEADERS
+# include "Performer/pr/pfDCS.h"
+#endif
+
+
+namespace CadKit
+{
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Create the group. VC++ has an internal compiler error if the GroupType
+//  is not a template argument, but just an pfGroup.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+template <class HandleType, class InterfaceType, class GroupType> 
+inline SlRefPtr<pfGroup> createGroup ( HandleType entity, InterfaceType *query, GroupType *cloneMe )
+{
+  SL_ASSERT ( entity );
+  SL_ASSERT ( query );
+
+  // Create a group. Make a clone If we are supposed to.
+  SlRefPtr<pfDCS> group;
+  if ( cloneMe )
+  {
+    // Note: docs say argument has to be zero. Also, unlike OSG, this will 
+    // copy down to (but not including) the leaf nodes.
+    group = dynamic_cast<pfDCS *> ( cloneMe->clone ( 0 ) );
+  }
+  else
+    group = new pfDCS;
+  if ( group.isNull() )
+    return NULL;
+
+  // Set the name.
+  group->setName ( query->getName ( entity ).c_str() );
+
+  // Set the matrix if there is one.
+  SlMatrix4f T;
+  if ( true == query->getTransform ( entity, T, false ) )
+  {
+    pfMatrix M ( T(0,0), T(1,0), T(2,0), T(3,0),
+                 T(0,1), T(1,1), T(2,1), T(3,1),
+                 T(0,2), T(1,2), T(2,2), T(3,2),
+                 T(0,3), T(1,3), T(2,3), T(3,3) );
+    group->setMat ( M );
+  }
+
+  // Return the new group.
+  return SlRefPtr<pfGroup> ( group );
+}
+
+
+}; // namespace CadKit
+
+
+#endif // _CADKIT_DATABASE_PERFORMER_LIBRARY_INLINE_FUNCTIONS_DATABASE_H_

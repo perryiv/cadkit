460f4f6490487162bf38a6f487889598de64d249
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2002-11-26 05:29:08 +0000

    Changed name of 4x4 matrix example. Added an NxN matrix example.

---
+++ CadKit/Examples/MatrixNN/MatrixNN.cpp
@@ -0,0 +1,23 @@
+#include "Standard/SlMatrixNN.h"
+#include "Standard/SlMatrixNNIO.h"
+#include "Standard/SlConstants.h"
+#include <iostream>
+
+using namespace CadKit;
+
+
+void testMatrices()
+{
+
+}
+
+
+int main ( int argc, char **argv )
+{
+  testMatrices();
+  
+  // Wait for user to press the 'any' key.
+  std::cin.get();
+
+  return 1;
+}

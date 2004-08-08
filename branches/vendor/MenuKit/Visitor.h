266075271608e2beccb58879b59ca9f0b68e9dcc
nobody <nobody@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2004-08-08 04:46:23 +0000

    This commit was manufactured by cvs2svn to create branch 'vendor'.

---
+++ branches/vendor/MenuKit/Visitor.h
@@ -0,0 +1,70 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2002, John K. Grant.
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Base visitor class.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _MENUKIT_ITEM_VISITOR_
+#define _MENUKIT_ITEM_VISITOR_
+
+#include "MenuKit/Referenced.h"
+
+namespace MenuKit {
+
+class Menu;
+class Button;
+
+class MENUKIT_EXPORT Visitor : public Referenced
+{
+public:
+
+  // Typedefs and smart-pointers.
+  typedef Referenced BaseClass;
+  MENUKIT_DECLARE_POINTER ( Visitor );
+
+  // Possible modes.
+  enum Mode { NONE, ALL };
+
+  // Apply this visitor to the items.
+  virtual void        apply ( Menu &m );
+  virtual void        apply ( Button &b );
+
+  // Set/get the mode.
+  void                mode ( Mode m ) { _mode = m; }
+  Mode                mode() const { return _mode; }
+
+  // Traverse the tree.
+  void                traverse ( Menu &m );
+
+protected:
+
+  // Construction.
+  Visitor();
+  Visitor ( Mode m );
+
+  // Use reference counting.
+  ~Visitor();
+
+private:
+
+  // No copying.
+  Visitor ( const Visitor & );
+  Visitor &operator = ( const Visitor & );
+
+  Mode _mode;
+};
+
+
+}; // namespace MenuKit
+
+
+#endif // _MENUKIT_ITEM_VISITOR_

b41fa7d3cf2e0aafd112ce7c0734b99e6986f646
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2004-08-08 04:05:03 +0000

    Initial revision

---
+++ Collision/PQP/CompileGuard.h
@@ -0,0 +1,24 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2002, Perry L. Miller IV and Denis V. Dorozkain
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  This header file should not be included in client code.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _COLLISION_PQP_COMPILE_GUARD_H_
+#define _COLLISION_PQP_COMPILE_GUARD_H_
+
+#ifndef _COMPILING_PQP_COLLISION
+# error "This header file is only used for compiling the library."
+# error "It should not be included in client code."
+#endif
+
+#endif // _COLLISION_PQP_COMPILE_GUARD_H_

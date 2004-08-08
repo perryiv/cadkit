9bc397132b56e2ac6963eb4fadc3bd87a2035308
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2004-08-08 04:46:14 +0000

    Initial revision

---
+++ trunk/MenuKit/OSG/GFX.h
@@ -0,0 +1,60 @@
+#ifndef _menukit_osg_gfx_functor_
+#define _menukit_osg_gfx_functor_
+
+#include "MenuKit/Referenced.h"
+
+#include "MenuKit/OSG/Export.h"
+#include "MenuKit/OSG/Skin.h"
+
+#include "osg/Vec4"
+#include "osgText/Font"
+
+namespace osg
+{
+  class Node;
+};
+
+namespace MenuKit
+{
+  class Item;
+  class Menu;
+
+  namespace OSG
+  {
+
+    // abstract base class for all skins
+    class MENUKIT_OSG_EXPORT GFX : public Referenced
+    {
+    public:
+      typedef Referenced BaseClass;
+      GFX ( Skin *skin, Item *active );
+      GFX ( const GFX & );
+      GFX& operator = (const GFX&);
+
+      MENUKIT_DECLARE_POINTER(GFX);
+
+      void skin(Skin* s) { _skin = s; }
+      const Skin* skin() { return _skin.get(); }
+
+      virtual osg::Node* operator() ( const Item* item );
+
+    protected:
+      osg::Vec3 calculate_move(const Menu*,const Item*,const Item*,
+                               unsigned int,float,float,float&,float&);
+      virtual osg::Node* traverse(const MenuKit::Menu*,float);
+      GFX();
+      virtual ~GFX() {}
+
+      void _setSkinMode ( const Item *item );
+      bool _isInActivePath ( const Item *item );
+
+    private:
+      Skin::Ptr _skin;
+      Item::Ptr _active;
+    };
+
+  };
+
+};
+
+#endif

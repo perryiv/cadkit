423cb97cf17b31a5312b3bbe5d7807de7d9ee96a
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2004-08-16 22:52:05 +0000

    Initial revision

---
+++ CadViewer/Plugins/scene_operations/CompileGuard.h
@@ -0,0 +1,24 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2002, Perry L. Miller IV
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  This header file should not be included in client code.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _CV_PLUGIN_SCENE_OPERATIONS_COMPILE_GUARD_H_
+#define _CV_PLUGIN_SCENE_OPERATIONS_COMPILE_GUARD_H_
+
+#ifndef _COMPILING_SCENE_OPERATIONS
+# error "This header file is only used for compiling the library."
+# error "It should not be included in client code."
+#endif
+
+#endif // _CV_PLUGIN_SCENE_OPERATIONS_COMPILE_GUARD_H_

1e9515d2f229642f9df434e2edb4eda1a7b6670c
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2004-08-25 07:54:38 +0000

    - Some restructuring of new PDB reader. It now loads the big file that was   previously causing a swap. I turned down the detail on the spheres. - Started adding code for LOD-based scene, but have not completed that yet.

--- osgPlugins/pdb/ReaderWriterPDB.h
+++ osgPlugins/pdb/ReaderWriterPDB.h
@@ -10,27 +10,47 @@
 #ifndef __READER_WRITER_PDB_H__
 #define __READER_WRITER_PDB_H__
 
-#include <osg/Geode>
-#include <osg/Group>
-#include <osg/Shape>
-#include <osg/ShapeDrawable>
+#include "osgDB/ReaderWriter"
 
-#include <osgDB/ReadFile>
-#include <osgDB/Registry>
-#include <osgDB/FileNameUtils>
-#include <osgDB/FileUtils>
+#include "osg/BoundingBox"
+#include "osg/Vec4"
 
 #include <string>
-#include <fstream>
-#include <stdlib.h>
+#include <list>
+#include <iosfwd>
 
-using namespace std;
+namespace osg { class Group; class LOD; };
 
-class ReaderWriterPDB : public osgDB::ReaderWriter {
- public:
-  virtual const char* className();
-  virtual bool acceptsExtension(const string &);
-  virtual ReadResult readNode(const string&, const osgDB::ReaderWriter::Options *);
+
+class ReaderWriterPDB : public osgDB::ReaderWriter
+{
+public:
+
+  typedef std::pair<std::string,osg::Vec4> Atom;
+  typedef std::list<Atom> Atoms;
+  typedef osgDB::ReaderWriter::ReadResult Result;
+  typedef osgDB::ReaderWriter::Options Options;
+
+  ReaderWriterPDB();
+  ~ReaderWriterPDB();
+
+  virtual const char*     className();
+  virtual bool            acceptsExtension ( const std::string &extension );
+  virtual Result          readNode ( const std::string &filename, const Options * );
+
+protected:
+
+  osg::Group *            _build() const;
+  void                    _init();
+  osg::LOD *              _makeAtom ( const Atom &atom ) const;
+  Result                  _read ( const std::string &, const Options * );
+  void                    _parse ( std::ifstream &in );
+
+private:
+
+  Atoms _atoms;
+  osg::BoundingBox _bbox;
 };
 
-#endif
+
+#endif // __READER_WRITER_PDB_H__

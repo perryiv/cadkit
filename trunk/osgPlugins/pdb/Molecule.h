b4726a14313491eb2616334303e297e4454389b3
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2004-08-26 17:07:24 +0000

    Forgot to add new files

---
+++ trunk/osgPlugins/pdb/Molecule.h
@@ -0,0 +1,48 @@
+
+
+
+#ifndef __OSG_PDB_PLUGIN_MOLECULE_H__
+#define __OSG_PDB_PLUGIN_MOLECULE_H__
+
+#include "Atom.h"
+#include "Bond.h"
+#include "MaterialChooser.h"
+
+#include "osg/Referenced"
+#include "osg/Group"
+
+#include <map>
+#include <list>
+
+class Molecule : public osg::Referenced
+{
+public:
+  typedef std::pair<Atom::ID, Atom> atom_pair;
+  typedef std::map<Atom::ID, Atom> Map;
+  typedef Map Atoms;
+  typedef std::list<Bond> Bonds;
+
+  Molecule(MaterialChooser* mc);
+  osg::Group* build() const { return _build(); }
+  void addAtom(const Atom&);
+  void addBond(Atom::ID, Atom::ID);
+
+protected:
+  osg::Group*             _build() const;
+  osg::LOD *              _makeAtom ( const Atom &atom ) const;
+  osg::LOD *              _makeBond ( const Bond &bond) const;
+  osg::Geode *            _makeSphere ( const osg::Vec3 &center, float radius, float detail ) const;
+  osg::Geode *            _makeCube   ( const osg::Vec3 &center, float size ) const;
+  void                    _setCentersAndRanges ( osg::LOD *lod ) const;
+private:
+  Atoms _atoms;
+  Bonds _bonds;
+  float _maxDistanceFactor;
+  float _lastRangeMax;
+  unsigned int _numLodChildren;
+  float _lodDistancePower;
+  MaterialChooser *_materialChooser;
+};
+
+
+#endif

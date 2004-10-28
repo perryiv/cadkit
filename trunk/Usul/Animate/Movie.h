712ecf6b7c9e357b5b208c15f4b94d130a8eff9e
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2004-10-28 14:49:45 +0000

    Added files for movie creation.

---
+++ trunk/Usul/Animate/Movie.h
@@ -0,0 +1,40 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2004, Adam Kubach
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __USUL_ANIMATE_MOVIE_H__
+#define __USUL_ANIMATE_MOVIE_H__
+
+
+namespace Usul
+{
+  namespace Animate
+  {
+    template < class Sequence, class Player > class Movie
+    {
+    public:
+      typedef typename Sequence::value_type Frame;
+
+      Movie () : _frames() { }
+
+      void setPlayer ( Player *player ) { _player = player; }
+
+      void append ( Frame &frame ) { _frames.push_back ( frame ); }
+
+      void play () { (*_player) ( _frames ); }
+
+    private:
+      Sequence _frames;
+      Player *_player;
+    }; // Movie
+
+  }; // namespace Animate
+
+}; // namespace Usul
+
+#endif //__USUL_ANIMATE_MOVIE_H__
+
---
+++ trunk/Usul/Animate/Player.h
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2004, Adam Kubach
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __USUL_ANIMATE_PLAYER_H__
+#define __USUL_ANIMATE_PLAYER_H__
+
+namespace Usul
+{
+  namespace Animate
+  {
+    template < class Sequence, class Callback > class Player
+    {
+    public:
+      typedef typename Sequence::value_type Frame;
+
+      Player ( Callback cb ) : _callback (cb) {}
+
+      virtual void operator() ( Sequence &frames ) = 0;
+
+    protected:
+      Callback _callback;
+    }; // Linear Player
+
+  }; // namespace Animate
+
+}; // namespace Usul
+
+#endif //__USUL_ANIMATE_LINEAR_PLAYER_H__

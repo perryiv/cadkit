1fa42ca79fbcc2f79feeabd86218e7849a507112
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2004-11-16 18:23:44 +0000

    Added IToolCommand interface

---
+++ Usul/Interfaces/IToolCommand.h
@@ -0,0 +1,41 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2004, Adam Kubach
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Interface for excuting a command with FOX event
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __USUL_INTERFACE_TOOL_COMMAND__
+#define __USUL_INTERFACE_TOOL_COMMAND__
+
+namespace FX { struct FXEvent; }
+
+namespace Usul
+{
+  namespace Interfaces
+  {
+    class IToolCommand : public Usul::Interfaces::IUnknown
+    {
+    public:
+      typedef Usul::Interfaces::IUnknown Unknown;
+
+      // Smart-pointer definitions.
+      USUL_DECLARE_QUERY_POINTERS ( IToolCommand );
+
+      enum { IID = 1100623970u };
+
+      virtual void execute( Unknown* caller, const FX::FXEvent &event) = 0;
+
+    };//class IToolCommand
+  }; //namespace Interfaces
+};//namespace Usul
+
+#endif //__USUL_INTERFACE_TOOL_COMMAND__

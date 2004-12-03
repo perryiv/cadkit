6d883ab73d03938bde07490aa80c214acd3eb8d5
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2004-12-03 18:41:13 +0000

    Moved the creation of new children in the oct-tree to a policy class.

--- Usul/Containers/OctTree.h
+++ Usul/Containers/OctTree.h
@@ -21,6 +21,48 @@ namespace Containers {
 
 ///////////////////////////////////////////////////////////////////////////////
 //
+//  Policy functor to make children adjacent.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+namespace Detail
+{
+  struct AdjacentChildren
+  {
+    template < class OctTreeType > void operator () ( OctTreeType &tree )
+    {
+      typedef typename OctTreeType::Real Real;
+      typedef typename OctTreeType::Children Children;
+      typedef typename Children::iterator ChildItr;
+      typedef typename OctTreeType::Vec3 Vec3;
+
+      // Determine new size.
+      Real s ( tree.size() * 0.5f );
+      Vec3 mn ( tree.minimum() );
+      Vec3 mx ( tree.maximum() );
+
+      // Clear and reserve because this works with a list as well as a vector.
+      // TODO: add error-checking for existing children?
+      Children &children = tree.children();
+      children.clear();
+      children.reserve ( 8 );
+
+      // Set the children.
+      _children.push_back ( OctTreeType ( s, mn[0],     mn[1],     mn[2]     ) );
+      _children.push_back ( OctTreeType ( s, mn[0] + s, mn[1],     mn[2]     ) );
+      _children.push_back ( OctTreeType ( s, mn[0],     mn[1] + s, mn[2]     ) );
+      _children.push_back ( OctTreeType ( s, mn[0] + s, mn[1] + s, mn[2]     ) );
+      _children.push_back ( OctTreeType ( s, mn[0],     mn[1],     mn[2] + s ) );
+      _children.push_back ( OctTreeType ( s, mn[0] + s, mn[1],     mn[2] + s ) );
+      _children.push_back ( OctTreeType ( s, mn[0],     mn[1] + s, mn[2] + s ) );
+      _children.push_back ( OctTreeType ( s, mn[0] + s, mn[1] + s, mn[2] + s ) );
+    }
+  };
+};
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
 //  Oct-tree class.
 //
 ///////////////////////////////////////////////////////////////////////////////
@@ -29,6 +71,7 @@ template
 <
   class Tester_,
   class ObjectContainer_, 
+  class ChildMaker_ = Usul::Containers::Detail::AdjacentChildren,
   class Real_ = double,
   class Vec3_ = Usul::Math::Vector3<RealType_>
 >
@@ -39,11 +82,12 @@ public:
   typedef ObjectContainer_ Objects;
   typedef typename Objects::value_type Object;
   typedef Tester_ Tester;
+  typedef ChildMaker_ ChildMaker;
   typedef std::vector<OctTree> Children;
   typedef typename Children::iterator ChildItr;
   typedef Real_ Real;
   typedef Vec3_ Vec3;
-  typedef OctTree<Tester,Objects,Real,Vec3> ThisType;
+  typedef OctTree<Tester,Objects,ChildMaker,Real,Vec3> ThisType;
 
 
   /////////////////////////////////////////////////////////////////////////////
@@ -52,13 +96,13 @@ public:
   //
   /////////////////////////////////////////////////////////////////////////////
 
-  explicit OctTree() : _size ( 0 ), _children(), _min(), _objects(), _tester()
+  explicit OctTree() : _size ( 0 ), _children(), _min(), _objects(), _tester(), _childMaker()
   {
   }
-  OctTree ( const OctTree &t ) : _size ( t._size ), _children ( t._children ), _min ( t._min ), _objects ( t._objects ), _tester ( t._tester )
+  OctTree ( const OctTree &t ) : _size ( t._size ), _children ( t._children ), _min ( t._min ), _objects ( t._objects ), _tester ( t._tester ), _childMaker ( t._childMaker )
   {
   }
-  OctTree ( Real size, Real minX, Real minY, Real minZ ) : _size ( size ), _min ( minX, minY, minZ ), _objects(), _tester()
+  OctTree ( Real size, Real minX, Real minY, Real minZ ) : _size ( size ), _min ( minX, minY, minZ ), _objects(), _tester(), _childMaker()
   {
   }
 
@@ -83,20 +127,14 @@ public:
 
   void subdivide ( unsigned int num )
   {
+    // Return now if we are at the end.
     if ( 0 == num )
       return;
 
-    _children.reserve ( 8 );
-    Real size ( this->size() / 2 );
-    _children.push_back ( ThisType ( size, _min[0],        _min[1],        _min[2]        ) );
-    _children.push_back ( ThisType ( size, _min[0] + size, _min[1],        _min[2]        ) );
-    _children.push_back ( ThisType ( size, _min[0],        _min[1] + size, _min[2]        ) );
-    _children.push_back ( ThisType ( size, _min[0] + size, _min[1] + size, _min[2]        ) );
-    _children.push_back ( ThisType ( size, _min[0],        _min[1],        _min[2] + size ) );
-    _children.push_back ( ThisType ( size, _min[0] + size, _min[1],        _min[2] + size ) );
-    _children.push_back ( ThisType ( size, _min[0],        _min[1] + size, _min[2] + size ) );
-    _children.push_back ( ThisType ( size, _min[0] + size, _min[1] + size, _min[2] + size ) );
+    // Make the children.
+    _childMaker ( *this );
 
+    // Divide the children.
     for ( ChildItr i = _children.begin(); i != _children.end(); ++i )
       i->subdivide ( num - 1 );
   }
@@ -223,6 +261,22 @@ public:
     return _children;
   }
 
+
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  Return the child-maker.
+  //
+  /////////////////////////////////////////////////////////////////////////////
+
+  const ChildMaker &childMaker() const
+  {
+    return _childMaker;
+  }
+  ChildMaker &childMaker()
+  {
+    return _childMaker;
+  }
+
 private:
 
   /////////////////////////////////////////////////////////////////////////////
@@ -236,6 +290,7 @@ private:
   Vec3 _min;
   Objects _objects;
   Tester _tester;
+  ChildMaker _childMaker;
 };
 
 

65063f67cdb38a4489eb16de5ab055dcebbdde78
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2004-12-06 22:25:57 +0000

    - Small change to findAdjacent() algorithm. - Changed oct-tree to work with generic objects. - Oct-tree subdivides as needed.

--- trunk/Usul/Algorithms/Polygons.h
+++ trunk/Usul/Algorithms/Polygons.h
@@ -35,7 +35,7 @@ namespace Polygons {
     {
       typedef typename VertexSequence::value_type Vertex;
 
-      unsigned int count = 0;
+      unsigned int count ( 0 );
 
       const Vertex &polyOneP1 = vertices.at( p1 );
       const Vertex &polyOneP2 = vertices.at( p1 + 1 );
@@ -46,15 +46,15 @@ namespace Polygons {
       const Vertex &polyTwoP3 = vertices.at( p2 + 2 );
 
       if( polyOneP1 != polyTwoP1 && polyOneP1 != polyTwoP2 && polyOneP1 != polyTwoP3 )
-        count++;
+        ++count;
       if( polyOneP2 != polyTwoP1 && polyOneP2 != polyTwoP2 && polyOneP2 != polyTwoP3 )
-        count++;
+        ++count;
       if( count == 2 )
         return false;
       if( count == 0 )
         return true;
       if( polyOneP3 != polyTwoP1 && polyOneP3 != polyTwoP2 && polyOneP3 != polyTwoP3 )
-        count++;
+        ++count;
       if( count >= 2 )
         return false;
       return true;
@@ -65,9 +65,11 @@ namespace Polygons {
   struct TriangleSorter
   {
     typedef typename VertexSequence::value_type Vertex;
+    typedef typename Vertex::value_type RealType;
     TriangleSorter ( const VertexSequence& vertices ) : _vertices ( vertices ) { }
     bool operator() ( unsigned int p1, unsigned int p2 )
     {
+      const RealType oneThird ( 1.0f / 3.0f );
       p1 *= 3;
       p2 *= 3;
       const Vertex &polyOneP1 = _vertices.at( p1 );
@@ -75,24 +77,109 @@ namespace Polygons {
       const Vertex &polyOneP3 = _vertices.at( p1 + 2 );
 
       Vertex polyOneCenter;
-      polyOneCenter[0] = ( polyOneP1[0] + polyOneP2[0] + polyOneP3[0] ) / 3;
-      polyOneCenter[1] = ( polyOneP1[1] + polyOneP2[1] + polyOneP3[1] ) / 3;
-      polyOneCenter[2] = ( polyOneP1[2] + polyOneP2[2] + polyOneP3[2] ) / 3;
+      polyOneCenter[0] = ( polyOneP1[0] + polyOneP2[0] + polyOneP3[0] ) * oneThird;
+      polyOneCenter[1] = ( polyOneP1[1] + polyOneP2[1] + polyOneP3[1] ) * oneThird;
+      polyOneCenter[2] = ( polyOneP1[2] + polyOneP2[2] + polyOneP3[2] ) * oneThird;
 
       const Vertex &polyTwoP1 = _vertices.at( p2 );
       const Vertex &polyTwoP2 = _vertices.at( p2 + 1 );
       const Vertex &polyTwoP3 = _vertices.at( p2 + 2 );
 
       Vertex polyTwoCenter;
-      polyTwoCenter[0] = ( polyTwoP1[0] + polyTwoP2[0] + polyTwoP3[0] ) / 3;
-      polyTwoCenter[1] = ( polyTwoP1[1] + polyTwoP2[1] + polyTwoP3[1] ) / 3;
-      polyTwoCenter[2] = ( polyTwoP1[2] + polyTwoP2[2] + polyTwoP3[2] ) / 3;
+      polyTwoCenter[0] = ( polyTwoP1[0] + polyTwoP2[0] + polyTwoP3[0] ) * oneThird;
+      polyTwoCenter[1] = ( polyTwoP1[1] + polyTwoP2[1] + polyTwoP3[1] ) * oneThird;
+      polyTwoCenter[2] = ( polyTwoP1[2] + polyTwoP2[2] + polyTwoP3[2] ) * oneThird;
 
       return polyOneCenter < polyTwoCenter;
     }
   private:
     const VertexSequence& _vertices;
   };
+
+
+template < class IndexSequence, class Iter >
+inline void removeFromIndexSequence(IndexSequence &indices, Iter& iter)
+{
+  Iter temp ( iter );
+  --iter;
+  indices.erase ( temp );
+}
+
+template < >
+inline void removeFromIndexSequence < std::list< unsigned int> , std::list< unsigned int>::reverse_iterator> 
+( std::list< unsigned int> &indices, std::list< unsigned int>::reverse_iterator& iter)
+{
+  std::list< unsigned int>::iterator temp ( iter.base() );
+  --iter;
+  indices.erase ( temp );
+}
+
+
+template < class KeeperIter, class Iter, class VertexSequence, class IndexSequence, class AdjacentTest >
+inline bool isPolygonAdjacent ( KeeperIter& keeperItr,
+                           Iter& indexItr,
+                           const VertexSequence& vertices,
+                           IndexSequence& keepers,
+                           IndexSequence& indices,
+                           const AdjacentTest& adjacentTest,
+                           unsigned int numVertsPerPoly,
+                           std::vector< unsigned int >& adjacentCount )
+{
+  // See if the current keeper is adjacent to this polygon.
+  if ( true == adjacentTest ( vertices, *keeperItr * numVertsPerPoly, *indexItr * numVertsPerPoly ) )
+  {
+    adjacentCount[ *keeperItr ] ++;
+    adjacentCount[ *indexItr ] ++;
+    // Put the polygon into the list of keepers.
+    keepers.push_back ( *indexItr );
+    removeFromIndexSequence( indices, indexItr );
+    return true;
+  }
+
+  return false;
+}
+
+
+template < class Iter, class VertexSequence, class IndexSequence, class AdjacentTest >
+inline Iter searchPolygonList ( Iter& keeperItr,
+                           Iter& indexItr,
+                           const VertexSequence& vertices,
+                           IndexSequence& keepers,
+                           IndexSequence& indices,
+                           const AdjacentTest& adjacentTest,
+                           unsigned int numVertsPerPoly,
+                           std::vector< unsigned int >& adjacentCount )
+{
+  IndexSequence::reverse_iterator rIter ( indexItr );
+  IndexSequence::iterator fIter( indexItr );
+
+  while ( adjacentCount [ *keeperItr ] != numVertsPerPoly )
+  {
+    if( rIter != indices.rend() )
+    {
+      if ( isPolygonAdjacent ( keeperItr, rIter, vertices, keepers, indices, adjacentTest, numVertsPerPoly, adjacentCount) )
+      {
+        ++rIter;
+        return rIter.base();
+      }
+      ++rIter;
+    }
+     
+    if( fIter != indices.end() )
+    {
+      if ( isPolygonAdjacent ( keeperItr, fIter, vertices, keepers, indices, adjacentTest, numVertsPerPoly, adjacentCount) )
+      {
+        ++fIter;
+        return fIter;
+      }
+      ++fIter;
+    }
+    if( rIter == indices.rend() && fIter == indices.end() )
+      break;
+  }
+  return indices.end();
+}
+
 
 ///////////////////////////////////////////////////////////////////////////////
 //
@@ -121,7 +208,7 @@ template
   class VertexSequence,
   class IndexSequence
 > 
-void findAdjacent ( const AdjacentTest &adjacentTest, 
+inline void findAdjacent ( const AdjacentTest &adjacentTest, 
                     Updater &updater,
                     const Sorter &sorter,
                     const VertexSequence &vertices, 
@@ -176,7 +263,7 @@ void findAdjacent ( const AdjacentTest &adjacentTest,
 
   // Fill the sequence of indices.
   IndexSequence indices;
-  indices.resize ( numPolygons );
+  indices.resize ( numPolygons ); // numPolygons - 1 ?
   {
     SizeType count ( 0 );
     for ( IndexIterator i = indices.begin(); i != indices.end(); ++i )
@@ -196,7 +283,7 @@ void findAdjacent ( const AdjacentTest &adjacentTest,
   // Initialize the current keeper.
   IndexIterator keeperItr = keepers.begin();
 
-  //Make an vector that contains the number of adjacent polygons found
+  // Make a vector that contains the number of adjacent polygons found
   std::vector < unsigned int > adjacentCount ( numPolygons, 0 );
 
   // While there are still keepers to test...
@@ -216,86 +303,6 @@ void findAdjacent ( const AdjacentTest &adjacentTest,
   }
 }
 
-template < class KeeperIter, class Iter, class VertexSequence, class IndexSequence, class AdjacentTest >
-bool isPolygonAdjacent ( KeeperIter& keeperItr,
-                           Iter& indexItr,
-                           const VertexSequence& vertices,
-                           IndexSequence& keepers,
-                           IndexSequence& indices,
-                           const AdjacentTest& adjacentTest,
-                           unsigned int numVertsPerPoly,
-                           std::vector< unsigned int >& adjacentCount )
-{
-  // See if the current keeper is adjacent to this polygon.
-  if ( true == adjacentTest ( vertices, *keeperItr * numVertsPerPoly, *indexItr * numVertsPerPoly ) )
-  {
-    adjacentCount[ *keeperItr ] ++;
-    adjacentCount[ *indexItr ] ++;
-    // Put the polygon into the list of keepers.
-    keepers.push_back ( *indexItr );
-    removeFromIndexSequence( indices, indexItr );
-    return true;
-  }
-
-  return false;
-}
-
-template < class IndexSequence, class Iter >
-void removeFromIndexSequence(IndexSequence &indices, Iter& iter)
-{
-  Iter temp ( iter );
-  --iter;
-  indices.erase ( temp );
-}
-
-template < >
-void removeFromIndexSequence < std::list< unsigned int> , std::list< unsigned int>::reverse_iterator> 
-( std::list< unsigned int> &indices, std::list< unsigned int>::reverse_iterator& iter)
-{
-  std::list< unsigned int>::iterator temp ( iter.base() );
-  --iter;
-  indices.erase ( temp );
-}
-
-template < class Iter, class VertexSequence, class IndexSequence, class AdjacentTest >
-Iter searchPolygonList   ( Iter& keeperItr,
-                           Iter& indexItr,
-                           const VertexSequence& vertices,
-                           IndexSequence& keepers,
-                           IndexSequence& indices,
-                           const AdjacentTest& adjacentTest,
-                           unsigned int numVertsPerPoly,
-                           std::vector< unsigned int >& adjacentCount )
-{
-  IndexSequence::reverse_iterator rIter ( indexItr );
-  IndexSequence::iterator fIter( indexItr );
-
-  while ( adjacentCount [ *keeperItr ] != numVertsPerPoly )
-  {
-    if( rIter != indices.rend() )
-    {
-      if ( isPolygonAdjacent ( keeperItr, rIter, vertices, keepers, indices, adjacentTest, numVertsPerPoly, adjacentCount) )
-      {
-        ++rIter;
-        return rIter.base();
-      }
-      ++rIter;
-    }
-     
-    if( fIter != indices.end() )
-    {
-      if ( isPolygonAdjacent ( keeperItr, fIter, vertices, keepers, indices, adjacentTest, numVertsPerPoly, adjacentCount) )
-      {
-        ++fIter;
-        return fIter;
-      }
-      ++fIter;
-    }
-    if( rIter == indices.rend() && fIter == indices.end() )
-      break;
-  }
-  return indices.end();
-}
 
 ///////////////////////////////////////////////////////////////////////////////
 //
@@ -312,7 +319,7 @@ Iter searchPolygonList   ( Iter& keeperItr,
 //
 ///////////////////////////////////////////////////////////////////////////////
 template < class VertexSequence, class AdjacentList, class AdjacentTest >
-void buildAdjacentList( AdjacentList& adjacentList, const VertexSequence& vertices, const AdjacentTest& adjacentTest, unsigned int numVertsPerPoly )
+inline void buildAdjacentList( AdjacentList& adjacentList, const VertexSequence& vertices, const AdjacentTest& adjacentTest, unsigned int numVertsPerPoly )
 {
   //TODO add error checking
 
--- trunk/Usul/Containers/OctTree.h
+++ trunk/Usul/Containers/OctTree.h
@@ -21,43 +21,58 @@ namespace Containers {
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-//  Policy functor to test the inclusion of a triangle.
+//  Policy functor to test if a triangle is in the cube.
 //
 ///////////////////////////////////////////////////////////////////////////////
 
 namespace Detail
 {
-  struct TriangleInside
+  template < class VertexContainer > struct TriangleInside
   {
-    template < class OctTreeType, class Triangle > void operator () ( const OctTreeType &tree, Triangle &t ) const
+    TriangleInside() : _v ( 0x0 )
     {
-      typedef typename OctTreeType::Vec3 Vec3;
-      const Vec3 mn ( tree.minimum() );
-      const Vec3 mx ( tree.maximum() );
+    }
+    template < class Vec3, class TriangleType > bool operator () ( const Vec3 &mn, const Vec3 &mx, const TriangleType &t ) const
+    {
+      VertexContainer &v = *_v;
 
       // Determine place of first vertex (in or out).
-      bool x0 ( t[0][0] > mn[0] && t[[0][0] <= mx[0] );
-      bool y0 ( t[0][1] > mn[1] && t[[0][1] <= mx[1] );
-      bool z0 ( t[0][2] > mn[2] && t[[0][2] <= mx[2] );
+      bool x0 ( v[t.v0][0] > mn[0] && v[t.v0][0] <= mx[0] );
+      bool y0 ( v[t.v0][1] > mn[1] && v[t.v0][1] <= mx[1] );
+      bool z0 ( v[t.v0][2] > mn[2] && v[t.v0][2] <= mx[2] );
 
-      // Determine place of second vertex (in or out.
-      bool x1 ( t[1][0] > mn[0] && t[[1][0] <= mx[0] );
-      bool y1 ( t[1][1] > mn[1] && t[[1][1] <= mx[1] );
-      bool z1 ( t[1][2] > mn[2] && t[[1][2] <= mx[2] );
+      // Determine place of second vertex (in or out).
+      bool x1 ( v[t.v1][0] > mn[0] && v[t.v1][0] <= mx[0] );
+      bool y1 ( v[t.v1][1] > mn[1] && v[t.v1][1] <= mx[1] );
+      bool z1 ( v[t.v1][2] > mn[2] && v[t.v1][2] <= mx[2] );
 
-      // Determine place of third vertex (in or out.
-      bool x2 ( t[2][0] > mn[0] && t[[2][0] <= mx[0] );
-      bool y2 ( t[2][1] > mn[1] && t[[2][1] <= mx[1] );
-      bool z2 ( t[2][2] > mn[2] && t[[2][2] <= mx[2] );
+      // Determine place of third vertex (in or out).
+      bool x2 ( v[t.v2][0] > mn[0] && v[t.v2][0] <= mx[0] );
+      bool y2 ( v[t.v2][1] > mn[1] && v[t.v2][1] <= mx[1] );
+      bool z2 ( v[t.v2][2] > mn[2] && v[t.v2][2] <= mx[2] );
 
       // A point is in if all three of its coordinates are in.
       bool p0 ( x0 && y0 && z0 );
       bool p1 ( x1 && y1 && z1 );
       bool p2 ( x2 && y2 && z2 );
 
-      // We need to have at least two points in.
+      // A triangle is in if at least two points are in.
       return ( ( p0 && p1 ) || ( p0 && p2 ) || ( p1 && p2 ) );
     }
+    const VertexContainer *vertices() const
+    {
+      return _v;
+    }
+    VertexContainer *vertices()
+    {
+      return _v;
+    }
+    void vertices ( VertexContainer *v )
+    {
+      _v = v;
+    }
+  private:
+    VertexContainer *_v;
   };
 };
 
@@ -70,8 +85,19 @@ namespace Detail
 
 namespace Detail
 {
-  struct AdjacentChildren
+  class AdjacentChildren
   {
+    template < class OctTreeType, class Real > 
+    void _add ( OctTreeType &tree, Real size, const Real &minX, const Real &minY, const Real &minZ ) const
+    {
+      typedef typename OctTreeType::Children Children;
+
+      // Add the child.
+      Children &children = tree.children();
+      children.push_back ( new OctTreeType ( size, minX, minY, minZ, &tree ) );
+      children.back()->tester().vertices ( tree.tester().vertices() );
+    }
+  public:
     template < class OctTreeType > void operator () ( OctTreeType &tree ) const
     {
       typedef typename OctTreeType::Real Real;
@@ -90,15 +116,15 @@ namespace Detail
       children.clear();
       children.reserve ( 8 );
 
-      // Set the children.
-      _children.push_back ( OctTreeType ( s, mn[0],     mn[1],     mn[2]     ) );
-      _children.push_back ( OctTreeType ( s, mn[0] + s, mn[1],     mn[2]     ) );
-      _children.push_back ( OctTreeType ( s, mn[0],     mn[1] + s, mn[2]     ) );
-      _children.push_back ( OctTreeType ( s, mn[0] + s, mn[1] + s, mn[2]     ) );
-      _children.push_back ( OctTreeType ( s, mn[0],     mn[1],     mn[2] + s ) );
-      _children.push_back ( OctTreeType ( s, mn[0] + s, mn[1],     mn[2] + s ) );
-      _children.push_back ( OctTreeType ( s, mn[0],     mn[1] + s, mn[2] + s ) );
-      _children.push_back ( OctTreeType ( s, mn[0] + s, mn[1] + s, mn[2] + s ) );
+      // Add the children.
+      this->_add ( tree, s, mn[0],     mn[1],     mn[2]     );
+      this->_add ( tree, s, mn[0] + s, mn[1],     mn[2]     );
+      this->_add ( tree, s, mn[0],     mn[1] + s, mn[2]     );
+      this->_add ( tree, s, mn[0] + s, mn[1] + s, mn[2]     );
+      this->_add ( tree, s, mn[0],     mn[1],     mn[2] + s );
+      this->_add ( tree, s, mn[0] + s, mn[1],     mn[2] + s );
+      this->_add ( tree, s, mn[0],     mn[1] + s, mn[2] + s );
+      this->_add ( tree, s, mn[0] + s, mn[1] + s, mn[2] + s );
     }
   };
 };
@@ -114,27 +140,62 @@ namespace Detail
 {
   struct AddIfInside
   {
-    template < class OctTreeType, class ObjectType > void operator () ( OctTreeType &tree, ObjectType &object ) const
+    template < class OctTreeType, class ObjectType > bool operator () ( OctTreeType &tree, ObjectType &object ) const
     {
       typedef typename OctTreeType::Children Children;
+      typedef typename OctTreeType::Objects Objects;
       typedef typename Children::iterator ChildItr;
+      typedef typename ObjectType::CubeType CubeType;
 
       // Is the object in this cube?
       if ( !tree.inside ( object ) )
-        tree.objects().push_back ( object );
+        return false;
+
+      // Shortcuts.
+      Children &children = tree.children();
+      Objects &objects   = tree.objects();
+
+      // Add the object to this cube.
+      object.cube = reinterpret_cast < CubeType * > ( &tree );
+      objects.push_back ( object );
+
+      // Make sure there are children.
+      if ( children.empty() )
+        tree.subdivide ( 1 );
 
       // Test the children.
-      Children &children = tree.children();
       for ( ChildItr i = children.begin(); i != children.end(); ++i )
       {
         // Try to add it to this child.
-        if ( i->add ( object ) )
-          return true;
+        if ( (*i)->add ( object ) )
+          break;
       }
 
-      // If we get to here then we could not add the object.
-      return false;
+      // We at least added the object to the given cube.
+      return true;
     }
+  };
+};
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Triangle class.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+namespace Detail
+{
+  struct Triangle
+  {
+    typedef void * CubeType;
+    typedef unsigned int IndexType;
+    Triangle() : v0(0), v1(0), v2(0), cube(0){}
+    Triangle ( IndexType i0, IndexType i1, IndexType i2 ) : v0(i0), v1(i1), v2(i2), cube(0){}
+    IndexType v0;
+    IndexType v1;
+    IndexType v2;
+    CubeType cube;
   };
 };
 
@@ -150,9 +211,9 @@ template
   class ObjectContainer_, 
   class ChildMaker_   = Usul::Containers::Detail::AdjacentChildren,
   class Tester_       = Usul::Containers::Detail::TriangleInside,
-  class Adder_        = Usul::Containers::Detail::AddIfInside
+  class Adder_        = Usul::Containers::Detail::AddIfInside,
   class Real_         = double,
-  class Vec3_         = Usul::Math::Vector3<RealType_>
+  class Vec3_         = Usul::Math::Vector3<Real_>
 >
 class OctTree
 {
@@ -163,7 +224,7 @@ public:
   typedef Tester_ Tester;
   typedef Adder_ Adder;
   typedef ChildMaker_ ChildMaker;
-  typedef std::vector<OctTree> Children;
+  typedef std::vector<OctTree*> Children;
   typedef typename Children::iterator ChildItr;
   typedef Real_ Real;
   typedef Vec3_ Vec3;
@@ -176,16 +237,28 @@ public:
   //
   /////////////////////////////////////////////////////////////////////////////
 
-  explicit OctTree() : _size ( 0 ), _children(), _min(), _objects(), _tester(), _childMaker(), _adder()
+  explicit OctTree ( OctTree *p = 0x0 ) : _size ( 0 ), _children(), _min(), _objects(), _tester(), _childMaker(), _adder(), _parent ( p )
   {
   }
-  OctTree ( const OctTree &t ) : _size ( t._size ), _children ( t._children ), _min ( t._min ), _objects ( t._objects ), _tester ( t._tester ), _childMaker ( t._childMaker ), _adder ( t._adder )
+  OctTree ( const OctTree &t ) : _size ( t._size ), _children ( t._children ), _min ( t._min ), _objects ( t._objects ), _tester ( t._tester ), _childMaker ( t._childMaker ), _adder ( t._adder ), _parent ( t._parent )
   {
   }
-  OctTree ( Real size, Real minX, Real minY, Real minZ ) : _size ( size ), _min ( minX, minY, minZ ), _objects(), _tester(), _childMaker(), _adder()
+  OctTree ( Real size, Real minX, Real minY, Real minZ, OctTree *p = 0x0 ) : _size ( size ), _min ( minX, minY, minZ ), _objects(), _tester(), _childMaker(), _adder(), _parent ( p )
   {
   }
 
+
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  Destructor.
+  //
+  /////////////////////////////////////////////////////////////////////////////
+
+  ~OctTree()
+  {
+    for ( ChildItr i = _children.begin(); i != _children.end(); ++i )
+      delete (*i);
+  }
 
   /////////////////////////////////////////////////////////////////////////////
   //
@@ -216,7 +289,7 @@ public:
 
     // Divide the children.
     for ( ChildItr i = _children.begin(); i != _children.end(); ++i )
-      i->subdivide ( num - 1 );
+      (*i)->subdivide ( num - 1 );
   }
 
 
@@ -238,7 +311,7 @@ public:
   //
   /////////////////////////////////////////////////////////////////////////////
 
-  bool add ( const Object &object )
+  bool add ( Object &object )
   {
     return _adder ( *this, object );
   }
@@ -360,6 +433,26 @@ public:
     return _adder;
   }
 
+
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  Set/get the parent.
+  //
+  /////////////////////////////////////////////////////////////////////////////
+
+  const OctTree *parent() const
+  {
+    return _parent;
+  }
+  OctTree *parent()
+  {
+    return _parent;
+  }
+  void parent ( OctTree *p )
+  {
+    _parent = p;
+  }
+
 private:
 
   /////////////////////////////////////////////////////////////////////////////
@@ -375,6 +468,7 @@ private:
   Tester _tester;
   ChildMaker _childMaker;
   Adder _adder;
+  OctTree *_parent;
 };
 
 

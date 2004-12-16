efb98da39a18f8e03dd546a05a0ac997514381a1
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2004-12-16 19:01:38 +0000

    - Some changes to Usul::Components::Object. - Added two files in Usul/File. - Added interface to identify if the file extension can be read by the   component.

---
+++ Usul/File/Path.h
@@ -0,0 +1,90 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2002, Perry L. Miller IV
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Functions for working with a file path.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _USUL_FILE_PATH_H_
+#define _USUL_FILE_PATH_H_
+
+#include <string>
+
+
+namespace Usul {
+namespace File {
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Get the directory from the path.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+inline std::string directory ( const std::string &path, bool wantSlash )
+{
+  typedef std::string::value_type Char;
+  typedef std::string::const_reverse_iterator Itr;
+  typedef std::string::size_type Size;
+  typedef std::equal_to<Char> IsEqual;
+  typedef std::logical_or<bool> Or;
+
+  // Look for the last slash.
+#ifdef _WIN32
+  Itr i ( std::find_if ( path.rbegin(), path.rend(), Usul::Predicates::unaryPair ( std::bind2nd ( IsEqual(), '\\' ), Or(), std::bind2nd ( IsEqual(), '/' ) ) ) );
+#else
+  Itr i ( std::find_if ( path.rbegin(), path.rend(), std::bind2nd ( IsEqual(), '/' ) ) );
+#endif
+
+  // Make the string.
+  std::string dir ( path.begin(), i.base() );
+
+  // Return now if it's empty.
+  if ( dir.empty() )
+    return dir;
+
+  // Drop the slash if we're supposed to.
+  Size last ( dir.size() - 1 );
+  if ( ( false == wantSlash ) && ( '\\' == dir[last] || '/' == dir[last] ) )
+    dir.erase ( last );
+
+  // Return the result.
+  return dir;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Get the extension from the path.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+inline std::string extension ( const std::string &path )
+{
+  typedef std::string::value_type Char;
+  typedef std::string::const_reverse_iterator Itr;
+  typedef std::equal_to<Char> IsEqual;
+  typedef std::logical_or<bool> Or;
+
+  // Look for the last period.
+  Itr i ( std::find_if ( path.rbegin(), path.rend(), std::bind2nd ( IsEqual(), '.' ) ) );
+
+  // Return the result.
+  const std::string ext ( i.base(), path.end() );
+  return ext;
+}
+
+
+}; // namespace File
+}; // namespace Usul
+
+
+#endif // _USUL_FILE_PATH_H_

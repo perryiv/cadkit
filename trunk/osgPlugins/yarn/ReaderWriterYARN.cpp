232a49169fc2bbfd65498f944df083a7ccf66f60
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2004-12-28 04:41:12 +0000

    Added reading of yarn files.

--- trunk/osgPlugins/yarn/ReaderWriterYARN.cpp
+++ trunk/osgPlugins/yarn/ReaderWriterYARN.cpp
@@ -23,10 +23,8 @@
 
 #include <vector>
 #include <sstream>
-#include <iostream>
-#include <cassert>
+#include <fstream>
 #include <stdexcept>
-#include <locale>
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -36,7 +34,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 ReaderWriterYARN::ReaderWriterYARN() :
-_meshes()
+  _meshes()
 {
 }
 
@@ -116,34 +114,43 @@ ReaderWriterYARN::Result ReaderWriterYARN::readNode ( const std::string &file, c
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-osg::Group * ReaderWriterYARN::_build() const
+osg::Group *ReaderWriterYARN::_build() const
 {
   // The scene root.
   osg::ref_ptr<osg::Group> root ( new osg::Group );
 
-  Random < float > rd ( 0.0, 1.0 );
+  // Random number generator.
+  Random<float> rd ( 0.0f, 1.0f );
 
-  for( Meshes::const_iterator i = _meshes.begin(); i != _meshes.end(); ++ i)
+  // Colors used below.
+  osg::Vec4 emissive ( 0.0f, 0.0f, 0.0f, 1.0f );
+  osg::Vec4 specular ( 0.8f, 0.8f, 0.8f, 1.0f );
+
+  // Loop through the meshes.
+  for ( Meshes::const_iterator i = _meshes.begin(); i != _meshes.end(); ++i )
   {
+    // Generate the node.
     osg::ref_ptr< osg::Node > node ( (*i)() );
 
-    float r ( rd() );
-	  float g ( rd() );
-	  float b ( rd() );
+    // Make a random colors.
+    osg::Vec4 diffuse ( rd(), rd(), rd(), 1.0f );
+    osg::Vec4 ambient ( diffuse );
 
-    osg::ref_ptr< osg::Material > mat ( new osg::Material() );
-	  mat->setAmbient(osg::Material::FRONT_AND_BACK, osg::Vec4(r, g, b, 1.0));
-	  mat->setDiffuse(osg::Material::FRONT_AND_BACK, osg::Vec4(r, g, b, 1.0));
-	  mat->setEmission(osg::Material::FRONT_AND_BACK, osg::Vec4(0.1, 0.1, 0.1, 1.0));
-	  mat->setShininess(osg::Material::FRONT_AND_BACK, 100);
-	  mat->setSpecular(osg::Material::FRONT_AND_BACK, osg::Vec4(0.8, 0.8, 0.8, 1.0));
+    // Make a material.
+    osg::ref_ptr<osg::Material> mat ( new osg::Material() );
+	  mat->setAmbient   ( osg::Material::FRONT_AND_BACK, ambient  );
+	  mat->setDiffuse   ( osg::Material::FRONT_AND_BACK, diffuse  );
+	  mat->setEmission  ( osg::Material::FRONT_AND_BACK, emissive );
+    mat->setSpecular  ( osg::Material::FRONT_AND_BACK, specular );
+	  mat->setShininess ( osg::Material::FRONT_AND_BACK, 100      );
 
+    // Set the material.
     osg::ref_ptr< osg::StateSet > ss ( node->getOrCreateStateSet() );
     ss->setAttribute( mat.get() );
 
-    root->addChild( node.get() );
+    // Add new mesh to the root.
+    root->addChild ( node.get() );
   }
-
 
   // Return the root.
   return root.release();
@@ -177,10 +184,13 @@ ReaderWriterYARN::Result ReaderWriterYARN::_read ( const std::string &filename, 
   if ( !this->acceptsExtension ( osgDB::getFileExtension ( filename ) ) )
     return ReadResult::FILE_NOT_HANDLED;
 
+  // Open the file.
   std::ifstream in ( filename.c_str() );
   if ( !in.is_open() )
-   throw std::runtime_error ( "Error 2346450991: Failed to open file: " + filename );
-  this->_parse ( in );
+    throw std::runtime_error ( "Error 2346450991: Failed to open file: " + filename );
+
+  // Parse the file.
+  this->_parseYarns ( in );
 
   // Build the scene.
   osg::ref_ptr<osg::Group> root ( this->_build() );
@@ -192,70 +202,114 @@ ReaderWriterYARN::Result ReaderWriterYARN::_read ( const std::string &filename, 
   return root.release();
 }
 
+
 ///////////////////////////////////////////////////////////////////////////////
 //
-//  Parse the file.
+//  Parse the yarns.
 //
 ///////////////////////////////////////////////////////////////////////////////
-void ReaderWriterYARN::_parse( std::ifstream& fin )
+
+void ReaderWriterYARN::_parseYarns ( std::ifstream& fin )
 {
-  const unsigned int size ( 512 );
-  char buf[ size ];
+  const unsigned int size ( 1024 );
+  char buf[size];
 
-  unsigned int totalYarnNumber ( 0 );
-  unsigned int totalSliceNumber ( 0 );
-
-  std::vector< int > stackNumbers;
+  unsigned int numYarns ( 0 );
+  unsigned int numSlices ( 0 );
 
   while ( !fin.eof() )
   {
-    fin.getline( buf, size );
+    // Read the line.
+    fin.getline ( buf, size );
 
+    // Get the first word.
     std::istringstream in ( buf );
-
     std::string word;
-
     in >> word;
 
-    if( word == "Total_Yarn_Number" )
+    // Convert to lower-case.
+    std::transform ( word.begin(), word.end(), word.begin(), ::tolower );
+
+    if ( word == "total_yarn_number" )
     {
-      in >> totalYarnNumber;
-      _meshes.resize( totalYarnNumber );
+      in >> numYarns;
+      _meshes.resize ( numYarns );
     }
-    else if ( word == "Number_of_Slices" )
-      in >> totalSliceNumber;
-    else if ( word == "Yarn_Number" )
+
+    else if ( word == "number_of_slices" )
     {
-      stackNumbers.resize( totalYarnNumber );
-      for( unsigned int i = 0; i < totalYarnNumber; ++i )
-      {
-        int yarnNumber, numStacks;
-        fin.getline( buf, size );
-        std::istringstream in ( buf );
-        in >> yarnNumber >> numStacks;
-        stackNumbers.at( yarnNumber ) = numStacks;
-        _meshes.at( yarnNumber ).points().resize( numStacks * totalSliceNumber );
-        _meshes.at( yarnNumber ).size( totalSliceNumber, numStacks );
-      }
+      in >> numSlices;
     }
+
+    else if ( word == "yarn_number" )
+    {
+      this->_parseNumStacks ( fin, numYarns, numSlices );
+    }
+
     else if ( word == "center_point" )
     {
-      for( unsigned int i = 0; i < totalSliceNumber; ++i )
-      {
-        int yarn, stack, slice;
-        float x, y, z;
-        fin.getline( buf, size );
-        std::istringstream in ( buf );
-        in >> yarn >> stack >> slice >> x >> y >> z;
-        Vertex v;
-        v = _meshes.at( yarn ).point( slice, stack );
-        v[0] = x;
-        v[1] = y;
-        v[2] = z;
-      }
+      osg::Vec3 center;
+      in >> center[0] >> center[1] >> center[2];
+      this->_parseStack ( fin, numSlices, center );
     }
   }
 }
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Parse the number of stacks.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+void ReaderWriterYARN::_parseNumStacks ( std::ifstream& fin, unsigned int numYarns, unsigned int numSlices )
+{
+  unsigned int yarn, stacks;
+  ++numSlices; // We close the loop.
+
+  for ( unsigned int i = 0; i < numYarns; ++i )
+  {
+    fin >> yarn >> stacks;
+    _meshes.at(yarn).points().resize  ( stacks * numSlices );
+    _meshes.at(yarn).normals().resize ( stacks * numSlices );
+    _meshes.at(yarn).size ( numSlices, stacks );
+  }
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Parse the stack.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+void ReaderWriterYARN::_parseStack ( std::ifstream& fin, unsigned int numSlices, const osg::Vec3 &center )
+{
+  unsigned int yarn, stack, slice;
+  float x, y, z;
+
+  for ( unsigned int i = 0; i < numSlices; ++i )
+  {
+    // Read the data.
+    fin >> yarn >> stack >> slice >> x >> y >> z;
+
+    // Set the vertex.
+    Vertex &v = _meshes.at(yarn).point ( slice, stack );
+    v[0] = x;
+    v[1] = y;
+    v[2] = z;
+
+    // Set the normal.
+    Normal &n = _meshes.at(yarn).normal ( slice, stack );
+    n = v - center;
+    n.normalize();
+  }
+
+  // Close the loop by setting the last vertex to be the first.
+  _meshes.at(yarn).point  ( numSlices, stack ) = _meshes.at(yarn).point  ( 0, stack );
+  _meshes.at(yarn).normal ( numSlices, stack ) = _meshes.at(yarn).normal ( 0, stack );
+}
+
 
 ///////////////////////////////////////////////////////////////////////////////
 //

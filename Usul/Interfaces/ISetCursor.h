f0605c78030a1625a3657b3ededce5569b19e452
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2004-12-30 16:33:34 +0000

    -Added toolbar buttons for modes and tools
    -Added ability for plugins to add buttons to the toolbar

---
+++ Usul/Interfaces/ISetCursor.h
@@ -0,0 +1,41 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2004, Adam Kubach
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Interface for setting the cursor
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _USUL_INTERFACE_SET_CURSOR_H__
+#define _USUL_INTERFACE_SET_CURSOR_H__
+
+#include "Usul/Interfaces/IUnknown.h"
+
+namespace Usul {
+namespace Interfaces {
+
+
+struct ISetCursor : public Usul::Interfaces::IUnknown
+{
+  /// Smart-pointer definitions.
+  USUL_DECLARE_QUERY_POINTERS ( ISetCursor );
+
+  /// Id for this interface.
+  enum { IID = 1104265934u };
+
+  
+}; // class ISetCursor
+
+
+}; // namespace Interfaces
+}; // namespace Usul
+
+
+#endif // _USUL_INTERFACE_SET_CURSOR_H__

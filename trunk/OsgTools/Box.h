95628724496f950045b39f7b04094cc32e08922b
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2005-01-18 20:53:46 +0000

    Added bounding-sphere and bounding-box display to OsgFox::Canvas

--- trunk/OsgTools/Box.h
+++ trunk/OsgTools/Box.h
@@ -20,14 +20,14 @@
 #include "MaterialSetter.h"
 
 #include "osg/Vec4"
+#include "osg/BoundingBox"
 #include "osg/Geode"
 #include "osg/Geometry"
 
 namespace OsgTools
 {
 
-  template<class ColorPolicy>
-  class Box : public ColorPolicyFunctor<ColorPolicy>
+  template < class ColorPolicy > class Box : public ColorPolicyFunctor < ColorPolicy >
   {
   public:
     typedef ColorPolicyFunctor<ColorPolicy> CPF;
@@ -38,18 +38,50 @@ namespace OsgTools
     {
     }
 
-    Box ( float w, float h, float d): CPF(), _width(w), _height(h), _depth(d) {}
-    Box ( float size, float detailRatio): CPF(), _width(size), _height(size), _depth(size) {}
-    Box ( const Box& b): CPF(b), _width(b._width), _height(b._height), _depth(b._depth) {}
-
-    virtual ~Box() {}
-
-    Box& operator = (const Box& b)
+    Box ( float w, float h, float d ) : CPF(),
+      _width  ( w ),
+      _height ( h ),
+      _depth  ( d )
     {
-      CPF::operator =(b);
-      _width=b._width;
-      _height=b._height;
-      _depth=b._depth;
+    }
+
+    Box ( float size, float detailRatio ) : CPF(),
+      _width  ( size ),
+      _height ( size ),
+      _depth  ( size )
+    {
+    }
+
+    Box ( const osg::BoundingBox &b ) : CPF(),
+      _width  ( 1.0f ), 
+      _height ( 1.0f ),
+      _depth  ( 1.0f )
+    {
+      if ( b.valid() )
+      {
+        _width  = b.xMax() - b.xMin();
+        _height = b.yMax() - b.yMin();
+        _depth  = b.zMax() - b.zMin();
+      }
+    }
+
+    Box ( const Box &b ) : CPF ( b ),
+      _width  ( b._width  ),
+      _height ( b._height ),
+      _depth  ( b._depth  )
+    {
+    }
+
+    virtual ~Box()
+    {
+    }
+
+    Box& operator = ( const Box& b )
+    {
+      CPF::operator = ( b );
+      _width  = b._width;
+      _height = b._height;
+      _depth  = b._depth;
       return *this;
     }
 
@@ -123,16 +155,17 @@ namespace OsgTools
 
     void set(float w,float h,float d) { _width = w; _height = h; _depth = d; }
 
-    void setDepth(float d) { _depth = d; }
+    void  setDepth ( float d ) { _depth = d; }
     float getDepth() const { return _depth; }
 
-    void setHeight(float h) { _height = h; }
+    void  setHeight ( float h ) { _height = h; }
     float getHeight() const { return _height; }
 
-    void setWidth(float w) { _width = w; }
+    void  setWidth ( float w ) { _width = w; }
     float getWidth() const { return _width; }
 
   private:
+
     float _width,_height,_depth;
   };
 

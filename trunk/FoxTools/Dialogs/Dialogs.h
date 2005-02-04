2d762b4ebce4fa05b5ef13902158a1324f2d512d
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2005-02-04 17:55:04 +0000

    FoxTools/Dialogs:   -Added dialog for prompting the user to save a file
    OsgFox:
      -Added Save under file menu
      -If document is unsaved, prompts user before exiting

---
+++ trunk/FoxTools/Dialogs/Dialogs.h
@@ -0,0 +1,59 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2005, Adam Kubach
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __FOXTOOLS_DIALOGS_H__
+#define __FOXTOOLS_DIALOGS_H__
+
+#include "fxver.h"
+#include "fxdefs.h"
+
+#include "FXString.h"
+#include "FXHash.h"
+#include "FXStream.h"
+#include "FXWindow.h"
+#include "FXMessageBox.h"
+
+#include <sstream>
+#include <string>
+
+namespace FoxTools {
+namespace Dialogs {
+
+
+inline int SaveDialog( FX::FXWindow* owner, const std::string& filename )
+{
+  std::ostringstream os;
+  os << filename << " is not saved.  Do you want to save?";
+
+  FX::FXMessageBox message ( owner, "Save File?", "Save File?", 0x0, FX::MBOX_YES_NO_CANCEL );
+  return message.warning ( owner, FX::MBOX_YES_NO_CANCEL, "Save File?", os.str().c_str() );
+}
+
+inline int MatrixDialog( FX::FXWindow* owner, FX::FXMatrix *matrix, const std::string& title, int layout )
+{
+  FX::FXDialogBox dialog ( owner, title.c_str(), layout ,0,0,0,0, 10,10,10,10, 4,4 );
+
+  FX::FXVerticalFrame* theFrame ( new FX::FXVerticalFrame ( &dialog, LAYOUT_FILL_X|LAYOUT_FILL_Y ) );
+
+  matrix->reparent( theFrame );
+
+  //Accept and cancel buttons
+  FX::FXHorizontalFrame*  buttonFrame ( new FX::FXHorizontalFrame ( theFrame, LAYOUT_FILL_X|LAYOUT_FILL_Y ) );
+  FX::FXButton *accept ( new FX::FXButton ( buttonFrame, "Accept", 0x0, &dialog, FX::FXDialogBox::ID_ACCEPT,FX::LAYOUT_LEFT|BUTTON_INITIAL|BUTTON_DEFAULT|FRAME_RAISED|FRAME_THICK,0,0,0,0, 20,20) );
+  FX::FXButton *cancel ( new FX::FXButton ( buttonFrame, "Cancel", 0x0, &dialog, FX::FXDialogBox::ID_CANCEL,FX::LAYOUT_RIGHT|BUTTON_INITIAL|BUTTON_DEFAULT|FRAME_RAISED|FRAME_THICK,0,0,0,0, 20,20) );
+
+  dialog.create();
+
+  return dialog.execute( FX::PLACEMENT_OWNER );
+}
+
+};
+};
+
+#endif

f8e6dae6598b581224c5698eb6eed480b7f8fb6a
rufeo <rufeo@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2005-02-15 15:46:31 +0000

    + corrected an addChild bug in simple_behavior/MenuController.cpp

--- trunk/MenuKit_examples/simple_behavior/create.h
+++ trunk/MenuKit_examples/simple_behavior/create.h
@@ -24,13 +24,21 @@ MenuKit::Menu* create_menu::operator()()
   MenuKit::Button::Ptr copy = new MenuKit::Button("Copy","");
   MenuKit::Button::Ptr paste = new MenuKit::Button("Paste","");
 
+  MenuKit::Button::Ptr solutionbrowser = new MenuKit::Button("Solution Browser","");
+  solutionbrowser->toggle( true );
+  MenuKit::Button::Ptr classbrowser = new MenuKit::Button("Class Browser","");
+  classbrowser->toggle( true );
+
+  MenuKit::Menu::Ptr toolbars = new MenuKit::Menu("Toolbars","",MenuKit::Menu::VERTICAL);
+  toolbars->append( solutionbrowser.get() );
+  toolbars->append( classbrowser.get() );
+
   MenuKit::Menu::Ptr file = new MenuKit::Menu("File","",MenuKit::Menu::VERTICAL);
   file->append( newfile.get() );
   file->append( open.get() );
   file->append( close.get() );
   file->append( spacer.get() );
   file->append( exit.get() );
-  file->expanded( true );
 
   MenuKit::Menu::Ptr edit = new MenuKit::Menu("Edit","",MenuKit::Menu::VERTICAL);
   edit->append( undo.get() );
@@ -38,10 +46,13 @@ MenuKit::Menu* create_menu::operator()()
   edit->append( copy.get() );
   edit->append( paste.get() );
 
+  MenuKit::Menu::Ptr view = new MenuKit::Menu("View","",MenuKit::Menu::VERTICAL);
+  view->append( toolbars.get() );
+
   MenuKit::Menu* top = new MenuKit::Menu("","",MenuKit::Menu::HORIZONTAL);
   top->append( file.get() );
   top->append( edit.get() );
-  top->expanded( true );
+  top->append( view.get() );
   return top;
 }
 

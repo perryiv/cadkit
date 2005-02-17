d64c6277e8987a6357f2dc5f5c5ec116d2dfa6d3
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2005-02-17 20:10:33 +0000

    OsgFox:   -Fixed problems with text matrices on canvas.   -Added interface for adding text to canvas.
    IMD:
      -Displays energy value during IMD.

---
+++ trunk/Usul/Interfaces/ITextMatrix.h
@@ -0,0 +1,46 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2005, Adam Kubach
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Interface for setting values in a text matrix
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _USUL_INTERFACE_TEXT_MATRIX_H_
+#define _USUL_INTERFACE_TEXT_MATRIX_H_
+
+#include "Usul/Interfaces/IUnknown.h"
+
+#include <string>
+
+namespace Usul {
+namespace Interfaces {
+
+
+struct ITextMatrix : public Usul::Interfaces::IUnknown
+{
+  /// Smart-pointer definitions.
+  USUL_DECLARE_QUERY_POINTERS ( ITextMatrix );
+
+  /// Id for this interface.
+  enum { IID = 1758536334u };
+
+  virtual void setText      ( float x, float y, unsigned int row, unsigned int col, const std::string& text ) = 0;
+  virtual void createMatrix ( float x, float y, unsigned int numRows, unsigned int numCols, int rowHeight, int columnWidth ) = 0;
+  virtual void removeMatrix ( float x, float y ) = 0;
+
+}; // class ITextMatrix
+
+
+}; // namespace Interfaces
+}; // namespace Usul
+
+
+#endif // _USUL_INTERFACE_TEXT_MATRIX_H_

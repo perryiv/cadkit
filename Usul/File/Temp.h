1236d20cd4e1cd2d8f4cc596dfd67e0967b75e84
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2005-03-02 22:10:37 +0000

    - Adding Usul::File::Temp, for working with temporary files. - Added an appropriate update message-map for File->Save. It checks for an   active document that needs to be saved.

---
+++ Usul/File/Temp.h
@@ -0,0 +1,71 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2002, Perry L. Miller IV
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Temporary file.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _USUL_FILE_TEMPORARY_H_
+#define _USUL_FILE_TEMPORARY_H_
+
+#include "Usul/Export/Export.h"
+
+#include <string>
+#include <iosfwd>
+
+
+namespace Usul {
+namespace File {
+
+
+class USUL_EXPORT Temp
+{
+public:
+
+  // Flags for the output stream.
+  enum Format { ASCII, BINARY };
+
+  // Constructor. Throws if fails to open temporary file.
+  Temp ( Temp::Format f = Temp::ASCII );
+
+  // Destructor. Should never throw.
+  ~Temp();
+
+  // Return name of internal temporary file.
+  const std::string &   name() const;
+
+  // Rename the file. This closes the stream first.
+  void                  rename ( const std::string &name );
+
+  // Access the output stream.
+  std::ostream &        stream();
+
+protected:
+
+  // No copying.
+  Temp ( const Temp & );
+  Temp &operator = ( const Temp & );
+
+  void                  _close();
+
+private:
+
+  std::string _name;
+  std::ostream *_stream;
+  bool _remove;
+};
+
+
+}; // namespace File
+}; // namespace Usul
+
+
+#endif // _USUL_FILE_TEMPORARY_H_

b17f969ad8efd65276744163566bbf29ec96a5b6
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2005-04-18 13:16:58 +0000

    -Added camera tab to change the center, distance, and rotation of the  trackball.

--- FoxTools/Headers/ScrollPane.h
+++ FoxTools/Headers/ScrollPane.h
@@ -1,19 +1,20 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2005, Perry L. Miller IV
+//  Copyright (c) 2005, Adam Kubach
 //  All rights reserved.
 //  BSD License: http://www.opensource.org/licenses/bsd-license.html
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-#ifndef __FOX_TOOLS_REAL_SPINNER_HEADERS_H__
-#define __FOX_TOOLS_REAL_SPINNER_HEADERS_H__
+#ifndef __FOX_TOOLS_SCROLL_BAR_HEADERS_H__
+#define __FOX_TOOLS_SCROLL_BAR_HEADERS_H__
+
 
 #include "FoxTools/Headers/Definitions.h"
 
-#include "FXArrowButton.h"
-#include "FXRealSpinner.h"
+#include "FXScrollPane.h"
 
 
-#endif // __FOX_TOOLS_REAL_SPINNER_HEADERS_H__
+#endif // __FOX_TOOLS_SCROLL_BAR_HEADERS_H__
+ 

6619661fa6f44688335f99b4df75d83eb3f96f6e
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2005-05-07 00:15:03 +0000

    -Adding clipping planes

---
+++ trunk/Usul/Interfaces/IClippingPlanes.h
@@ -0,0 +1,40 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2005, Adam Kubach
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __USUL_INTERFACES_CLIPPING_PLANES_H__
+#define __USUL_INTERFACES_CLIPPING_PLANES_H__
+
+#include "Usul/Interfaces/IUnknown.h"
+
+
+namespace Usul {
+namespace Interfaces {
+
+struct IClippingPlanes : public Usul::Interfaces::IUnknown
+{
+  /// Smart-pointer definitions.
+  USUL_DECLARE_QUERY_POINTERS ( IClippingPlanes );
+
+  /// Id for this interface.
+  enum { IID = 2541716112u };
+
+  // Add/Remove clipping plane
+  virtual void                  addClippingPlane ( ) = 0;
+  virtual void                  removeClippingPlane ( unsigned int ) = 0;
+
+  // Get the number of clipping planes in the scene
+  virtual unsigned int          numClippingPlanes () = 0;
+
+}; // struct IClippingPlanes
+
+}
+}
+
+#endif // __USUL_INTERFACES_CLIPPING_PLANES_H__
+

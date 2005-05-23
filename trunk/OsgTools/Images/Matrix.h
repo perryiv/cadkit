7b6aba548afd5c96f3ca1eb64a1e69fc35efa2b7
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2005-05-23 15:50:26 +0000

    -Added image processing functions to OsgTools
    -Added plugins to work with image processing additions

---
+++ trunk/OsgTools/Images/Matrix.h
@@ -0,0 +1,52 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2005, Adam Kubach
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __OSG_TOOLS_IMAGES_MATRIX_H__
+#define __OSG_TOOLS_IMAGES_MATRIX_H__
+
+#include <vector>
+
+namespace OsgTools {
+  namespace Images {
+
+template < class T >
+class Matrix
+{
+public:
+  Matrix ( unsigned int r, unsigned int c ) :
+    _r ( r ),
+    _c ( c ),
+    _array ( r * c )
+  {
+  }
+
+  T& at ( unsigned int r, unsigned int c )
+  {
+    return _array.at( r * _r + c );
+  }
+
+  const T& at ( unsigned int r, unsigned int c ) const
+  {
+    return _array.at( r * _r + c );
+  }
+
+  unsigned int rows () const { return _r; }
+
+  unsigned int columns () const { return _c; }
+
+private:
+  unsigned int _r;
+  unsigned int _c;
+  std::vector < T > _array;
+};
+
+  }
+}
+
+#endif // __OSG_TOOLS_IMAGES_MATRIX_H__

a4eccc6705a353d738a5abbe1152c0fd56028c9d
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2005-06-02 01:14:55 +0000

    -Added image model
    -Added image child window
    
    -Added OsgDocument

---
+++ trunk/FoxTools/Headers/ImageView.h
@@ -0,0 +1,21 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2005, Perry L. Miller IV
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __FOX_TOOLS_IMAGE_VIEW_HEADERS_H__
+#define __FOX_TOOLS_IMAGE_VIEW_HEADERS_H__
+
+
+#include "FoxTools/Headers/Definitions.h"
+
+#include "FXHash.h"
+#include "FXStream.h"
+#include "FXImageView.h"
+
+
+#endif // __FOX_TOOLS_IMAGE_VIEW_HEADERS_H__

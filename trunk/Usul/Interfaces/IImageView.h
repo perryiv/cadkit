7bf31f401b8dd8bd0d0fc2a5a61bdb9760edaf4d
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2005-06-07 11:04:46 +0000

    -Combined MdiGLChildWindow and MdiImageChildWindow into MdiChildWindow

--- trunk/Usul/Interfaces/IImageView.h
+++ trunk/Usul/Interfaces/IImageView.h
@@ -9,42 +9,37 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-//  Interface for getting the view
+//  Interface for getting the image view
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-#ifndef _USUL_INTERFACE_VIEWER_H_
-#define _USUL_INTERFACE_VIEWER_H_
+#ifndef _USUL_INTERFACE_IMAGE_VIEW_H_
+#define _USUL_INTERFACE_IMAGE_VIEW_H_
 
 #include "Usul/Interfaces/IUnknown.h"
+
+namespace OsgFox { namespace Views { class ImageView; }  }
 
 namespace Usul {
 namespace Interfaces {
 
 
-struct IViewer : public Usul::Interfaces::IUnknown
+struct IImageView : public Usul::Interfaces::IUnknown
 {
   /// Smart-pointer definitions.
-  USUL_DECLARE_QUERY_POINTERS ( IViewer );
+  USUL_DECLARE_QUERY_POINTERS ( IImageView );
 
   /// Id for this interface.
-  enum { IID = 3739984930u };
+  enum { IID = 2325881488u };
 
-  virtual void render() = 0;
+  virtual OsgFox::Views::ImageView*       imageView() = 0;
+  virtual const OsgFox::Views::ImageView* imageView() const = 0;
 
-  virtual void clear() = 0;
-
-  // These may be moved to own interface
-  virtual int x() = 0;
-  virtual int y() = 0;
-  virtual int height() = 0;
-  virtual int width() = 0;
-
-}; // struct IViewer
+}; // struct IImageView
 
 
-} // namespace Interfaces
-} // namespace Usul
+}; // namespace Interfaces
+}; // namespace Usul
 
 
-#endif // _USUL_INTERFACE_VIEWER_H_
+#endif // _USUL_INTERFACE_IMAGE_VIEW_H_

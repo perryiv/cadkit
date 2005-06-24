3b53f79426d79477c47d5bda3d7bf20180fd5763
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2005-06-24 20:22:01 +0000

    -Move Preferences to Core -Core Preferences now uses Usul::Shared::Preferences -Preferences no longer dependent on a view

--- trunk/Usul/Shared/Preferences.h
+++ trunk/Usul/Shared/Preferences.h
@@ -30,16 +30,40 @@ class USUL_EXPORT Preferences
 {
 public:
 
+  // Typedefs.
+  typedef std::map < std::string, bool >          Bools;
+  typedef std::map < std::string, int >           Ints;
+  typedef std::map < std::string, unsigned int >  Uints;
+  typedef std::map < std::string, float >         Floats;
+  typedef std::map < std::string, double >        Doubles;
+  typedef std::map < std::string, std::string >   Strings;
+
   // Get the value.
   bool                  getBool   ( const std::string &key );
   const std::string &   getString ( const std::string &key );
+  int                   getInt    ( const std::string &key );
+  unsigned int          getUint   ( const std::string &key );
+  float                 getFloat  ( const std::string &key );
+  double                getDouble ( const std::string &key );
 
   // Return the single instance.
   static Preferences &  instance();
 
   // Set the value.
-  void                  setBool   ( const std::string &key, bool value );
+  void                  setBool   ( const std::string &key, bool               value );
   void                  setString ( const std::string &key, const std::string &value );
+  void                  setInt    ( const std::string &key, int                value );
+  void                  setUint   ( const std::string &key, unsigned int       value );
+  void                  setFloat  ( const std::string &key, float              value );
+  void                  setDouble ( const std::string &key, double             value );
+
+  // Get the container.
+  const Bools&          getBools   ( ) const { return _bools;   }
+  const Strings&        getStrings ( ) const { return _strings; }
+  const Ints&           getInts    ( ) const { return _ints;    }
+  const Uints&          getUints   ( ) const { return _uints;   }
+  const Floats&         getFloats  ( ) const { return _floats;  }
+  const Doubles&        getDoubles ( ) const { return _doubles; }
 
 private:
 
@@ -51,8 +75,13 @@ private:
   Preferences &operator = ( const Preferences & );
   Preferences ( const Preferences & );
 
-  std::map < std::string, bool >        _bools;
-  std::map < std::string, std::string > _strings;
+  Bools    _bools;
+  Ints     _ints;
+  Uints    _uints;
+  Floats   _floats;
+  Doubles  _doubles;
+  Strings  _strings;
+  
 };
 
 

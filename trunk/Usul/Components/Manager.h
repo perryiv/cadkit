e60980e56c708f6748877c1c677c7ee218070b76
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2005-06-24 20:53:30 +0000

    - Adding convenience functions to Usul::Components::Manager in order to   easily use plugins in command-line apps. - Repaired some merge-related compile errors.

--- trunk/Usul/Components/Manager.h
+++ trunk/Usul/Components/Manager.h
@@ -36,28 +36,32 @@ public:
   typedef std::set < IUnknown::ValidRefPtr >  UnknownSet;
   typedef std::list < std::string > Strings;
 
-  static Manager* instance();
+  static Manager& instance();
 
-  void load  ( unsigned long iid, const std::list<std::string> &plugins, bool keepGoingIfException = true );
-  void clear ();
+  void                          clear();
 
-  //do we have any unknowns?
-  bool empty () const { return _unknowns.empty(); }
+  // Do we have any unknowns?
+  bool                          empty () const { return _unknowns.empty(); }
+
+  // Load the plugins.
+  void                          load ( unsigned long iid, const std::string &dir, const std::string &ext, bool keepGoingIfException = true );
+  void                          load ( unsigned long iid, const Strings &plugins, bool keepGoingIfException = true );
 
   // Return list of plugin names. This queries each unknown pointer for IPlugin.
-  Strings names() const;
+  Strings                       names() const;
 
   //return the unknowns
-  UnknownSet& unknowns ()  { return _unknowns; }
+  UnknownSet&                   unknowns() { return _unknowns; }
 
   //Get a single IUnknown
-  Usul::Interfaces::IUnknown* getInterface( unsigned long iid );
+  Usul::Interfaces::IUnknown*   getInterface( unsigned long iid );
 
   //Get a set of IUnknowns
-  UnknownSet                  getInterfaces( unsigned long iid );
-  UnknownSet                  getInterfaces( unsigned long iid1, unsigned long iid2 );
+  UnknownSet                    getInterfaces( unsigned long iid );
+  UnknownSet                    getInterfaces( unsigned long iid1, unsigned long iid2 );
 
 private:
+
   typedef UnknownSet::iterator UnknownItr;
   typedef Usul::Interfaces::IClassFactory Factory;
 

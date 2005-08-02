0853942a2b34bf54f0a154507aff92a6563bd15c
imikejackson <imikejackson@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2005-08-02 11:46:59 +0000

    The Clock class abstracts the process of obtaining a millisecond count from the system.

---
+++ trunk/Usul/System/Clock.h
@@ -0,0 +1,31 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2005, Mike Jackson
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Class that represents a screen.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _USUL_SYSTEM_CLOCK_CLASS_H_
+#define _USUL_SYSTEM_CLOCK_CLASS_H_
+
+#include "Usul/Export/Export.h"
+#include "Usul/Types/Types.h"
+
+namespace Usul {
+  namespace System {
+    
+    Usul::Types::Uint64 milliseconds();
+    
+  }; // namespace System
+}; // namespace Usul
+
+
+#endif // _USUL_SYSTEM_CLOCK_CLASS_H_

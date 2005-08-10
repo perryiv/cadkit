6c00d7db4c366867af213b27c37c374949676a24
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2005-08-10 19:16:43 +0000

    -Fixed bug that caused an exception thrown when running keep all  connected in smooth shading. -Fixed bug that caused normals to disappear when running keep all  connected in flat shading and then switching to smooth shading. -Individual display lists are updated after a triangle is added. -Fixed bug that would cause long skinny triangles to not be intersected.

--- OsgTools/Triangles/Partition.h
+++ OsgTools/Triangles/Partition.h
@@ -31,52 +31,60 @@ public:
   Partition( );
 
   // Clear the partition.
-  void clear();
+  void                      clear();
 
-  // Add a triangle
-  void add ( Triangle *, const osg::Vec3Array &vertices, const osg::Vec3& n );
+  // Add a triangle.
+  void                      add ( Triangle *, const osg::Vec3Array &vertices, const osg::Vec3& n );
 
   // Get the vertex index of the i'th triangle in the drawable.
-  unsigned int index0 ( const osg::Drawable *d, unsigned int i ) const;
-  unsigned int index1 ( const osg::Drawable *d, unsigned int i ) const;
-  unsigned int index2 ( const osg::Drawable *d, unsigned int i ) const;
-
-  // Remove a triangle.
-  void remove ( const osg::Drawable*, unsigned int index );
-
-  // Reserve enough room.
-  void reserve ( unsigned int );
-
-  // Get the shared vertex of the i'th triangle in the drawable.
-  SharedVertex* sharedVertex0 ( const osg::Drawable *d, unsigned int i );
-  SharedVertex* sharedVertex1 ( const osg::Drawable *d, unsigned int i );
-  SharedVertex* sharedVertex2 ( const osg::Drawable *d, unsigned int i );
-
-  // Get the shared vertex of the i'th triangle in the drawable.
-  const SharedVertex* sharedVertex0 ( const osg::Drawable *d, unsigned int i ) const;
-  const SharedVertex* sharedVertex1 ( const osg::Drawable *d, unsigned int i ) const;
-  const SharedVertex* sharedVertex2 ( const osg::Drawable *d, unsigned int i ) const;
-
-  // Subdivide the partition.  Needs to be called before triangles are added.
-  void subdivide ( const osg::BoundingBox&, unsigned int times );
+  unsigned int              index0 ( const osg::Drawable *d, unsigned int i ) const;
+  unsigned int              index1 ( const osg::Drawable *d, unsigned int i ) const;
+  unsigned int              index2 ( const osg::Drawable *d, unsigned int i ) const;
 
   // Build the node.
-  osg::Node* operator () ( osg::Vec3Array *vertices, osg::Vec3Array *normals, bool average );
+  osg::Node*                operator () ( osg::Vec3Array *vertices, osg::Vec3Array *normals, bool average );
 
+  // Purge any excess memory.
+  void                      purge ();
+
+  // Remove a triangle.
+  void                      remove ( const osg::Drawable*, unsigned int index );
+
+  // Reserve enough room.
+  void                      reserve ( unsigned int );
+
+  // Get the shared vertex of the i'th triangle in the drawable.
+  SharedVertex*             sharedVertex0 ( const osg::Drawable *d, unsigned int i );
+  SharedVertex*             sharedVertex1 ( const osg::Drawable *d, unsigned int i );
+  SharedVertex*             sharedVertex2 ( const osg::Drawable *d, unsigned int i );
+
+  // Get the shared vertex of the i'th triangle in the drawable.
+  const SharedVertex*       sharedVertex0 ( const osg::Drawable *d, unsigned int i ) const;
+  const SharedVertex*       sharedVertex1 ( const osg::Drawable *d, unsigned int i ) const;
+  const SharedVertex*       sharedVertex2 ( const osg::Drawable *d, unsigned int i ) const;
+
+  // Subdivide the partition.  Needs to be called before triangles are added.
+  void                      subdivide ( const osg::BoundingBox&, unsigned int times );
 
 private:
   
   // Get primitive set index for given vertices.
-  unsigned int       _index ( const osg::Vec3& v0, const osg::Vec3& v1, const osg::Vec3& v2 );
+  unsigned int              _index ( const osg::Vec3& v0, const osg::Vec3& v1, const osg::Vec3& v2 );
 
-  unsigned int       _indexn ( const osg::Drawable *d, unsigned int i, unsigned int n ) const;
+  // Get the n'th index of triangle i in the drawable.
+  unsigned int              _indexn ( const osg::Drawable *d, unsigned int i, unsigned int n ) const;
 
-  void               _divide ( const osg::BoundingBox &bb, osg::BoundingBox &bb1, osg::BoundingBox &bb2 );
-  void               _subdivide ( const osg::BoundingBox&, unsigned int times );
+  // Divide the bounding box into two along the longest axis.
+  void                      _divide ( const osg::BoundingBox &bb, osg::BoundingBox &bb1, osg::BoundingBox &bb2 );
 
-  Triangle*          _triangle( const osg::Drawable *d, unsigned int i );
-  const Triangle*    _triangle( const osg::Drawable *d, unsigned int i ) const;
+  // Subdivide the given bounding box n number of times.
+  void                      _subdivide ( const osg::BoundingBox&, unsigned int times );
 
+  // Get the i'th triangle in the given drawable.
+  Triangle*                 _triangle( const osg::Drawable *d, unsigned int i );
+  const Triangle*           _triangle( const osg::Drawable *d, unsigned int i ) const;
+
+  // Typedefs.
   typedef std::vector < osg::BoundingBox > Cubes;
   typedef osg::DrawElementsUInt            DrawElements;
   typedef osg::ref_ptr < DrawElements >    PrimitiveSetPtr;

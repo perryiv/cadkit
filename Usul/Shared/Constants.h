292802fe14938a7cda8bf5b2d2fee8d639f70ab5
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2005-09-02 03:01:21 +0000

    Adding new file of constants.

---
+++ Usul/Shared/Constants.h
@@ -0,0 +1,43 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2002, Perry L Miller IV
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Constant values.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _USUL_SHARED_CONSTANT_VALUES_H_
+#define _USUL_SHARED_CONSTANT_VALUES_H_
+
+#include <string>
+
+
+namespace Usul {
+namespace Shared {
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Constant strings used for preferences, registry keys, etc.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+const std::string ALLOW_SPIN        ( "allow_spin"      );
+const std::string ANIMATION_TIME    ( "animation_time"  );
+const std::string DISPLAY_LISTS     ( "display_lists"   );
+const std::string LOW_LODS          ( "low_lods"        );
+const std::string HIGH_LODS         ( "high_lods"       );
+
+
+}; // namespace Shared
+}; // namespace Usul
+
+
+#endif //_USUL_SHARED_CONSTANT_VALUES_H_

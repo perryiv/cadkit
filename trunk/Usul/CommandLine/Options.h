d23cff12216570c1245b82db521c698d5a2bdefb
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2005-12-07 01:07:04 +0000

    -Adding parsing of command line options.

---
+++ trunk/Usul/CommandLine/Options.h
@@ -0,0 +1,56 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2005, Adam Kubach
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Options given on the command line.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef __USUL_COMMAND_LINE_OPTIONS_H__
+#define __USUL_COMMAND_LINE_OPTIONS_H__
+
+
+#include <map>
+#include <string>
+
+namespace Usul {
+namespace CommandLine {
+
+class Options
+{
+ public:
+  typedef std::map < std::string, std::string > Map;
+  typedef Map::iterator iterator;
+
+  Options();
+
+  void insert ( const std::string&, const std::string& );
+
+  // Does this option exist?
+  bool option ( const std::string& ) const;
+
+  // Get the value for the option.
+  std::string value ( const std::string& ) const;
+
+  iterator begin() { return _options.begin(); }
+  iterator end() { return _options.end(); }
+
+ private:
+  Map _options;
+};
+
+}
+}
+
+
+
+#endif //__USUL_COMMAND_LINE_OPTIONS_H__
+

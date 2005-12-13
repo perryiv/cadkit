e95b1e2d3110519f0bcd7104ad7bdc4608df8b9f
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2005-12-13 02:10:48 +0000

    -Accidently committed changes to comments to test HeaderDoc.

--- trunk/Usul/Interfaces/IUnknown.h
+++ trunk/Usul/Interfaces/IUnknown.h
@@ -13,11 +13,6 @@
 ///
 ///////////////////////////////////////////////////////////////////////////////
 
-/*!
-  @class IUnknown
-  @abstract Base class for all other interfaces.
-*/
-
 #ifndef _USUL_INTERFACE_UNKNOWN_H_
 #define _USUL_INTERFACE_UNKNOWN_H_
 
@@ -30,35 +25,19 @@ namespace Interfaces {
 
 struct IUnknown
 {
-  public:
   /// Smart-pointer definitions.
   USUL_DECLARE_QUERY_POINTERS ( IUnknown );
 
-  /*!
-    @enum Id for this interface.
-    @abstract Unique ID for this interface
-    @discussion Unique ID for this interface
-   */
+  /// Id for this interface.
   enum { IID = 1075271105 };
 
-  /*!
-    @function queryInterface
-    @abstract See if the interface is supported
-    @param iid Unique ID to query for
-    @result An IUnknown that implements given ID.
-   */
+  /// See if the interface is supported.
   virtual IUnknown *      queryInterface ( unsigned long iid ) = 0;
 
-  /*!
-    @function ref
-    @abstract Reference the interface.
-   */
+  /// Reference the interface.
   virtual void            ref() = 0;
 
-  /*!
-    @function unref
-    @abstract Unreference the interface.
-   */
+  /// Unreference the interface.
   virtual void            unref ( bool allowDeletion = true ) = 0;
 };
 
@@ -67,12 +46,11 @@ struct IUnknown
 }; // namespace Usul
 
 
-
-/*!
-  @defined USUL_DECLARE_IUNKNOWN_MEMBERS
-  @discussion Macro to declare IUnknown member functions.
-*/
-
+///////////////////////////////////////////////////////////////////////////////
+///
+///  Macro to declare IUnknown member functions.
+///
+///////////////////////////////////////////////////////////////////////////////
 
 #define USUL_DECLARE_IUNKNOWN_MEMBERS \
   virtual Usul::Interfaces::IUnknown *  queryInterface ( unsigned long iid ); \
@@ -80,10 +58,12 @@ struct IUnknown
   virtual void                          unref ( bool allowDeletion = true )
 
 
-/*!
-  @defined USUL_IMPLEMENT_IUNKNOWN_MEMBERS
-  @discussion Macro to implement IUnknown member functions (except queryInterface).  Typically the base class will be Usul::Base::Referenced.
-*/
+///////////////////////////////////////////////////////////////////////////////
+///
+///  Macro to implement IUnknown member functions (except queryInterface).
+///  Typically the base class will be Usul::Base::Referenced.
+///
+///////////////////////////////////////////////////////////////////////////////
 
 #define USUL_IMPLEMENT_IUNKNOWN_MEMBERS(class_name,base_class) \
   void class_name::ref()                        { base_class::ref(); } \

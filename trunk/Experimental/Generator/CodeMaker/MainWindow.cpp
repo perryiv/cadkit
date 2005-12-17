948e0a9f59ae30973283747a2001bb0830173a31
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2005-12-17 07:36:02 +0000

    Working on code generator. Have it loading tab-page from XML file. Wires are not hooked up yet.

--- trunk/Experimental/Generator/CodeMaker/MainWindow.cpp
+++ trunk/Experimental/Generator/CodeMaker/MainWindow.cpp
@@ -17,6 +17,7 @@
 #include "MainWindow.h"
 #include "Application.h"
 
+#include "Usul/CommandLine/Arguments.h"
 #include "Usul/System/Screen.h"
 
 IMPLEMENT_CLASS ( MainWindow, MainWindow::BaseClass );
@@ -43,6 +44,26 @@ MainWindow::MainWindow() : BaseClass ( 0x0, wxID_ANY, "Code Maker",
   wxPoint ( Usul::System::Screen::width() * 0.1, Usul::System::Screen::height() * 0.1 ),
   wxSize  ( Usul::System::Screen::width() * 0.8, Usul::System::Screen::height() * 0.8 ) )
 {
+  // One big split window.
+  wxSplitterWindow *splitter ( new wxSplitterWindow ( this, wxID_ANY ) );
+  splitter->SetSashGravity ( 0.5 );
+
+  // Load left panel.
+  const std::string leftPanelFile ( Usul::CommandLine::Arguments::instance().directory() + "/LeftPanel.xrc" );
+  wxXmlResource::Get()->Load ( leftPanelFile.c_str() );
+  wxPanel *leftPanel ( wxXmlResource::Get()->LoadPanel ( splitter, "ID_LEFT_PANEL" ) );
+
+  // Make right panel.
+  wxPanel *rightPanel ( new wxPanel ( splitter, wxID_ANY ) );
+
+  // Tell splitter what to split.
+  splitter->SplitVertically ( leftPanel, rightPanel, 0 );
+
+  // Right half.
+  wxTextCtrl *rightText ( new wxTextCtrl ( rightPanel, wxID_ANY, "Right Text" ) );
+  wxSizer *rightSizer = new wxBoxSizer ( wxHORIZONTAL );
+  rightSizer->Add ( rightText, 1, wxGROW | wxALL, 0 );
+  rightPanel->SetSizer ( rightSizer );
 }
 
 

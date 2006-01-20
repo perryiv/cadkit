e84e28ae97a364dd1888b5025847c740aecc3fb9
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2006-01-20 05:17:57 +0000

    Added documentation for curve and surface point evaluation.

--- GenericNurbs/Docs/Web/nav_bar.php
+++ GenericNurbs/Docs/Web/nav_bar.php
@@ -10,6 +10,7 @@ main_navigation_bar_entry ( "algorithms.php",    "Algorithms",  $nav_bar_vertica
 main_navigation_bar_entry ( "create.php",        "Create",      $nav_bar_vertical_space, $nav_bar_current_cell_color, $nav_bar_current_text_color, $url );
 main_navigation_bar_entry ( "download.php",      "Download",    $nav_bar_vertical_space, $nav_bar_current_cell_color, $nav_bar_current_text_color, $url );
 main_navigation_bar_entry ( "doxygen.php",       "Doxygen",     $nav_bar_vertical_space, $nav_bar_current_cell_color, $nav_bar_current_text_color, $url );
+main_navigation_bar_entry ( "evaluate.php",      "Evaluate",    $nav_bar_vertical_space, $nav_bar_current_cell_color, $nav_bar_current_text_color, $url );
 main_navigation_bar_entry ( "inheritance.php",   "Inheritance", $nav_bar_vertical_space, $nav_bar_current_cell_color, $nav_bar_current_text_color, $url );
 main_navigation_bar_entry ( "library.php",       "Library",     $nav_bar_vertical_space, $nav_bar_current_cell_color, $nav_bar_current_text_color, $url );
 main_navigation_bar_entry ( "license.php",       "License",     $nav_bar_vertical_space, $nav_bar_current_cell_color, $nav_bar_current_text_color, $url );

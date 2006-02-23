8e2c1335c987f2a630cb5b3f89cd0eed673e9138
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2006-02-23 23:43:55 +0000

    -Updating Visual Studio 8 files

---
+++ Experimental/VTKTools/Commands/Progress.cpp
@@ -0,0 +1,16 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2006, Adam Kubach
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include "Progress.h"
+
+using namespace VTKTools::Commands;
+
+void 	Progress::Execute (vtkObject *caller, unsigned long eventId, void *callData)
+{
+}

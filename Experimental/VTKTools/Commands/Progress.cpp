fcee4e97915373835e10e3fd75877d5ab695f482
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2006-03-10 12:45:59 +0000

    -Adding new document for faster loading of Paradis files.

--- Experimental/VTKTools/Commands/Progress.cpp
+++ Experimental/VTKTools/Commands/Progress.cpp
@@ -11,6 +11,28 @@
 
 using namespace VTKTools::Commands;
 
-void 	Progress::Execute (vtkObject *caller, unsigned long eventId, void *callData)
+Progress::Progress() :
+_progress ( 0x0 )
 {
+}
+
+Progress::~Progress()
+{
+}
+
+
+void Progress::progressBar( Usul::Interfaces::IProgressBar* progressBar )
+{
+  _progress = progressBar;
+}
+
+void Progress::Execute ( vtkObject *caller, unsigned long eventId, void *data )
+{
+  if( eventId == vtkCommand::ProgressEvent )
+  {
+    double *progress ( reinterpret_cast < double * > ( data ) );
+
+    if ( _progress.valid() )
+      _progress->updateProgressBar ( *progress * 100 );
+  }
 }

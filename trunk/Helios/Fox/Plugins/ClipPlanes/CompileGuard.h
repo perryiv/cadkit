9fafa7d0b6a43b41f6f0efcd6da8211127eac7bb
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2006-04-14 08:30:28 +0000

    -Adding more plugins to Helios Module

---
+++ trunk/Helios/Fox/Plugins/ClipPlanes/CompileGuard.h
@@ -0,0 +1,24 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2004, Adam Kubach
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  This header file should not be included in client code.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _CLIP_PLANES_COMPILE_GUARD_H_
+#define _CLIP_PLANES_COMPILE_GUARD_H_
+
+#ifndef _COMPILING_CLIP_PLANES
+# error "This header file is only used for compiling the library."
+# error "It should not be included in client code."
+#endif
+
+#endif // _VIEW_SEEK_COMPILE_GUARD_H_
---
+++ trunk/Helios/Fox/Plugins/EditPolygon/GroupPrimitives.h
@@ -0,0 +1,57 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2005, Adam Kubach
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _EDIT_POLYGONS_GROUP_PRIMITIVES_H_
+#define _EDIT_POLYGONS_GROUP_PRIMITIVES_H_
+
+#include "FoxTools/Headers/Object.h"
+
+#include "Usul/Base/Referenced.h"
+#include "Usul/Pointers/Pointers.h"
+#include "Usul/Interfaces/IUnknown.h"
+
+namespace OsgFox {
+namespace Plugins {
+namespace EditPolygons {
+
+class GroupPrimitives : public Usul::Base::Referenced
+{
+public:
+
+  /// Typedefs.
+  typedef Usul::Base::Referenced BaseClass;
+  typedef Usul::Interfaces::IUnknown Unknown;
+
+  /// Smart-pointer definitions.
+  USUL_DECLARE_REF_POINTERS ( GroupPrimitives );
+
+  GroupPrimitives ( Usul::Interfaces::IUnknown *mainWindow );
+
+  virtual void execute ( Usul::Interfaces::IUnknown *caller );
+
+ 
+protected:
+  //For fox do not use
+  GroupPrimitives () { }
+
+  // Use ref counting
+  virtual ~GroupPrimitives();
+
+  Usul::Interfaces::IUnknown::ValidAccessQueryPtr _mainWindow;
+
+
+};
+
+
+} //namespace EditPolygons
+} //namespace Plugins
+} //namespace OsgFox
+
+
+#endif // _EDIT_POLYGONS_GROUP_PRIMITIVES_H_

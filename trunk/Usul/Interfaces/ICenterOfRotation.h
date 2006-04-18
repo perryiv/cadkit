f584c4de10ee136ef79e5f7d05125607ac2443ce
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2006-04-18 02:18:44 +0000

    -Adding missing interfaces

---
+++ trunk/Usul/Interfaces/ICenterOfRotation.h
@@ -0,0 +1,43 @@
+﻿
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2005, Adam Kubach
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Hide/Show the center of rotation.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __USUL_INTERFACES_ICENTEROFROTATION_H__
+#define __USUL_INTERFACES_ICENTEROFROTATION_H__
+
+#include "Usul/Interfaces/IUnknown.h"
+
+namespace Usul {
+namespace Interfaces {
+
+
+struct ICenterOfRotation : public Usul::Interfaces::IUnknown
+{
+  /// Smart-pointer definitions.
+  USUL_DECLARE_QUERY_POINTERS ( ICenterOfRotation );
+
+  /// Id for this interface.
+  enum { IID = 1376266839u };
+
+  virtual void showCenterOfRotation ( bool b ) = 0;
+  virtual bool showCenterOfRotation ( ) const = 0;
+
+}; // struct ICenterOfRotation
+
+
+} // end namespace Interfaces
+} // end namespace Usul
+
+
+#endif /* __USUL_INTERFACES_ICENTEROFROTATION_H__ */

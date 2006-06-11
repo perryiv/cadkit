7890972c954b2338152263cba662701116d44144
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2006-06-11 18:28:13 +0000

    Moving assembly info file to top-level directory.

--- trunk/DotNet/Persistence/Form.cs
+++ trunk/DotNet/Persistence/Form.cs
@@ -7,48 +7,45 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-namespace CadKit
+namespace CadKit.Persistence
 {
-  namespace Persistence
+  public class Form : System.Windows.Forms.Form
   {
-    public class Form : System.Windows.Forms.Form
+    public Form()
     {
-      public Form()
-      {
-        this.Load += _formLoaded;
-        this.FormClosing += _formClosing;
-      }
-
-      private void _formClosing( object sender, System.Windows.Forms.FormClosingEventArgs e )
-      {
-        if ( false == _hasWritten )
-        {
-          CadKit.Persistence.FormConfig.writeRect( this, this.PersistentName );
-          _hasWritten = true;
-        }
-      }
-
-      private void _formLoaded( object sender, System.EventArgs e )
-      {
-        CadKit.Persistence.FormConfig.readRect( this, this.PersistentName );
-
-        // Catch when owner windows are closed.
-        System.Windows.Forms.Form owner = this.Owner;
-        while ( null != owner )
-        {
-          owner.FormClosing += _formClosing;
-          owner = owner.Owner;
-        }
-      }
-
-      public string PersistentName
-      {
-        get { return _persistentName; }
-        set { _persistentName = value; }
-      }
-
-      private string _persistentName = typeof( Form ).ToString();
-      bool _hasWritten = false;
+      this.Load += _formLoaded;
+      this.FormClosing += _formClosing;
     }
+
+    private void _formClosing( object sender, System.Windows.Forms.FormClosingEventArgs e )
+    {
+      if ( false == _hasWritten )
+      {
+        CadKit.Persistence.FormConfig.writeRect( this, this.PersistentName );
+        _hasWritten = true;
+      }
+    }
+
+    private void _formLoaded( object sender, System.EventArgs e )
+    {
+      CadKit.Persistence.FormConfig.readRect( this, this.PersistentName );
+
+      // Catch when owner windows are closed.
+      System.Windows.Forms.Form owner = this.Owner;
+      while ( null != owner )
+      {
+        owner.FormClosing += _formClosing;
+        owner = owner.Owner;
+      }
+    }
+
+    public string PersistentName
+    {
+      get { return _persistentName; }
+      set { _persistentName = value; }
+    }
+
+    private string _persistentName = typeof( Form ).ToString();
+    bool _hasWritten = false;
   }
 }

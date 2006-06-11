81ec8ac797f045431740f453753d0b61981e5670
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2006-06-11 18:32:14 +0000

    Adding new dot-net version of Helios. Also adding a C# plugin manager.

---
+++ DotNet/Helios/Lib/Application.cs
@@ -0,0 +1,85 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2006, Perry L Miller IV
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+namespace CadKit.Helios.Lib
+{
+  public partial class Application
+  {
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    private Application()
+    {
+    }
+
+    /// <summary>
+    /// Single instance.
+    /// </summary>
+    public static Application instance()
+    {
+      if ( null == _instance )
+        _instance = new Application();
+      return _instance;
+    }
+
+    /// <summary>
+    /// Set/get the name of the program. Gets set from the assembly (program) name.
+    /// </summary>
+    public string Name
+    {
+      get { return _name; }
+      set { _name = value; }
+    }
+
+    /// <summary>
+    /// Set/get the main form.
+    /// </summary>
+    public CadKit.Helios.Lib.MainForm MainForm
+    {
+      get { return _mainForm; }
+      set { _mainForm = value; }
+    }
+
+    /// <summary>
+    /// Get the directory where this assembly resides.
+    /// </summary>
+    public string directory()
+    {
+      string path = System.Windows.Forms.Application.ExecutablePath;
+      string dir = System.IO.Path.GetDirectoryName( path );
+      return dir;
+    }
+
+    /// <summary>
+    /// Get the assembly file name.
+    /// </summary>
+    public string file()
+    {
+      return System.Reflection.Assembly.GetEntryAssembly().GetName().Name + ".exe";
+    }
+
+    /// <summary>
+    /// Get the icons directory.
+    /// </summary>
+    public string iconDir()
+    {
+      string dir = this.directory();
+      dir = dir.Replace( "\\bin\\Debug", "" );
+      dir = dir.Replace( "\\bin\\Release", "" );
+      return dir;
+    }
+
+    /// <summary>
+    /// Data members.
+    /// </summary>
+    private static Application _instance = null;
+    private string _name = System.Reflection.Assembly.GetEntryAssembly().GetName().Name; // Handles ".vshost";
+    private CadKit.Helios.Lib.MainForm _mainForm = null;
+  }
+}
---
+++ DotNet/Helios/Lib/MainForm.cs
@@ -0,0 +1,75 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2006, Perry L Miller IV
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+namespace CadKit.Helios.Lib
+{
+  public partial class MainForm : CadKit.Persistence.Form
+  {
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    public MainForm( string persistentName )
+    {
+      this.PersistentName = persistentName;
+      this.InitializeComponent();
+      this._loadPlugins();
+    }
+
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose( bool disposing )
+    {
+      if ( disposing && ( components != null ) )
+      {
+        components.Dispose();
+      }
+      base.Dispose( disposing );
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Text = "MainForm";
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Load all appropriate plugins.
+    /// </summary>
+    protected void _loadPlugins()
+    {
+      try
+      {
+        string dir = CadKit.Helios.Lib.Application.instance().directory();
+        string name = System.Reflection.Assembly.GetEntryAssembly().GetName().Name;
+        string path = dir + '/' + name + ".xml";
+        CadKit.Plugins.Manager.instance().load( path );
+      }
+      catch ( System.Exception e )
+      {
+        System.Console.WriteLine( "Error 3359202912: {0}", e.Message );
+      }
+    }
+  }
+}

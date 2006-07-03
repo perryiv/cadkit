f5e1a046ba73ba2b9a4a3d40822e90cae13561a3
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2006-07-03 08:56:43 +0000

    Change in indent size.

--- trunk/DotNet/Win32/LastError.cs
+++ trunk/DotNet/Win32/LastError.cs
@@ -7,47 +7,44 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-namespace CadKit
+namespace CadKit.Win32
 {
-  namespace Win32
+  public static class LastError
   {
-    public static class LastError
+    /// <summary>
+    /// Constants.
+    /// </summary>
+    public const int FORMAT_MESSAGE_FROM_SYSTEM = 0x00001000; // See winbase.h
+    public const string SYSTEM_KERNEL_DLL = "kernel32.dll";
+
+    /// <summary>
+    /// Format the last Win32 error.
+    /// </summary>
+    [System.Runtime.InteropServices.DllImport(Win32.LastError.SYSTEM_KERNEL_DLL)]
+    private static extern int FormatMessage(int flags, System.IntPtr source, int messageId, int languageId, System.Text.StringBuilder buffer, int size, System.IntPtr arguments);
+
+    /// <summary>
+    /// Get the last Win32 error.
+    /// </summary>
+    [
+    System.ComponentModel.Category("Win32"),
+    System.ComponentModel.Description("Windows System Error"),
+    System.ComponentModel.Browsable(false)
+    ]
+    public static string Message
     {
-      /// <summary>
-      /// Constants.
-      /// </summary>
-      public const int FORMAT_MESSAGE_FROM_SYSTEM = 0x00001000; // See winbase.h
-      public const string SYSTEM_KERNEL_DLL = "kernel32.dll";
-
-      /// <summary>
-      /// Format the last Win32 error.
-      /// </summary>
-      [System.Runtime.InteropServices.DllImport( Win32.LastError.SYSTEM_KERNEL_DLL )]
-      private static extern int FormatMessage( int flags, System.IntPtr source, int messageId, int languageId, System.Text.StringBuilder buffer, int size, System.IntPtr arguments );
-
-      /// <summary>
-      /// Get the last Win32 error.
-      /// </summary>
-      [
-      System.ComponentModel.Category( "Win32" ),
-      System.ComponentModel.Description( "Windows System Error" ),
-      System.ComponentModel.Browsable( false )
-      ]
-      public static string Message
+      get
       {
-        get
+        System.Text.StringBuilder message = new System.Text.StringBuilder(1024);
+        int error = System.Runtime.InteropServices.Marshal.GetLastWin32Error();
+        int result = Win32.LastError.FormatMessage(Win32.LastError.FORMAT_MESSAGE_FROM_SYSTEM, System.IntPtr.Zero, error, 0, message, message.Capacity, System.IntPtr.Zero);
+        if (0 == result)
         {
-          System.Text.StringBuilder message = new System.Text.StringBuilder( 1024 );
-          int error = System.Runtime.InteropServices.Marshal.GetLastWin32Error();
-          int result = Win32.LastError.FormatMessage( Win32.LastError.FORMAT_MESSAGE_FROM_SYSTEM, System.IntPtr.Zero, error, 0, message, message.Capacity, System.IntPtr.Zero );
-          if ( 0 == result )
-          {
-            return "";
-          }
-          else
-          {
-            return message.ToString().TrimEnd( new char[] { '\n', '\r' } );
-          }
+          return "";
+        }
+        else
+        {
+          return message.ToString().TrimEnd(new char[] { '\n', '\r' });
         }
       }
     }

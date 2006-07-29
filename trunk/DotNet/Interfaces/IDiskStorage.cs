761a35f8edd70c12a68f0a04e7c8446cfbf7b7fe
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2006-07-29 01:25:47 +0000

    Adding interfaces for disk storage.

---
+++ trunk/DotNet/Interfaces/IDiskStorage.cs
@@ -0,0 +1,19 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2006, Perry L Miller IV
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+namespace CadKit.Interfaces
+{
+  public interface IDiskStorage
+  {
+    /// <summary>
+    /// Get the name of the disk storage.
+    /// </summary>
+    string Name { get; }
+  }
+}

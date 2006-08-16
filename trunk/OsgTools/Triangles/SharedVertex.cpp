a10817e99d323a365b7139e9eb121063cf7a2a64
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2006-08-16 20:52:57 +0000

    -OsgTools.sln and OsgTools.vcproj are now 8.0 -removing OsgTools_vs8.vcproj

--- trunk/OsgTools/Triangles/SharedVertex.cpp
+++ trunk/OsgTools/Triangles/SharedVertex.cpp
@@ -42,7 +42,7 @@ SharedVertex::SharedVertex ( unsigned int index, unsigned int numTrianglesToRese
 {
   /*USUL_STATIC_ASSERT (  4 == sizeof ( _index        ) );
   USUL_STATIC_ASSERT (  1 == sizeof ( _flags        ) );
-  USUL_STATIC_ASSERT (  1 == sizeof ( _ref          ) );
+  //USUL_STATIC_ASSERT (  1 == sizeof ( _ref          ) );
 
 #if defined ( _MSC_VER ) && _MSC_VER < 1400
   USUL_STATIC_ASSERT ( 16 == sizeof ( _triangles    ) );
@@ -50,7 +50,7 @@ SharedVertex::SharedVertex ( unsigned int index, unsigned int numTrianglesToRese
 #elif defined ( _MSC_VER ) && _MSC_VER == 1400
 #ifdef _DEBUG
   USUL_STATIC_ASSERT ( 20 == sizeof ( _triangles    ) );
-  USUL_STATIC_ASSERT ( 28 == sizeof ( SharedVertex  ) ); // Why?
+  //USUL_STATIC_ASSERT ( 28 == sizeof ( SharedVertex  ) ); // Why?
 #else
   USUL_STATIC_ASSERT ( 16 == sizeof ( _triangles    ) );
   USUL_STATIC_ASSERT ( 24 == sizeof ( SharedVertex  ) ); // Why?

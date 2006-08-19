5d665be91ba6003c99fc1a9a559fd1d5019356bc
imikejackson <imikejackson@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2006-08-19 20:53:32 +0000

    This is the Main CMake file for the Helios Project. Using this file will ensure all the other libraries are properly built in order. There are several ways this file can be employed. Read the comments in the file itself for some more information. This will build: Usul. Threads, FoxTools, OsgTools, FoxCore, FoxViews, Helios, Fox/Plugins and Helios/Plugins. Some of the targets are commented out due to build difficulties.

---
+++ trunk/Helios/FindOSG.cmake
@@ -0,0 +1,46 @@
+
+# Pick up if the user has set an environment variable to point to the 
+# OpenThreads  Library Directory. If these are not set
+# then they will simply be ignored.
+ set(OT_LIB_DIR "$ENV{OPENTHREADS_LIB_DIR}")
+ set(OT_INC_DIR "$ENV{OPENTHREADS_INC_DIR}")
+ 
+ # Pick up if the user has set an environment variable to point to the 
+#  OpenSceneGraph Library Directory. If these are not set
+#  then they will simply be ignored.
+ set(OSG_LIB_DIR "$ENV{OSG_LIB_DIR}")
+ set(OSG_INC_DIR "$ENV{OSG_INC_DIR}")
+ 
+
+# ------------ Find OpenThreads Library ----------------------
+FIND_LIBRARY(OPENTHREADS_LIB OpenThreads
+ ${OT_LIB_DIR}
+)
+
+# ------------ Find osg Library ----------------------
+FIND_LIBRARY(OSG_LIB osg
+ ${OSG_LIB_DIR}
+)
+
+# ------------ Find osgDB Library ----------------------
+FIND_LIBRARY(OSG_DB_LIB osgDB
+ ${OSG_LIB_DIR}
+)
+
+# ------------ Find osgGA Library ----------------------
+FIND_LIBRARY(OSG_GA_LIB osgGA
+ ${OSG_LIB_DIR}
+)
+
+# ------------ Find osgText Library ----------------------
+FIND_LIBRARY(OSG_TEXT_LIB osgText
+ ${OSG_LIB_DIR}
+)
+
+# ------------ Find osgUtil Library ----------------------
+FIND_LIBRARY(OSG_UTIL_LIB osgUtil
+ ${OSG_LIB_DIR}
+)
+
+
+

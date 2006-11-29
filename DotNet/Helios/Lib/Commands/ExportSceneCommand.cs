1378418843ede8ac6e2cd85b817d04b6e2dff1fb
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2006-11-29 19:36:37 +0000

    -Adding export image and export scene commands

---
+++ DotNet/Helios/Lib/Commands/ExportSceneCommand.cs
@@ -0,0 +1,63 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2006, Adam Kubach
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+namespace CadKit.Helios.Commands
+{
+  public class ExportSceneCommand : CadKit.Commands.Command
+  {
+    public ExportSceneCommand(object caller)
+      : base()
+    {
+      _caller = caller;
+      _text = "Export Scene";
+    }
+
+    /// <summary>
+    /// Execute the command.
+    /// </summary>
+    protected override void _execute()
+    {
+      try
+      {
+        System.Windows.Forms.SaveFileDialog dialog = new System.Windows.Forms.SaveFileDialog();
+        dialog.AddExtension = true;
+        dialog.Title = CadKit.Helios.Application.Instance.Name + " -- Export Scene";
+
+        CadKit.Interfaces.IExportScene export = CadKit.Documents.Manager.Instance.ActiveView as CadKit.Interfaces.IExportScene;
+        if (null != export)
+        {
+          string finalFilter = "";
+          CadKit.Interfaces.Filters filters = export.Filters;
+          foreach (CadKit.Interfaces.Filter filter in filters)
+          {
+            finalFilter += (filter.Text + '|' + filter.Extensions + '|');
+          }
+          finalFilter = finalFilter.TrimEnd(new char[] { '|' });
+          dialog.Filter = finalFilter;
+          dialog.ShowDialog();
+          string filename = dialog.FileName;
+          export.exportScene(filename);
+        }
+      }
+      catch (System.Exception exception)
+      {
+        System.Console.WriteLine("Error 314952192: exception caught while trying to export scene.");
+        System.Console.WriteLine("Message: {0}", exception.Message);
+      }
+    }
+
+    /// <summary>
+    /// Determine if the button should be enabled.
+    /// </summary>
+    protected override bool _shouldBeEnabled()
+    {
+      return CadKit.Documents.Manager.Instance.ActiveView != null;
+    }
+  }
+}

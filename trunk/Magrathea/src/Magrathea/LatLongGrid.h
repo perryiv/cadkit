ae89f285abc7840c7b668ac31888ae2a0b80bf45
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-02-11 06:32:17 +0000

    -Bisecting lines to fit on the earth better

--- trunk/Magrathea/src/Magrathea/LatLongGrid.h
+++ trunk/Magrathea/src/Magrathea/LatLongGrid.h
@@ -1,13 +1,12 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2007, Decision Theater at Arizona State University
+//  Copyright (c) 2007, Arizona State University
 //  All rights reserved.
 //  BSD License: http://www.opensource.org/licenses/bsd-license.html
 //  Created by: Adam Kubach
 //
 ///////////////////////////////////////////////////////////////////////////////
-
 
 #ifndef __LAT_LONG_GRID_H__
 #define __LAT_LONG_GRID_H__

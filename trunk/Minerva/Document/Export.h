3343cb76928d1a4515d6ac9da917763dc591fc11
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-03-03 16:22:49 +0000

    -Adding native Minerva document.

--- trunk/Minerva/Document/Export.h
+++ trunk/Minerva/Document/Export.h
@@ -1,7 +1,7 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2006, Arizona State University
+//  Copyright (c) 2007, Arizona State University
 //  All rights reserved.
 //  BSD License: http://www.opensource.org/licenses/bsd-license.html
 //  Created by: Adam Kubach
@@ -10,23 +10,23 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-//  Defines what MINERVA_EXPORT means.
+//  Defines what MINERVA_DOCUMENT_EXPORT means.
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-#ifndef _MINERVA_EXPORT_H_
-#define _MINERVA_EXPORT_H_
+#ifndef _MINERVA_DOCUMENT_EXPORT_H_
+#define _MINERVA_DOCUMENT_EXPORT_H_
 
 #ifdef _WIN32
 # pragma warning ( disable : 4275 ) // Irrelevant VC6 warning.
 # pragma warning ( disable : 4251 ) // Irrelevant VC6 warning.
-# ifdef _COMPILING_MINERVA
-#   define MINERVA_EXPORT __declspec ( dllexport ) // We are compiling this library so the classes are exported.
+# ifdef _COMPILING_MINERVA_DOCUMENT
+#   define MINERVA_DOCUMENT_EXPORT __declspec ( dllexport ) // We are compiling this library so the classes are exported.
 # else
-#   define MINERVA_EXPORT __declspec ( dllimport ) // The classes will be imported into the client's code.
+#   define MINERVA_DOCUMENT_EXPORT __declspec ( dllimport ) // The classes will be imported into the client's code.
 # endif
 #else // _WIN32
-# define MINERVA_EXPORT
+# define MINERVA_DOCUMENT_EXPORT
 #endif
 
-#endif // _MINERVA_EXPORT_H_
+#endif // MINERVA_DOCUMENT_EXPORT

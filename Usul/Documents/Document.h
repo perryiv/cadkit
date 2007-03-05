e715ba50f6619814b9d68d5540fdcf0d8dfb603e
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-03-05 05:08:49 +0000

    -Adding comments. -Adding document to solution file

--- Usul/Documents/Document.h
+++ Usul/Documents/Document.h
@@ -2,6 +2,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 //
 //  Copyright (c) 2002, Perry L. Miller IV
+//  Copyright (c) 2005, Perry L. Miller IV and Adam Kubach
 //  All rights reserved.
 //  BSD License: http://www.opensource.org/licenses/bsd-license.html
 //

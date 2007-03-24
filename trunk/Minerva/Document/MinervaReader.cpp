e83707bc1f148b71435eca6a373d073b2820983d
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-03-24 06:41:01 +0000

    -Using SerializeXML library for saving and loading Document

--- trunk/Minerva/Document/MinervaReader.cpp
+++ trunk/Minerva/Document/MinervaReader.cpp
@@ -46,12 +46,16 @@ MinervaReader::~MinervaReader()
 
 void MinervaReader::operator()()
 {
+  Minerva::Core::registerFactories();
+  Serialize::XML::Factory::instance().add ( new Serialize::XML::TypeCreator<MinervaDocument> ( "MinervaDocument" ) );
+  Serialize::XML::deserialize ( _filename, *_document );
+
   // Create the archive.
-  std::ifstream fin ( _filename.c_str() );
+  /*std::ifstream fin ( _filename.c_str() );
   boost::archive::xml_iarchive ia ( fin );
 
   Minerva::Core::registerTypes( ia );
   ia.register_type< MinervaDocument > ();
 
-  ia >> boost::serialization::make_nvp( "Document", *_document );
+  ia >> boost::serialization::make_nvp( "Document", *_document );*/
 }

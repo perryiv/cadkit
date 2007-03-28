f0dbc14f9eeb1e8cb2e3b25309ad699e8e2d658c
ashish_cy <ashish_cy@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-03-28 10:19:48 +0000

    - Interface includes two more virtual functions to query the status of the movie player.

--- trunk/DotNet/Interfaces/IMovieMode.cs
+++ trunk/DotNet/Interfaces/IMovieMode.cs
@@ -7,5 +7,15 @@ namespace CadKit.Interfaces
   public interface IMovieMode : IReferenced
   {
     void setMovieMode( bool val );
+
+    void play();
+
+    void pause();
+
+    void restart();
+
+    bool isPlaying();
+
+    bool isPaused();
   }
 }

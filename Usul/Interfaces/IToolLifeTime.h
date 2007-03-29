f8ec7a1396959efab3264df663950aec8733f777
ashish_cy <ashish_cy@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-03-29 20:40:08 +0000

    - Added new interface that defines the life time of the tool. ( there would be only two options ) temporary or not temporary

---
+++ Usul/Interfaces/IToolLifeTime.h
@@ -0,0 +1,44 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2007, Arizona State University
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//  Created by: Aashish Chaudhary
+//
+///////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////
+//
+//  Interface for defining life time for a tool. 
+//
+/////////////////////////////////////////////////////////////////////////////
+  
+#ifndef __USUL_INTERFACES_TOOL_LIFE_TIME_H__
+#define __USUL_INTERFACES_TOOL_LIFE_TIME_H__
+
+#include "Usul/Interfaces/IUnknown.h"
+
+namespace Usul {
+namespace Interfaces   {
+    
+struct IToolLifeTime : public Usul::Interfaces::IUnknown
+{
+  typedef Usul::Interfaces::IUnknown Unknown;
+
+  /// Smart-pointer definitions.
+  USUL_DECLARE_QUERY_POINTERS ( IToolLifeTime );
+
+  enum { IID = 3504968652u };
+
+  virtual bool execute ( Unknown* caller, bool left, bool middle, bool right, bool motion, float x, float y, float z ) = 0;  
+
+  virtual bool temporary()   = 0;
+
+}; //class IToolLifeTime
+
+} //namespace Interfaces
+} //namespace Usul
+
+#endif // __USUL_INTERFACES_TOOL_LIFE_TIME_H__
+

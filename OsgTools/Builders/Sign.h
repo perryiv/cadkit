d057832ef75468b838e1281dcd6d89da6ba29d4b
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-04-07 19:29:17 +0000

    -Adding sign class.  Still needs more work.

---
+++ OsgTools/Builders/Sign.h
@@ -0,0 +1,54 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2006, Arizona State University
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//  Created by: Adam Kubach
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __OSG_TOOLS_BUILDERS_SIGN_H__
+#define __OSG_TOOLS_BUILDERS_SIGN_H__
+
+#include "OsgTools/Export.h"
+
+#include "osg/Vec3"
+
+#include <string>
+
+namespace osg { class Node; }
+
+namespace OsgTools {
+namespace Builders {
+
+class OSG_TOOLS_EXPORT Sign
+{
+public:
+  Sign();
+  ~Sign();
+
+  osg::Node*  operator() ();
+
+protected:
+  osg::Node*  _buildText();
+  osg::Node*  _buildSign();
+  osg::Node*  _buildPost();
+  osg::Node*  _buildAnchor();
+
+private:
+  std::string _text;
+
+  osg::Vec3 _start;
+  osg::Vec3 _end;
+
+  bool _drawAnchor;
+  bool _drawPost;
+  bool _drawSign;
+};
+
+
+}
+}
+
+#endif // __OSG_TOOLS_BUILDERS_SIGN_H__

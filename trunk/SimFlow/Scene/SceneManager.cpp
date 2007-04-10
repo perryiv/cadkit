d009c1f7154687223bfb22529c10d9c3ad060e24
ashish_cy <ashish_cy@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-04-10 23:57:17 +0000

    - Added License information

--- trunk/SimFlow/Scene/SceneManager.cpp
+++ trunk/SimFlow/Scene/SceneManager.cpp
@@ -1,4 +1,13 @@
 
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2007, Arizona State University
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//  Created by: Aashish Chaudhary
+//
+///////////////////////////////////////////////////////////////////////////////
+
 #include "SceneManager.h"
 
 using namespace SimFlow::Scene;

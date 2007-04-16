f81ca430a700492131ddd5504ad5de2cfaaf10b2
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-04-16 23:20:27 +0000

    -geocode script is more modular -csv parsing script uses functions from geocode file

--- trunk/Experimental/Geocode/geocode-csv.py
+++ trunk/Experimental/Geocode/geocode-csv.py
@@ -1,22 +1,39 @@
 #!/usr/bin/python
 
+###############################################################################
+##
+##  Copyright (c) 2007, Arizona State University
+##  All rights reserved.
+##  BSD License: http://www.opensource.org/licenses/bsd-license.html
+##  Created by: Adam Kubach
+##
+###############################################################################
+
 import csv
 import sys,string
-
-#p = csv.parser()
+import geocode
 
 print "Enter a filename: ",
 filename = string.rstrip(sys.stdin.readline())
 
-file = open ( filename, 'r' )
-print file
+reader = csv.reader ( open( filename, "rb" ) )
+print reader
 
-while 1:
-    line = file.readline()
-    if not line:
-	break
-    print line
+out = open( 'output.csv', 'w' )
+print out
 
-#for line in file:
-#    print line
+reader.next()
+for row in reader:
+    for s in row:
+        out.write ( s )
+        out.write ( ", " )
+    g = geocode.geocode ( row[1], row[3], row[4], row[5] )
+    out.write ( g.precision )
+    out.write ( ", " )
+    out.write ( g.latitude )
+    out.write ( ", " )
+    out.write ( g.longitude )
+    out.write ( ", " )
+    out.write ( "\n" )
+
 

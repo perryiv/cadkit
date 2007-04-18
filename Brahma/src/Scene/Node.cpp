bf9e8bebda95cdee85a6e838d2b7f39d3662454b
ashish_cy <ashish_cy@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-04-18 18:09:27 +0000

    - Added group Node - Moved the definitions to .cpp files for Node

---
+++ Brahma/src/Scene/Node.cpp
@@ -0,0 +1,23 @@
+
+#include "Node.h"
+
+void Brahma::Scene::Node::id( const std::string& id )
+{
+  _id = id;
+}
+
+const std::string& Brahma::Scene::Node::id() const
+{
+  return _id;
+}
+
+void Brahma::Scene::Node::data( const std::string& source )
+{
+  _data = source;
+}
+
+const std::string& Brahma::Scene::Node::data() const 
+{
+  return _data;
+}
+

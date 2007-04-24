e9f3ce25a93f7bf05a0419281b8b56602e29e21e
ashish_cy <ashish_cy@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-04-24 02:22:20 +0000

    - Too many namespaces has name Core, Scene etc...

---
+++ trunk/Brahma/src/Brahma/Scene/BaseNodeVisitor.h
@@ -0,0 +1,36 @@
+
+#ifndef __SCENE_BASENODEVISITOR_H__
+#define __SCENE_BASENODEVISITOR_H__
+
+#include "Core/Object.h"
+
+namespace Brahma
+{
+  namespace Scene
+  {
+    class Node;
+    class Group;
+    class Transform;
+
+    class BaseNodeVisitor : public Core::Object
+    {
+      public:         
+        
+        USUL_DECLARE_REF_POINTERS( BaseNodeVisitor );
+
+        virtual void apply( Node& )         = 0;
+
+        virtual void apply( Grroup& )       = 0;
+
+        virtual void apply( Transform& )    = 0;
+
+      protected:
+
+        virtual ~BaseNodeVisitor()
+        {
+        }
+    };
+  }
+}
+
+#endif // __SCENE_BASENODEVISITOR_H__

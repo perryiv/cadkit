90f391534358b92f3b08ebc454c4f75e2d9cb03e
ashish_cy <ashish_cy@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-04-26 23:21:47 +0000

    Node object now cannot be initiated..

--- Brahma/src/Brahma/Scene/Node.cpp
+++ Brahma/src/Brahma/Scene/Node.cpp
@@ -12,21 +12,22 @@ void Brahma::Scene::Node::id( const std::string& id )
   _id = id;
 }
 
-Brahma::Scene::Database* Brahma::Scene::Node::database() 
-{
-  return _database.get();
-}
+//
+//Brahma::Scene::LeafNode* Brahma::Scene::Node::database() 
+//{
+//  return _database.get();
+//}
+//
+//void Brahma::Scene::Node::database( Brahma::Scene::LeafNode* db )
+//{
+//  _database = db;
+//}
+//
 
-void Brahma::Scene::Node::database( Brahma::Scene::Database* db )
-{
-  _database = db;
-}
-
-
-void Brahma::Scene::Node::accept( Brahma::Scene::NodeVisitor &nv )
-{
-	nv.visit( *this );
-}
+//void Brahma::Scene::Node::accept( Brahma::Scene::NodeVisitor &nv )
+//{
+//	nv.visit( *this );
+//}
 
 //const std::string& Brahma::Scene::Node::data() const 
 //{

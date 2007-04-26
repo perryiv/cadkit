63203d3c2b393bf4b5bc9e733c0b3cda7cb87fc6
ashish_cy <ashish_cy@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-04-26 23:23:35 +0000

    Interface for node visitor that has to implemented  by the parser

---
+++ Brahma/src/Brahma/Scene/NodeVisitor.h
@@ -0,0 +1,36 @@
+
+#ifndef __SCENE_NODEVISITOR_H__
+#define __SCENE_NODEVISITOR_H__
+
+#include "Brahma/Core/Object.h"
+
+namespace Brahma
+{
+  namespace Scene
+  {
+    class LeafNode;
+    class Group;
+    class Transform;
+
+    class NodeVisitor : public Core::Object
+    {
+      public:         
+        
+        USUL_DECLARE_REF_POINTERS( NodeVisitor );				
+
+        virtual void visit( LeafNode&, const bool& createNew = true )		= 0;
+
+        virtual void visit( Group&, const bool& createNew = true )			= 0;        
+
+				virtual void visit( Transform&, const bool& createNew = true )	= 0;        
+
+      protected:
+
+        virtual ~NodeVisitor()
+        {
+        }
+    };
+  }
+}
+
+#endif // __SCENE_NODEVISITOR_H__

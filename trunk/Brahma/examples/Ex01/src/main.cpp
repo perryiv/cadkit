17bb7d19e367fc0b3dd146ac289f8cfd77091c67
ashish_cy <ashish_cy@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-04-27 00:07:16 +0000

    - Ever Changing example .. should create another one..

--- trunk/Brahma/examples/Ex01/src/main.cpp
+++ trunk/Brahma/examples/Ex01/src/main.cpp
@@ -1,4 +1,13 @@
 
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2007, Arizona State University
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//  Created by: Aashish Chaudhary
+//
+///////////////////////////////////////////////////////////////////////////////
+
 #include "Brahma/Core/Brahma.h"
 
 #include "Brahma/Scene/Node.h"

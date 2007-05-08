21a56ea0804f9f44972724dbffe409b382da63b1
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-05-08 19:32:42 +0000

    -Updating from database takes place in different thread.

--- trunk/Minerva/Apps/MinervaVR/main.cpp
+++ trunk/Minerva/Apps/MinervaVR/main.cpp
@@ -1,15 +1,16 @@
-//
-//
-// Disable useless warnings
-#ifdef WIN32
-	#pragma warning( disable : 4290 4251 4267 4275 4244 4312)
-#endif
 
-// VRJ includes
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2006, Arizona State University
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//  Created by: Adam Kubach
+//
+///////////////////////////////////////////////////////////////////////////////
+
 #include "MinervaVR.h"
 
-// Juggler include 
-#include <vrj/Kernel/Kernel.h>
+#include "vrj/Kernel/Kernel.h"
 
 #include "Usul/CommandLine/Arguments.h"
 #include "Usul/Components/Manager.h"
@@ -33,6 +34,7 @@ Usul::Threads::SetThreadFactory threadFactory ( &Threads::OT::newOpenThreadsThre
 int main( int argc, char* argv[] )
 {
   Usul::CommandLine::Arguments::instance().set ( argc, argv );
+
   // Add some plugin extensions.
 #ifdef _DEBUG
   Usul::Components::Manager::instance().addPluginExtension ( "plugd" );
@@ -61,20 +63,21 @@ int main( int argc, char* argv[] )
   
   // Number of argumets should be more than 1 :: Needs atleast one config file ::
   if( argc < 2 )
-    {
-      std::cerr << " Test: " << std::endl;
-      //printUsageInformation();
-      std::exit(1);
-    }
+  {
+    std::cerr << " Test: " << std::endl;
+    //printUsageInformation();
+    std::exit(1);
+  }
 
   for( int i = 1; i < argc; ++i )
-    {
-      kernel->loadConfigFile( argv[i] );
-    }
+  {
+    kernel->loadConfigFile( argv[i] );
+  }
 
   kernel->start();
   kernel->setApplication( application );
   kernel->waitForKernelStop();
+
   return 0;
 }
 

e49c56c4f6f9eb28b844819fd00630b26fd9788c
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-05-14 07:43:01 +0000

    -- Changed name of Usul/Strings/ManagedToNative.h to Usul/Strings/Managed.h -- Added Usul/Strings/Qt.h -- Added Usul/Threads/Named for mapping strings to thread IDs

---
+++ trunk/Usul/Strings/Qt.h
@@ -0,0 +1,68 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2006, Perry Miller
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Convert QString to native.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _USUL_STRINGS_QT_STRING_TO_NATIVE_
+#define _USUL_STRINGS_QT_STRING_TO_NATIVE_
+
+#include "QtCore/QString"
+
+#include <string>
+
+
+namespace Usul {
+namespace Strings {
+
+
+inline void convert ( const QString &source, std::string &target )
+{
+  target.clear();
+  if ( false == source.isEmpty() )
+  {
+    const QByteArray bytes ( source.toAscii() );
+    target.assign ( bytes.begin(), bytes.end() );
+  }
+}
+
+
+inline std::string convert ( const QString &source )
+{
+  std::string target;
+  Usul::Strings::convert ( source, target );
+  return target;
+}
+
+
+template < class QtStringContainer, class StandardStringContainer > 
+inline void convert ( const QtStringContainer &q, StandardStringContainer &s )
+{
+  s.clear();
+  if ( false == q.empty() )
+  {
+    s.reserve ( q.size() );
+    for ( QtStringContainer::const_iterator i = q.begin(); i != q.end(); ++i )
+    {
+      const QString &temp ( *i );
+      const QByteArray bytes ( temp.toAscii() );
+      s.push_back ( std::string ( bytes.begin(), bytes.end() ) );
+    }
+  }
+}
+
+
+} // String
+} // Usul
+
+
+#endif // _USUL_STRINGS_QT_STRING_TO_NATIVE_

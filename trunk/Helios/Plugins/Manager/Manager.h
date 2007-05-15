76a71f57d088bf24f9b5cd7cf7549e447b58764c
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-05-15 20:23:58 +0000

    -Adding plugin manager to parse xml file and populate Usul's Component Manager.   Not complete yet.

---
+++ trunk/Helios/Plugins/Manager/Manager.h
@@ -0,0 +1,52 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2007, Arizona State University
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//  Created by: Adam Kubach
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __CADKIT_HELIOS_PLUGINS_MANAGER_H__
+#define __CADKIT_HELIOS_PLUGINS_MANAGER_H__
+
+#include "Export.h"
+
+#include <string>
+
+namespace CadKit {
+namespace Helios {
+namespace Plugins {
+namespace Manager {
+
+class HELIOS_PLUGINS_EXPORT Manager
+{
+public:
+
+	/// Return the instance.
+	static Manager& instance();
+	
+	/// Get/Set the filename to parse.
+	void                      filename ( const std::string& filename );
+	const std::string&        filename() const;
+
+	/// Parse the file.
+	void                      parse();
+private:
+	Manager();
+	~Manager();
+	
+	std::string     _filename;
+	
+	static Manager *_instance;
+};
+
+
+}
+}
+}
+}
+
+
+#endif // __CADKIT_HELIOS_PLUGINS_MANAGER_H__

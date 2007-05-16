84af3e2d2bdb7d30627010c4ce6cfadbe8de39c8
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-05-16 22:06:47 +0000

    -Adding data members to Command class

--- trunk/Usul/Commands/Command.h
+++ trunk/Usul/Commands/Command.h
@@ -45,6 +45,22 @@ public:
 
   /// Usul::Interfaces::IUnknown members.
   USUL_DECLARE_IUNKNOWN_MEMBERS;
+  
+  /// Get/Set the text.
+  void                         text( const std::string& value );
+  const std::string&           text() const;
+  
+  /// Get/Set the tooltip.
+  void                         tooltip ( const std::string& value );
+  const std::string&           tooltip() const;
+  
+  /// Get/Set the shortcut.
+  void                         shortcut ( const std::string& value );
+  const std::string&           shortcut() const;
+  
+  /// Get/Set the icon path.
+  void                         iconPath ( const std::string& value );
+  const std::string&           iconPath() const;
 
 protected:
 
@@ -66,6 +82,11 @@ private:
   void                      _destroy();
 
   IUnknown::RefPtr _caller;
+  std::string      _text;
+  std::string      _toolTipText;
+  std::string      _shortcut;
+  std::string      _iconPath;
+  
 };
 
 

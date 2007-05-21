154f29c6f2ddf0eb14ca5e80ae3fd22edc99c239
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-05-21 15:05:42 +0000

    -Adding class to contain branding information.

---
+++ Usul/App/Application.h
@@ -0,0 +1,55 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2007, Arizona State University
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//  Created by: Adam Kubach
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  I'm not sure if this class is needed, or if this is the correct location for it.
+//  I'm currently thinking that this class will be used for branding.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __USUL_APP_APPLICATION_H__
+#define __USUL_APP_APPLICATION_H__
+
+#include "Usul/Export/Export.h"
+
+#include <string>
+
+namespace Usul {
+namespace App {
+
+class USUL_EXPORT Application
+{
+public:
+	static Application& instance();
+	
+	/// Get the icon directory.
+	const std::string                    iconDirectory() const;
+	
+	/// Get/Set the splash screen image.
+	void                                 splashImage( const std::string& image );
+	const std::string &                  splashImage( ) const;
+	
+	/// Get the path to the splash screen image.
+	std::string                          splashImagePath() const;
+	
+private:
+	Application();
+	~Application();
+	
+	static Application *_instance;
+	
+	std::string         _splashImage;
+};
+
+}
+}
+
+#endif // __USUL_APP_APPLICATION_H__

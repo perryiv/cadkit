52e0afcf2a5a651e4fabb9630195eb63dc96dd64
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-06-19 06:03:40 +0000

    -Adding helper struct to change and restore current working directory.

--- trunk/Usul/System/Directory.h
+++ trunk/Usul/System/Directory.h
@@ -25,6 +25,23 @@ public:
   /// Get/Set the current working directory.
   static void          cwd ( const std::string& directory );
   static std::string   cwd ();
+
+  /// Helper struct the change and restore current working directory.
+  struct ScopedCwd
+  {
+    ScopedCwd ( const std::string& directory ) :
+      _oldCwd ( Directory::cwd() )
+    {
+      Directory::cwd ( directory );
+    }
+    ~ScopedCwd ()
+    {
+      Directory::cwd ( _oldCwd );
+    }
+
+  private:
+    std::string _oldCwd;
+  };
 };
 
 }

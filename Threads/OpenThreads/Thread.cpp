7a3a95b97fed7937a1c0c727256f965ab54823d3
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-06-22 01:18:20 +0000

    Added a kill function to thread class. Not stable yet.

--- Threads/OpenThreads/Thread.cpp
+++ Threads/OpenThreads/Thread.cpp
@@ -18,6 +18,7 @@
 #include "Usul/Adaptors/MemberFunction.h"
 #include "Usul/Functions/SafeCall.h"
 #include "Usul/Strings/Format.h"
+#include "Usul/System/Sleep.h"
 #include "Usul/Trace/Trace.h"
 
 #include "OpenThreads/Thread"
@@ -29,44 +30,108 @@ using namespace Threads::OT;
 
 ///////////////////////////////////////////////////////////////////////////////
 //
+//  Beginning of namespace Detail.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+namespace Threads {
+namespace OT {
+namespace Detail {
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
 //  Concrete thread class.
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-namespace Detail
+class OpenThread : public OpenThreads::Thread
 {
-  class OpenThread : public OpenThreads::Thread
+public:
+
+  typedef OpenThreads::Thread BaseClass;
+
+  OpenThread ( Threads::OT::Thread *thread ) : BaseClass(),
+    _thread ( thread )
   {
-  public:
+    USUL_TRACE_SCOPE;
+  }
 
-    typedef OpenThreads::Thread BaseClass;
+  virtual ~OpenThread()
+  {
+    USUL_TRACE_SCOPE;
+    // Nothing to delete, we do not own the thread object.
+  }
 
-    OpenThread ( Threads::OT::Thread *thread ) : BaseClass(),
-      _thread ( thread )
+  virtual void run()
+  {
+    USUL_TRACE_SCOPE;
+    if ( 0x0 != _thread )
     {
-      USUL_TRACE_SCOPE;
+      Usul::Functions::safeCall ( Usul::Adaptors::memberFunction ( _thread, &Threads::OT::Thread::_execute ) );
     }
+  }
 
-    virtual ~OpenThread()
-    {
-      USUL_TRACE_SCOPE;
-      // Nothing to delete, we do not own the thread object.
-    }
+private:
 
-    virtual void run()
+  Threads::OT::Thread *_thread;
+};
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Helper function to wait for a thread to stop running.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+void wait ( unsigned int ierations, unsigned int seconds, Usul::Threads::Thread *usulThread, OpenThreads::Thread *openThread )
+{
+  if ( 0x0 == usulThread || 0x0 == openThread )
+    return;
+
+  // If the thread is still running then wait for it.
+  if ( true == openThread->isRunning() )
+  {
+    for ( unsigned int i = 0; i < ierations; ++i )
     {
-      USUL_TRACE_SCOPE;
-      if ( 0x0 != _thread )
+      if ( true == openThread->isRunning() )
       {
-        Usul::Functions::safeCall ( Usul::Adaptors::memberFunction ( _thread, &Threads::OT::Thread::_execute ) );
+        // Many threads may call cout, so formatting the string first will 
+        // ensure this line stays together.
+        std::cout << Usul::Strings::format 
+          ( "Warning 4624401900: waiting for thread ", usulThread, " to stop running, id: ", 
+            usulThread->id(), ", OpenThreads id: ", openThread->getThreadId(), '\n' );
+        std::cout << std::flush;
+
+        // Wait here...
+        Usul::System::Sleep::seconds ( seconds );
       }
     }
-
-  private:
-
-    Threads::OT::Thread *_thread;
-  };
+  }
 }
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Helper function to delete a thread.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+void deleteThread ( OpenThreads::Thread *thread )
+{
+  delete thread;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  End of namespace Detail.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+} // namespace Detail
+} // namespace OT
+} // namespace Threads
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -91,7 +156,7 @@ Thread::Thread() : BaseClass(),
 Thread::~Thread()
 {
   USUL_TRACE_SCOPE;
-  Usul::Functions::safeCall ( Usul::Adaptors::memberFunction ( this, &Thread::_destroy ), "3716194294" );
+  Usul::Functions::safeCall ( Usul::Adaptors::memberFunction ( this, &Threads::OT::Thread::_destroy ), "3716194294" );
 }
 
 
@@ -107,40 +172,29 @@ void Thread::_destroy()
   OpenThreads::Thread *t ( _thread );
   _thread = 0x0;
 
-  if ( 0x0 != t )
+  // Handle no thread (or a killed thread).
+  if ( 0x0 == t )
+    return;
+
+  // If the thread is still running then wait for it.
+  Detail::wait ( 10, 1, this, t );
+
+  // If it's not running then delete it.
+  if ( false == t->isRunning() )
   {
-    // If the thread is still running then wait for it.
-    if ( true == t->isRunning() )
-    {
-      for ( unsigned int i = 0; i < 10; ++i )
-      {
-        if ( true == t->isRunning() )
-        {
-          // Many threads may call cout, so formatting the string first will 
-          // ensure this line stays together.
-          std::cout << Usul::Strings::format 
-            ( "Warning 4624401900: waiting for thread ", this, " to stop running, id: ", 
-              this->id(), ", OpenThreads id: ", t->getThreadId(), '\n' );
+    // By detaching we make calls to join() do nothing.
+    t->detach();
 
-          // Wait one second.
-          OpenThreads::Thread::microSleep ( 1000000 );
-        }
-      }
-    }
+    // Delete the OpenThreads object.
+    Usul::Functions::safeCallV1 ( &Threads::OT::Detail::deleteThread, t, "3646441490" );
+  }
 
-    // If it's not running then delete it.
-    if ( false == t->isRunning() )
-    {
-      delete t;
-    }
-
-    // Otherwise, it's a leak, which is better than bringing down the house.
-    else
-    {
-      std::cout << Usul::Strings::format 
-        ( "Warning 3096769450: memory leak for thread ", this, " because it failed to stop running, id: ", 
-          this->id(), ", OpenThreads id: ", t->getThreadId(), '\n' );
-    }
+  // Otherwise, it's a leak, which is better than bringing down the house.
+  else
+  {
+    std::cout << Usul::Strings::format 
+      ( "Warning 3096769450: memory leak for thread ", this, " because it failed to stop running, id: ", 
+        this->id(), ", OpenThreads id: ", t->getThreadId(), '\n' );
   }
 }
 
@@ -154,7 +208,29 @@ void Thread::_destroy()
 void Thread::kill()
 {
   USUL_TRACE_SCOPE;
-  Usul::Functions::safeCall ( Usul::Adaptors::memberFunction ( this, &Thread::_destroy ), "1854115959" );
+
+  // Don't kill twice.
+  if ( BaseClass::KILLED == this->result() )
+    return;
+
+  // Don't kill an idle thread.
+  if ( true == this->isIdle() )
+    return;
+
+  // Grab pointer and detach.
+  OpenThreads::Thread *t ( _thread );
+  _thread = 0x0;
+
+  // If it's still running then cancel it.
+  if ( true == t->isRunning() )
+  {
+    // OpenThreads implementation of cancel() is a "kill".
+    Usul::Functions::safeCall ( Usul::Adaptors::memberFunction ( t, &OpenThreads::Thread::setCancelModeAsynchronous ), "3247502066" );
+    Usul::Functions::safeCall ( Usul::Adaptors::memberFunction ( t, &OpenThreads::Thread::cancel ), "3474159748" );
+  }
+
+  // Call this last to keep from getting purged!
+  BaseClass::kill();
 }
 
 

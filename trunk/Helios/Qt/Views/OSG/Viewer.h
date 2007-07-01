de7656d1ff6cc869efc63060e62ecc12b3de4bd0
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-07-01 06:17:22 +0000

    -Hooking up the scene and Viewer -Implementing mouse and key board event handlers.

--- trunk/Helios/Qt/Views/OSG/Viewer.h
+++ trunk/Helios/Qt/Views/OSG/Viewer.h
@@ -59,6 +59,11 @@ protected:
   virtual void                            paintEvent  ( QPaintEvent * event );
   virtual void                            resizeEvent ( QResizeEvent * event );
   virtual void                            focusInEvent ( QFocusEvent * event );
+  virtual void                            mouseMoveEvent ( QMouseEvent * event );
+  virtual void                            mousePressEvent ( QMouseEvent * event );
+  virtual void                            mouseReleaseEvent ( QMouseEvent * event );
+  virtual void                            keyPressEvent ( QKeyEvent * event );
+  virtual void                            keyReleaseEvent ( QKeyEvent * event );
 
 private:
 

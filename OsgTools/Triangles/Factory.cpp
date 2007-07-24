a12d0e12eb0f1921c26ac6ce3c0773f25e120a2e
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-07-24 21:28:29 +0000

    Small changes in headers, deleting extra white space, etc.

--- OsgTools/Triangles/Factory.cpp
+++ OsgTools/Triangles/Factory.cpp
@@ -16,7 +16,9 @@
 #ifdef _MSC_VER
 #pragma warning ( disable : 4800 ) // Forcing value to bool 'true' or 'false'
 #define WIN32_LEAN_AND_MEAN        // Exclude rarely-used stuff from Windows headers.
-// #define NOMINMAX                   // Do not define min and max as macros.
+# ifndef NOMINMAX
+#  define NOMINMAX                 // Do not define min and max as macros.
+# endif
 #endif
 
 #include "OsgTools/Triangles/Factory.h"

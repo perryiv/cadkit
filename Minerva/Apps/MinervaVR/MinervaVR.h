5e1c2be4b923e90531456d9387f5283d1e889888
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-07-31 23:23:51 +0000

    -Removing command queue.  Is in the base VRV Application.

--- Minerva/Apps/MinervaVR/MinervaVR.h
+++ Minerva/Apps/MinervaVR/MinervaVR.h
@@ -23,26 +23,18 @@
 #include "Usul/Threads/Thread.h"
 #include "Usul/CommandLine/Options.h"
 #include "Usul/Adaptors/Random.h"
-#include "Usul/Interfaces/ICommand.h"
-#include "Usul/Interfaces/ICommandQueueAdd.h"
 
 #include "Magrathea/Planet.h"
 
 #include <list>
 
 class MinervaVR : public VRV::Core::Application,
-                  public Minerva::Interfaces::IAnimationControl,
-                  public Usul::Interfaces::ICommandQueueAdd
+                  public Minerva::Interfaces::IAnimationControl
 {
 public:
   // Typedefs.
   typedef VRV::Core::Application             BaseClass;
-  typedef Usul::Threads::RecursiveMutex      Mutex;
-  typedef Usul::Threads::Guard<Mutex>        Guard;
   typedef Usul::Threads::Thread              Thread;
-  typedef Usul::Interfaces::ICommand         Command;
-  typedef Command::QueryPtr                  CommandPtr;
-  typedef std::list < CommandPtr >           CommandQueue;
   typedef std::list < std::string >          Args;
 
   USUL_DECLARE_IUNKNOWN_MEMBERS;
@@ -52,22 +44,20 @@ public:
   virtual ~MinervaVR();		
 
  protected:
-  virtual void init();
+  virtual void                 init();
   
-  virtual void preFrame();
-  virtual void draw();
-  virtual void postFrame();
+  virtual void                 preFrame();
+  virtual void                 draw();
+  virtual void                 postFrame();
 
-  void         _initLight ();
-  void         _initScene ();
-  void         _initLegend ();
+  void                         _initLight ();
+  void                         _initScene ();
+  void                         _initLegend ();
 
-  void         _updateScene( Usul::Threads::Thread *thread = 0x0 );
-  void         _buildScene();
+  void                         _updateScene( Usul::Threads::Thread *thread = 0x0 );
+  void                         _buildScene();
 
-  void         _launchUpdateThread();
-
-  void         _processCommands ();
+  void                         _launchUpdateThread();
 
   /// Minerva::Interfaces::IAnimationControl
   /// Start the animation.
@@ -83,9 +73,6 @@ public:
   virtual void                 animateSpeed ( double speed );
   virtual double               animateSpeed () const;
 
-  /// Usul::Interfaces::ICommandQueueAdd
-  virtual void                 addCommand ( Usul::Interfaces::ICommand* command );
-
  private:
   typedef VRV::Functors::BaseFunctor           Functor;
   typedef Functor::RefPtr                      FunctorPtr;
@@ -97,7 +84,6 @@ public:
   osg::Vec4f                                   _background;
   Thread::RefPtr                               _updateThread;
   Usul::Adaptors::Random < double >            _rand;
-  CommandQueue                                 _commandQueue;
   FunctorPtr                                   _navigator;
 };
 

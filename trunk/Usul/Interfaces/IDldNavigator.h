0428b1467c42749ab62d4ee6b52171d641c8f364
jsconner <jsconner@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-08-02 19:14:07 +0000

    - added interfaces to access materials, displaylists, and the memorypool for the triangleset and document classes.

---
+++ trunk/Usul/Interfaces/IDldNavigator.h
@@ -0,0 +1,46 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2007, Jeff Conner
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __USUL_INTERFACES_IDLDNAVIGATOR_H__
+#define __USUL_INTERFACES_IDLDNAVIGATOR_H__
+
+#include "Usul/Interfaces/IUnknown.h"
+
+//namespace osg { class Vec4; }
+
+namespace Usul {
+namespace Interfaces {
+
+
+struct IDldNavigator : public Usul::Interfaces::IUnknown
+{
+  /// Smart-pointer definitions.
+  USUL_DECLARE_QUERY_POINTERS ( IDldNavigator );
+
+  /// Id for this interface.
+  enum { IID = 2919146659u };
+
+  // decrement file position counter
+  virtual void              decrementFilePosition () = 0;
+
+  // increment file position counter
+  virtual void              incrementFilePosition () = 0;
+
+  // send the signal to load the file in the current file position
+  virtual void              loadCurrentFile() = 0;
+
+
+}; //struct IVertices
+
+
+
+}
+}
+
+#endif // __USUL_INTERFACES_IDLDNAVIGATOR_H__

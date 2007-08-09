85884d279d9377fb328e5361cdd612aba79c0658
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-08-09 20:03:32 +0000

    -Adding thread creation function.

--- trunk/DotNet/Init/Init.cpp
+++ trunk/DotNet/Init/Init.cpp
@@ -11,11 +11,15 @@
 #include "Init.h"
 
 #include "Threads/OpenThreads/Mutex.h"
+#include "Threads/OpenThreads/Thread.h"
+
 #include "Usul/Threads/Mutex.h"
+#include "Usul/Threads/Manager.h"
 
 using namespace CadKit::Init::Glue;
 
 Init::Init()
 {
   Usul::Threads::SetMutexFactory factory ( &Threads::OT::newOpenThreadsMutex );
+  Usul::Threads::Manager::instance().factory ( &Threads::OT::newOpenThreadsThread );
 }

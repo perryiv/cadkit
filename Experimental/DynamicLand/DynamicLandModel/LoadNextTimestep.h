ba0791598fa932cc2797825b934f5d9952fdb227
jsconner <jsconner@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-08-15 00:25:38 +0000

    - added a command to cycle to and load the next timestep

---
+++ Experimental/DynamicLand/DynamicLandModel/LoadNextTimestep.h
@@ -0,0 +1,37 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2007, Arizona State University
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//  Author(s): Adam Kubach
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __DynamicLand_LOAD_NEXT_TIMESTEP_COMMAND_H__
+#define __DynamicLand_LOAD_NEXT_TIMESTEP_COMMAND_H__
+
+#include "Usul/Commands/Command.h"
+
+
+class LoadNextTimestep : public Usul::Commands::Command
+{
+public:
+  typedef Usul::Commands::Command BaseClass;
+
+  // Smart-pointer definitions.
+  USUL_DECLARE_REF_POINTERS ( LoadNextTimestep );
+
+  LoadNextTimestep ( Usul::Interfaces::IUnknown* caller );
+
+protected:
+  virtual ~LoadNextTimestep ();
+
+  virtual void _execute ();
+  virtual void _dummy ();
+
+
+};
+
+
+#endif // __DynamicLand_LOAD_NEXT_TIMESTEP_COMMAND_H__

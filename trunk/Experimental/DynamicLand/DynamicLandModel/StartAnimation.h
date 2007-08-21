bc12df0084b864648464264927e8b04cd7790eb8
jsconner <jsconner@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-08-21 01:14:57 +0000

    - added job loading in the background - added animation functionality

---
+++ trunk/Experimental/DynamicLand/DynamicLandModel/StartAnimation.h
@@ -0,0 +1,37 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2007, Arizona State University
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//  Author(s): Adam Kubach
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __DynamicLand_START_ANIMATION_COMMAND_H__
+#define __DynamicLand_START_ANIMATION_COMMAND_H__
+
+#include "Usul/Commands/Command.h"
+
+
+class StartAnimation : public Usul::Commands::Command
+{
+public:
+  typedef Usul::Commands::Command BaseClass;
+
+  // Smart-pointer definitions.
+  USUL_DECLARE_REF_POINTERS ( StartAnimation );
+
+  StartAnimation ( Usul::Interfaces::IUnknown* caller );
+
+protected:
+  virtual ~StartAnimation ();
+
+  virtual void _execute ();
+  virtual void _dummy ();
+
+
+};
+
+
+#endif // __DynamicLand_START_ANIMATION_COMMAND_H__

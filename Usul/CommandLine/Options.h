29d7a3f92bbe8866379f44763ce41cdfea162029
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-08-24 00:43:31 +0000

    Added some more ways to use the Options class.

--- Usul/CommandLine/Options.h
+++ Usul/CommandLine/Options.h
@@ -18,6 +18,7 @@
 #define __USUL_COMMAND_LINE_OPTIONS_H__
 
 #include "Usul/Export/Export.h"
+#include "Usul/Strings/Convert.h"
 
 #include <map>
 #include <string>
@@ -36,42 +37,64 @@ class USUL_EXPORT Options
   Options();
 
   void insert ( const std::string&, const std::string& );
+  void insert ( const Options & );
 
   // Does this option exist?
   bool option ( const std::string& ) const;
 
   // Get the value for the option.
-  std::string value ( const std::string& ) const;
+  std::string value ( const std::string& option, const std::string &defaultValue = "" ) const;
 
   /**
    * @brief Returns the given option as a string value
    * @param option A named Option
    * @return The value of the option as a string
    */
-  std::string stringValue ( const std::string& option ) const;
+  std::string stringValue ( const std::string& option, const std::string &defaultValue = "" ) const;
 
   /**
    * @brief Returns the given option as a integer value
    * @param option A named Option
    * @return The value of the option as a integer
    */
-  unsigned long int intValue (const std::string& option) const;
+  unsigned long longValue ( const std::string& option, unsigned long defaultValue = 0 ) const;
  
   /**
    * @brief Returns the given option as a float value
    * @param option A named Option
    * @return The value of the option as a float
    */
-  float floatValue(const std::string& option) const;
+  float floatValue ( const std::string& option, float defaultValue = 0.0f ) const;
 
+  // Iterators.
+  iterator        begin()       { return _options.begin(); }
+  const_iterator  begin() const { return _options.begin(); }
+  iterator        end()         { return _options.end();   }
+  const_iterator  end()   const { return _options.end();   }
 
-  iterator begin() { return _options.begin(); }
-  iterator end() { return _options.end(); }
+  // Get the option.
+  template < class T > void get ( const std::string &option, T &t, const T &defaultValue )
+  {
+    Map::const_iterator iter = _options.find ( option );
+    t = ( ( iter != _options.end() ) ? Usul::Strings::fromString<T> ( iter->second ) : defaultValue );
+  }
 
-  const_iterator begin() const { return _options.begin(); }
-  const_iterator end()   const { return _options.end();   }
+  // Get the option.
+  template < class T > T get ( const std::string &option, const T &defaultValue )
+  {
+    T t;
+    this->get<T> ( option, t, defaultValue );
+    return t;
+  }
 
- private:
+  // Remove the option.
+  void remove ( const std::string &name )
+  {
+    _options.erase ( name );
+  }
+
+private:
+
   Map _options;
 };
 

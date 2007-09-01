5e25d4c7bd7ea5c68fa7b0b722e965aaca9f4bc5
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-09-01 20:45:49 +0000

    Adding perl scripts to stitch walls together. They are better than compiled binaries at accepting a large number of command-line arguments.

--- trunk/Tools/Scripts/stitch_5_walls.pl
+++ trunk/Tools/Scripts/stitch_5_walls.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 ################################################################################
 #
-#  Stitch 3 walls together.
+#  Stitch 5 walls together.
 #
 ################################################################################
 
@@ -13,7 +13,9 @@ while ( @ARGV )
   my $file0 = shift @ARGV;
   my $file1 = shift @ARGV;
   my $file2 = shift @ARGV;
-  my $command = sprintf ( "stitch_images --input $file2 $file0 $file1 --output %09d.jpg", $output );
+  my $file3 = shift @ARGV;
+  my $file4 = shift @ARGV;
+  my $command = sprintf ( "stitch_images --input $file4 $file2 $file0 $file1 $file3 --output %09d.jpg", $output );
   print "$command\n";
   `$command`;
   $output++;

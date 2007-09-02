de2dd0262c68f8d63aa0572f8703a72168a5605c
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-09-02 07:41:43 +0000

    -Adding plugin to list layers of current document.  Not implemented yet.  Just a skeleton plugin.

---
+++ trunk/Minerva/Plugins/LayerManager/LayersTree.h
@@ -0,0 +1,26 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2007, Arizona State University
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//  Author: Adam Kubach
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __MINERVA_PLUGINS_LAYERS_TREE_H__
+#define __MINERVA_PLUGINS_LAYERS_TREE_H__
+
+#include "QtGui/QTreeWidget"
+
+class LayersTree : public QTreeWidget
+{
+public:
+  typedef QTreeWidget BaseClass;
+
+  LayersTree ( QWidget *parent = 0x0 );
+  virtual ~LayersTree ();
+};
+
+
+#endif __MINERVA_PLUGINS_LAYERS_TREE_H__

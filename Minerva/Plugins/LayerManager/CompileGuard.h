b82a1e8ba564927d8fa3774a8c5446dd7972d213
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-09-02 07:41:43 +0000

    -Adding plugin to list layers of current document.  Not implemented yet.  Just a skeleton plugin.

---
+++ Minerva/Plugins/LayerManager/CompileGuard.h
@@ -0,0 +1,25 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2007, Arizona State University
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//  Created by: Adam Kubach
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  This header file should not be included in client code.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _LAYER_MANAGER_COMPILE_GUARD_H_
+#define _LAYER_MANAGER_COMPILE_GUARD_H_
+
+#ifndef _COMPILING_LAYER_MANAGER
+# error "This header file is only used for compiling the library."
+# error "It should not be included in client code."
+#endif
+
+#endif // _LAYER_MANAGER_COMPILE_GUARD_H_
---
+++ Minerva/Plugins/LayerManager/LayerManagerComponent.h
@@ -0,0 +1,62 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2007, Arizona State University
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//  Author: Adam Kubach
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _LAYER_MANAGER_COMPONENT_H_
+#define _LAYER_MANAGER_COMPONENT_H_
+
+#include "Minerva/Plugins/LayerManager/CompileGuard.h"
+
+#include "Usul/Base/Referenced.h"
+#include "Usul/Interfaces/IPlugin.h"
+#include "Usul/Interfaces/IActiveDocumentListener.h"
+
+class LayerManagerComponent : public Usul::Base::Referenced,
+                              public Usul::Interfaces::IPlugin,
+                              public Usul::Interfaces::IActiveDocumentListener
+{
+public:
+
+  /// Typedefs.
+  typedef Usul::Base::Referenced                                   BaseClass;
+  typedef Usul::Interfaces::IUnknown                               Unknown;
+
+  /// Smart-pointer definitions.
+  USUL_DECLARE_REF_POINTERS ( LayerManagerComponent );
+
+  /// Usul::Interfaces::IUnknown members.
+  USUL_DECLARE_IUNKNOWN_MEMBERS;
+
+  /// Constructor
+  LayerManagerComponent();
+  
+protected:
+
+  /// Return name of plugin.
+  virtual std::string           getPluginName() const;
+
+  // Do not copy.
+  LayerManagerComponent ( const LayerManagerComponent & );
+  LayerManagerComponent &operator = ( const LayerManagerComponent & );
+
+  /// Use reference counting.
+  virtual ~LayerManagerComponent();
+  
+  /// The active document has changed (IActiveDocumentListener).
+  virtual void                          activeDocumentChanged ( Usul::Interfaces::IUnknown *oldDoc, Usul::Interfaces::IUnknown *newDoc );
+};
+
+
+#endif // _LAYER_MANAGER_COMPONENT_H_

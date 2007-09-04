37f4d928e675b720f46b119a20f7fa214df17e49
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-09-04 20:12:04 +0000

    Trying to control the bounding sphere (and resulting clipping planes) with a callback.

--- Experimental/StarSystem/Body.cpp
+++ Experimental/StarSystem/Body.cpp
@@ -23,14 +23,54 @@
 
 #include "ossimPlanet/ossimPlanet.h"
 #include "ossimPlanet/ossimPlanetLand.h"
+#include "ossimPlanet/ossimPlanetLandModel.h"
 
 #include "osgDB/DatabasePager"
 
 #include "osg/Group"
 
+#include <limits>
+
 using namespace StarSystem;
 
 STAR_SYSTEM_IMPLEMENT_NODE_CLASS ( Body );
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Callback for setting the bounding sphere.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+namespace Helper
+{
+  class BoundingSphereCallback : public osg::Node::ComputeBoundingSphereCallback
+  {
+  public:
+
+    BoundingSphereCallback ( StarSystem::Body *body ) : _body ( body )
+    {
+    }
+
+    virtual osg::BoundingSphere computeBound ( const osg::Node & ) const
+    {
+      const double radius ( _body->maxRadius() );
+      const double maxFloat ( std::numeric_limits<float>::max() );
+      if ( radius > maxFloat )
+        std::cout << "Error 1058343654: bounding sphere is bigger than maximum float" << std::endl;
+      const StarSystem::Body::Vec3d center ( _body->center() );
+      return osg::BoundingSphere ( osg::Vec3d ( center[0], center[1], center[2] ), static_cast<float> ( radius ) );
+    }
+
+  protected:
+
+    virtual ~BoundingSphereCallback(){}
+
+  private:
+
+    StarSystem::Body *_body; // Raw pointer to avoid circular reference.
+  };
+}
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -41,6 +81,7 @@ STAR_SYSTEM_IMPLEMENT_NODE_CLASS ( Body );
 
 Body::Body() : BaseClass(),
   _planet    ( new ossimPlanet() ),
+  _landModel ( 0x0 ),
   _transform ( new osg::MatrixTransform() )
 {
   USUL_TRACE_SCOPE;
@@ -52,16 +93,25 @@ Body::Body() : BaseClass(),
   // Add the planet to the transform.
   _transform->addChild ( _planet );
 
+  const ossimPlanetLandType landType ( ossimPlanetLandType_ELLIPSOID );
+  //const ossimPlanetLandType landType ( ossimPlanetLandType_NORMALIZED_ELLIPSOID );
+
   // Set the default attributes.
-  _planet->getLand()->setLandType ( ossimPlanetLandType_ELLIPSOID );
-  //_planet->getLand()->setLandType ( ossimPlanetLandType_NORMALIZED_ELLIPSOID );
+  _planet->getLand()->setLandType ( landType );
   _planet->getLand()->setElevationEnabledFlag ( true );
   _planet->getLand()->setHeightExag ( 1.0 );
   _planet->getLand()->setElevationPatchSize ( 16 );
   _planet->getLand()->setMaxLevelDetail ( 32 );
   _planet->setEnableEphemerisFlag ( false );
-  _planet->setEnableHudFlag ( true );
+  _planet->setEnableHudFlag ( false );
   _planet->getLand()->resetGraph();
+
+  // Set the bounding sphere callback.
+  _planet->setComputeBoundingSphereCallback ( new Helper::BoundingSphereCallback ( this ) );
+
+  // Shortcut to the land.
+  _landModel = dynamic_cast < ossimPlanetEllipsoidLandModel * > ( _planet->getLand()->getModel().get() );
+  Usul::Pointers::reference ( _landModel );
 }
 
 
@@ -87,8 +137,12 @@ Body::~Body()
 void Body::_destroy()
 {
   USUL_TRACE_SCOPE;
-  _planet->unref(); _planet = 0x0;
-  _transform->unref(); _transform = 0x0;
+
+  _planet->setComputeBoundingSphereCallback ( 0x0 );
+
+  Usul::Pointers::unreference ( _planet );    _planet    = 0x0;
+  Usul::Pointers::unreference ( _landModel ); _landModel = 0x0;
+  Usul::Pointers::unreference ( _transform ); _transform = 0x0;
 }
 
 
@@ -146,4 +200,18 @@ Body::Vec3d Body::center() const
   Guard guard ( this->mutex() );
   const osg::Vec3d c ( _transform->getMatrix().getTrans() );
   return Vec3d ( c[0], c[1], c[2] );
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Get the maximum radius.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+double Body::maxRadius() const
+{
+  USUL_TRACE_SCOPE;
+  Guard guard ( this->mutex() );
+  return osg::WGS_84_RADIUS_EQUATOR;
 }

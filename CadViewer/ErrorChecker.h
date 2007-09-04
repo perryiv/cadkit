0eed37c9c83b04928faaeaa8b8e638535ec54389
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-09-04 21:02:39 +0000

    -Removing warning checker. -Uses composite policy in both debug and release.

--- CadViewer/ErrorChecker.h
+++ CadViewer/ErrorChecker.h
@@ -20,7 +20,6 @@
 #include "Usul/Errors/PrintingPolicy.h"
 #include "Usul/Errors/ThrowingPolicy.h"
 #include "Usul/Errors/CompositePolicy.h"
-#include "Usul/Errors/DoNothingPolicy.h"
 
 #include <stdexcept>
 
@@ -34,58 +33,27 @@ namespace CV {
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-#ifdef _DEBUG
-
-  #ifdef _MSC_VER
-
-    typedef Usul::Errors::CompositePolicy
-    <
-      Usul::Errors::AssertPolicy, 
-      Usul::Errors::ThrowingPolicy < std::runtime_error >
-    >
-    ErrorChecker;
-
-  #else
-
-    typedef Usul::Errors::CompositePolicy
-    <
-      Usul::Errors::PrintingPolicy, 
-      Usul::Errors::ThrowingPolicy < std::runtime_error >
-    >
-    ErrorChecker;
-
-  #endif
-
-#else
-
-  typedef Usul::Errors::DoNothingPolicy ErrorChecker;
-
-#endif
-
-
-///////////////////////////////////////////////////////////////////////////////
-//
-//  Specify the type of warning checking.
-//
-///////////////////////////////////////////////////////////////////////////////
 
 #ifdef _MSC_VER
 
   typedef Usul::Errors::CompositePolicy
   <
-    Usul::Errors::PrintingPolicy,
-    Usul::Errors::AssertPolicy
+    Usul::Errors::AssertPolicy, 
+    Usul::Errors::ThrowingPolicy < std::runtime_error >
   >
-  WarningChecker;
+  ErrorChecker;
 
 #else
 
-  typedef Usul::Errors::PrintingPolicy WarningChecker;
+  typedef Usul::Errors::CompositePolicy
+  <
+    Usul::Errors::PrintingPolicy, 
+    Usul::Errors::ThrowingPolicy < std::runtime_error >
+  >
+  ErrorChecker;
 
 #endif
 
-
-}; // namespace CV
-
+}
 
 #endif // _CV_ERROR_CHECKER_H_

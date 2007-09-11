edbee9c75e7833010e83a21ed32612bf01402d25
jsconner <jsconner@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-09-11 21:16:06 +0000

    - TriangleDocument additions to read grass raster files.  File format is .grs and is an xml file with the grass database directory ( directory containing cellhd and fcell directories ) and the filename to load. - I have included a sample .grs file called "raster.grs" - After this change in Windows "plug: Triangle Model" now depends on XMLTree as well.

---
+++ Helios/Plugins/TriangleModel/TriangleReaderGrassRaster.cpp
@@ -0,0 +1,470 @@
+#include "TriangleReaderGrassRaster.h"
+
+#include "Usul/File/Stats.h"
+#include "Usul/Trace/Trace.h"
+#include "Usul/Math/Vector3.h"
+#include "Usul/Math/Vector2.h"
+#include "Usul/Predicates/CloseFloat.h"
+#include "Usul/Policies/Update.h"
+#include "Usul/Endian/Endian.h"
+#include "Usul/Strings/Convert.h"
+#include "Usul/Strings/Case.h"
+
+
+
+#include <algorithm>
+#include <fstream>
+#include <stdexcept>
+#include <vector>
+
+//USUL_IMPLEMENT_IUNKNOWN_MEMBERS ( TriangleReaderGrassRaster, TriangleReaderGrassRaster::BaseClass );
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Constructor
+//
+///////////////////////////////////////////////////////////////////////////////
+
+TriangleReaderGrassRaster::TriangleReaderGrassRaster( const std::string &file, Usul::Interfaces::IUnknown *caller, TriangleDocument *doc ):
+BaseClass(),
+_dir            ( "C:/data/grassraster/" ),
+_caller         ( caller ),
+//_file           ( "ziqlab_watersheds_15m_dem_clipped", Usul::File::size ( file ) ),
+_file           ( "zz15m", Usul::File::size ( "zz15m" ) ),
+_document       ( doc ),
+_xmlFilename    ( file )
+{
+  
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Destructor
+//
+///////////////////////////////////////////////////////////////////////////////
+
+TriangleReaderGrassRaster::~TriangleReaderGrassRaster()
+{
+	USUL_TRACE_SCOPE;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Helper class for reading a stream.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+namespace Helper
+{
+  
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Functor to swap bytes in containers.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+struct SwapBytes
+{
+  
+  void operator () ( float &v ) const
+  {
+    Usul::Endian::FromBigToSystem::convert ( v );
+  }
+  void operator () ( double &v ) const
+  {
+    Usul::Endian::FromBigToSystem::convert ( v );
+  }
+  void operator () ( int &v ) const
+  {
+    Usul::Endian::FromBigToSystem::convert ( v );
+  }
+  
+};
+
+  struct ReadStream
+  {
+    
+    ReadStream ( const std::string &file, std::istream &in ) : _file ( file ), _in ( in )
+    {
+    }
+    ReadStream ( const ReadStream &r ) : _file ( r._file ), _in ( r._in )
+    {
+    }
+    template < class T > void operator () ( T &t )
+    {
+      if ( _in.eof() )
+      {
+        throw std::runtime_error ( "Error 3191797615: Unexpected end of file when reading: " + _file );
+      }
+      _in >> t;
+    }
+  private:
+    ReadStream &operator = ( const ReadStream & );
+    std::string _file;
+    std::istream &_in;
+  };
+}
+
+
+
+/////////////////
+//  PUBLIC
+/////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Read the file.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+osg::Node* TriangleReaderGrassRaster::buildScene()
+{
+  USUL_TRACE_SCOPE;
+  osg::ref_ptr< osg::Group > group ( new osg::Group );
+
+  OsgTools::Triangles::TriangleSet::Options opt;
+  opt[ "normals" ] = "per-vertex";
+  opt[ "colors" ]  = "per-vertex";
+  std::cout << "Building scene..." << std::endl;
+//  group->addChild( _triangleSet->buildScene( opt, 0x0 ) );
+  group->addChild( _document->buildScene( opt, _caller ) );
+  return group.release();
+
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Read the file.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+void TriangleReaderGrassRaster::operator()()
+{
+  USUL_TRACE_SCOPE;
+  // Clear accumulated state.
+  this->clear();
+
+  //Read Parameters from XML
+  this->_readXML();
+
+  // Read the file.
+  this->_read();
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Read the file.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+void TriangleReaderGrassRaster::read()
+{
+  USUL_TRACE_SCOPE;
+  // Clear accumulated state.
+  this->clear();
+
+  //Read Parameters from XML
+  this->_readXML();
+
+  // Read the file.
+  this->_read();
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Initialize. Note: we do not clear the reference-counted objects because 
+//  we hand those to the document.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+void TriangleReaderGrassRaster::clear()
+{
+  USUL_TRACE_SCOPE;
+}
+
+
+/////////////////
+//  PROTECTED
+/////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Parse the xml grass raster file information.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+void TriangleReaderGrassRaster::_readXML()
+{
+  USUL_TRACE_SCOPE;
+  //Guard guard ( this->mutex() );
+
+  XmlTree::Document::RefPtr document ( new XmlTree::Document );
+  document->load ( _xmlFilename );
+
+  if ( "grs" == document->name() )
+  {
+    // if the parameter file is loaded correctly go ahead and load the 1st map
+    this->_parseXML( *document, _caller );
+  }
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Parse the xml grass raster file information.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+void TriangleReaderGrassRaster::_parseXML( XmlTree::Node &node, Usul::Interfaces::IUnknown *caller )
+{
+  USUL_TRACE_SCOPE;
+  //Guard guard ( this->mutex() );
+  typedef XmlTree::Document::Attributes Attributes;
+  typedef XmlTree::Document::Children Children;
+
+  Attributes& attributes ( node.attributes() );
+  for ( Attributes::iterator iter = attributes.begin(); iter != attributes.end(); ++iter )
+  {
+    if ( "directory" == iter->first )
+    {
+      Usul::Strings::fromString ( iter->second, _dir );
+    }
+    if ( "prefix" == iter->first )
+    {
+      Usul::Strings::fromString ( iter->second, _file.first );
+      _file.second = Usul::File::size ( _file.first );
+    }
+  }
+
+#if 0
+  Children& children ( node.children() );
+	
+  for ( Children::iterator iter = children.begin(); iter != children.end(); ++iter )
+  {
+    XmlTree::Node::RefPtr node ( *iter );
+    if ( "set" == node->name() )
+    {
+      
+    }
+    
+  }
+#endif  
+
+
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Read the headerfile.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+void TriangleReaderGrassRaster::_readHeader()
+{
+  USUL_TRACE_SCOPE;
+  // Open a stream with a large buffer.
+  const unsigned long int bufSize ( 4095 );
+  char buffer[bufSize+1];
+  std::ifstream in;
+  in.rdbuf()->pubsetbuf ( buffer, bufSize );
+  std::string filename = _dir + "cellhd/" + _file.first;
+  in.open ( filename.c_str() );
+  if ( !in.is_open() )
+    throw std::runtime_error ( "Error 1099205557: Failed to open file: " + filename );
+  
+  //Read the header file
+  {
+    std::string label;
+    in >> label >> _header.proj;      // projection
+    in >> label >> _header.zone;      // zone
+    in >> label >> _header.north;     // north
+    in >> label >> _header.south;     // south
+    in >> label >> _header.east;      // east
+    in >> label >> _header.west;      // west
+    in >> label >> _header.cols;      // number of columns
+    in >> label >> _header.rows;      // number of rows
+    in >> label >> label >>_header.ew_resol;  // East/West Resolution
+    in >> label >> label >>_header.ns_resol;  // North/South Resolution
+    in >> label >> _header.format;
+    in >> label >> _header.compressed;
+  }
+  
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Read the file.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+void TriangleReaderGrassRaster::_read()
+{
+  USUL_TRACE_SCOPE;
+  // Read header file.
+  std::cout << "Reading Header Information..." << std::endl;
+  this->_readHeader();
+
+  // Initialize local vaiables.
+  Usul::Math::Vec2ui gridSize ( _header.rows + 1 , _header.cols +1 );
+  Usul::Math::Vec2d lowerLeft ( _header.west, _header.south );
+  float cellSize ( _header.ns_resol );
+  float noDataValue ( 0 );
+
+  // Useful typedefs.
+  typedef std::vector<float> Values;
+  typedef osg::Vec3f Vec3;
+  typedef Usul::Predicates::CloseFloat<double> Close;
+
+  // More local variables.
+  const unsigned int numPoints ( gridSize[0] * gridSize[1] );
+  Vec3 a0 ( 0, 0, 0 ), a1 ( 0, 0, 0 ), b0 ( 0, 0, 0 ), b1 ( 0, 0, 0 );
+  Close close ( 0.0005 ); // Reasonable?
+  Usul::Policies::TimeBased update ( 1000 ); // Every second.
+
+
+  // Open a stream with a large buffer.
+  const unsigned long int bufSize ( 4095 );
+  char buffer[bufSize+1];
+  std::ifstream in;
+  in.rdbuf()->pubsetbuf ( buffer, bufSize );
+  std::string filename = _dir + "fcell/" + _file.first;
+  in.open ( filename.c_str(), std::ios::in | std::ios::binary );
+  //std::ifstream in ( filename.c_str(), std::ios::in | std::ios::binary );
+  if ( !in.is_open() )
+    throw std::runtime_error ( "Error 1099205557: Failed to open file: " + filename );
+
+
+  typedef float ValueType;
+  typedef std::vector<ValueType> ValueVector;
+  ValueVector vertices ( numPoints, 0 );
+  in.read ( reinterpret_cast<char *> ( &vertices[0] ), vertices.size() * sizeof ( ValueVector::value_type ) );
+  
+  // Swap the bytes.
+  std::for_each ( vertices.begin(), vertices.end(), Helper::SwapBytes() );
+ 
+  
+
+  //delete old triangles
+  //_triangleSet->clear();
+
+  // Reserve triangles.
+  // NOTE: commented out this line to debug a bad_alloc error happening
+  //       in reserve, called by reserveTriangles( ... )
+  //_triangleSet->reserveTriangles ( gridSize[0] * gridSize[1] );
+
+  // Grab the first row.
+  //std::for_each ( row0.begin(), row0.end(), vertices );
+
+  
+
+  std::cout << "Reading Vertices..." << std::endl;
+  // Loop until we've read all the points.
+  const unsigned int stopR ( gridSize[0] - 1 );
+  // Set the progress numbers.
+  _progress.first = 0;
+  _progress.second = stopR;
+
+  for ( unsigned int i = 0; i < stopR; ++i )
+  {
+    // Grab the next row.
+    //std::for_each ( row1.begin(), row1.end(), vertices );
+
+    // Shortcuts.
+    const Values::value_type xa ( lowerLeft[1] + ( ( i     ) * cellSize ) );
+    const Values::value_type xb ( lowerLeft[1] + ( ( i + 1 ) * cellSize ) );
+
+    // Loop over the columns.
+    const unsigned int stopC ( gridSize[1] - 1 );
+    for ( unsigned int j = 0; j < stopC; ++j)
+    {
+      // Shortcuts.
+      const Values::value_type y0 ( lowerLeft[0] + ( ( j     ) * cellSize ) );
+      const Values::value_type y1 ( lowerLeft[0] + ( ( j + 1 ) * cellSize ) );
+
+      // Calculate vertices of current triangles.
+      
+      const Vec3 a ( xa, y0, vertices.at ( ( i * stopC ) + j ) );
+      const Vec3 b ( xa, y1, vertices.at ( ( i * stopC ) + ( j + 1 ) ) );
+      const Vec3 c ( xb, y0, vertices.at ( ( ( i + 1 ) * stopC ) + j     ) );
+      const Vec3 d ( xb, y1, vertices.at ( ( ( i + 1 ) * stopC ) + ( j + 1 ) ) );
+      
+
+      // See if points are close to "no data".
+      if ( false == close ( d[2], noDataValue ) &&
+           false == close ( a[2], noDataValue ) )
+      {
+        // Add first triangle.
+        if ( false == close ( c[2], noDataValue ) )
+        {
+          Vec3 n ( ( d - c ) ^ ( a - c ) );
+          n.normalize();
+          OsgTools::Triangles::Triangle::RefPtr t ( _document->addTriangle ( a, c, d, n, false ) );
+          t->original ( true );
+          //_triangleSet->addTriangle ( a, c, d, n, false );
+        }
+
+        // Add second triangle.
+        if ( false == close ( b[2], noDataValue ) )
+        {
+          Vec3 n ( ( b - d ) ^ ( a - d ) );
+          n.normalize();
+          OsgTools::Triangles::Triangle::RefPtr t ( _document->addTriangle ( a, d, b, n, false ) );
+          t->original ( true );
+          //_triangleSet->addTriangle ( a, d, b, n, false );
+        }
+      }
+
+         }
+    //std::cout << "\rPercent Complete: " << i << "/" << static_cast<float>( numPoints / gridSize[0] ) <<  std::flush;
+    // Feedback.
+      this->_incrementProgress ( update(), static_cast<float>( i ), static_cast<float>( numPoints / stopC ) );
+ 
+    // Swap rows.
+    //row0.swap ( row1 );
+  }
+  
+  
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Update the progress bar.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+void TriangleReaderGrassRaster::_incrementProgress ( bool state, float i, float num )
+{
+  USUL_TRACE_SCOPE;
+  std::cout << "Percent Complete: " << i << "/" << num << "( " <<  ( i / num ) * 100 << " )\r" << std::flush;
+  unsigned int &numerator   ( _progress.first  );
+  unsigned int &denominator ( _progress.second );
+  _document->setProgressBar ( state, numerator, denominator, _caller );
+  ++numerator;
+  USUL_ASSERT ( numerator <= denominator );
+}
+
+
+/////////////////
+//  PRIVATE
+/////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+///////////////////////////////////////////////////////////////////////////////
+
+
---
+++ Helios/Plugins/TriangleModel/TriangleReaderGrassRaster.h
@@ -0,0 +1,92 @@
+#ifndef __TriangleReaderGrassRaster_H__
+#define __TriangleReaderGrassRaster_H__
+
+//#include <string>
+
+#include "Usul/Interfaces/IUnknown.h"
+#include "Usul/Base/Referenced.h"
+#include "Usul/Pointers/Pointers.h"
+#include "Usul/Types/Types.h"
+
+#include "OsgTools/Triangles/TriangleSet.h"
+#include "Helios/Plugins/TriangleModel/TriangleDocument.h"
+
+#include "XmlTree/Document.h"
+
+class TriangleReaderGrassRaster : public Usul::Base::Referenced
+{
+public:
+
+  typedef Usul::Base::Referenced BaseClass;
+  typedef OsgTools::Triangles::TriangleSet::RefPtr TriangleSetPtr;
+  typedef TriangleDocument::ValidRefPtr TriangleDocumentPtr;
+  typedef Usul::Interfaces::IUnknown IUnknown;
+  typedef std::pair < std::string, Usul::Types::Uint64 > FileInfo;
+  typedef std::pair < unsigned int, unsigned int > Progress;
+  typedef osg::ref_ptr< osg::Vec3Array > Vec3ArrayPtr;
+
+  struct Header
+  { 
+    unsigned int    proj;
+    unsigned int    zone;
+    double          north;
+    double          south;
+    double          east;
+    double          west;
+    unsigned int    cols;
+    unsigned int    rows;
+    double          ew_resol;
+    double          ns_resol;
+    int             format;
+    int             compressed;
+ };
+
+  USUL_DECLARE_REF_POINTERS ( TriangleReaderGrassRaster );
+
+  /// Usul::Interfaces::IUnknown members.
+  //USUL_DECLARE_IUNKNOWN_MEMBERS;
+
+  TriangleReaderGrassRaster ( const std::string &file, Usul::Interfaces::IUnknown *caller, TriangleDocument *doc  );
+  virtual                   ~TriangleReaderGrassRaster();
+
+  // Read the file.
+  void                operator()();
+
+  // Clear accumulated state.
+  void                      clear();
+
+  // Read the file.
+  void                      read();
+
+  // Build the scene
+  osg::Node*                buildScene();
+
+
+protected:
+
+  
+
+  void                      _read();
+  void                      _readHeader();
+  void                      _incrementProgress ( bool state, float i, float num );
+  
+  void                      _readXML();
+  void                      _parseXML( XmlTree::Node &node, Usul::Interfaces::IUnknown *caller );
+
+private:
+   // No copying.
+  TriangleReaderGrassRaster ( const TriangleReaderGrassRaster & );
+  TriangleReaderGrassRaster &operator = ( const TriangleReaderGrassRaster & );
+
+  Header                    _header; 
+  std::string               _dir;
+  //TriangleSetPtr            _triangleSet;
+  IUnknown::RefPtr          _caller;
+  Progress                  _progress;
+  FileInfo                  _file;
+  TriangleDocumentPtr       _document;
+  std::string               _xmlFilename;
+
+};
+
+#endif // __TriangleReaderGrassRaster_H__

8a716cb8220de921679d8a801d4d872de4a4fc91
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-10-04 00:49:32 +0000

    -Adding plugin to add wms layers.

---
+++ Minerva/Plugins/WmsLayerQt/AddWmsLayerWidget.h
@@ -0,0 +1,42 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2007, Arizona State University
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//  Author(s): Adam Kubach
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __ADD_WMS_LAYER_WIDGET_H__
+#define __ADD_WMS_LAYER_WIDGET_H__
+
+#include "Minerva/Plugins/WmsLayerQt/CompileGuard.h"
+
+#include "QtGui/QWidget"
+
+#include <vector>
+#include <string>
+
+class QLineEdit;
+
+namespace Usul { namespace Interfaces { struct IUnknown; } }
+
+class AddWmsLayerWidget : public QWidget
+{
+  Q_OBJECT;
+public:
+  typedef QWidget BaseClass;
+
+  AddWmsLayerWidget( QWidget *parent = 0x0 );
+  virtual ~AddWmsLayerWidget();
+
+  void apply ( Usul::Interfaces::IUnknown * caller );
+
+private:
+  QLineEdit *_server;
+  QLineEdit *_cacheDirectory;
+};
+
+
+#endif // __ADD_WMS_LAYER_WIDGET_H__
---
+++ Minerva/Plugins/WmsLayerQt/WmsLayerQtComponent.h
@@ -0,0 +1,68 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2007, Arizona State University
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//  Created by: Adam Kubach
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _WMS_LAYER_QT_H_
+#define _WMS_LAYER_QT_H_
+
+#include "Minerva/Plugins/WmsLayerQt/CompileGuard.h"
+#include "Minerva/Plugins/WmsLayerQt/AddWmsLayerWidget.h"
+
+#include "Usul/Base/Referenced.h"
+#include "Usul/Interfaces/IPlugin.h"
+#include "Usul/Interfaces/ILayerAddGUIQt.h"
+
+class WmsLayerQtComponent : public Usul::Base::Referenced,
+                            public Usul::Interfaces::IPlugin,
+                            public Usul::Interfaces::ILayerAddGUIQt
+
+{
+public:
+
+  /// Typedefs.
+  typedef Usul::Base::Referenced                                   BaseClass;
+  typedef Usul::Interfaces::IUnknown                               Unknown;
+
+  /// Smart-pointer definitions.
+  USUL_DECLARE_REF_POINTERS ( WmsLayerQtComponent );
+
+  /// Usul::Interfaces::IUnknown members.
+  USUL_DECLARE_IUNKNOWN_MEMBERS;
+
+  /// Constructor
+  WmsLayerQtComponent();
+  
+protected:
+
+  virtual QWidget*            layerAddGUI ( );
+  virtual std::string         name () const;
+  virtual void                apply ( Usul::Interfaces::IUnknown* caller );
+
+  /// Return name of plugin.
+  virtual std::string           getPluginName() const;
+
+  // Do not copy.
+  WmsLayerQtComponent ( const WmsLayerQtComponent & );
+  WmsLayerQtComponent &operator = ( const WmsLayerQtComponent & );
+
+  /// Use reference counting.
+  virtual ~WmsLayerQtComponent();
+
+private:
+  AddWmsLayerWidget *_widget;
+};
+
+
+#endif // _WMS_LAYER_QT_H_

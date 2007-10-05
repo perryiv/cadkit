eb7cea193cf52bc0fc2f529f828d829a54cdd46a
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-10-05 22:36:06 +0000

    -Forgot to add the animate plugin. -Setting the navigation manipulator in viewer when view matrix is set.

---
+++ trunk/Helios/Plugins/Animate/Frame.h
@@ -0,0 +1,59 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2007, Arizona State University
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//  Author: Adam Kubach
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __VRV_ANIMATE_FRAME_H__
+#define __VRV_ANIMATE_FRAME_H__
+
+#include "Usul/Base/Object.h"
+
+#include "Serialize/XML/Macros.h"
+
+#include "osg/Matrix"
+#include "osg/Quat"
+#include "osg/Vec3"
+
+namespace Animate {
+
+class Frame : public Usul::Base::Object
+{
+public:
+  typedef Usul::Base::Object BaseClass;
+
+  USUL_DECLARE_REF_POINTERS ( Frame );
+
+  /// Construction.
+  Frame ();
+  Frame ( const osg::Matrix& m );
+
+  /// Get/Set the matrix.
+  void                     matrix ( const osg::Matrix& m );
+  const osg::Matrix&       matrix () const;
+
+  /// Get the rotation.
+  osg::Quat                rotation () const;
+
+  /// Get the translation.
+  osg::Vec3                translation () const;
+
+protected:
+  virtual ~Frame ();
+
+private:
+  osg::Matrix _m;
+
+  SERIALIZE_XML_DEFINE_MAP;
+  SERIALIZE_XML_DEFINE_MEMBERS ( Frame );
+};
+
+
+}
+
+
+#endif // __VRV_ANIMATE_FRAME_H__
---
+++ trunk/Helios/Plugins/Animate/KeyFramePath.h
@@ -0,0 +1,84 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2007, Arizona State University
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//  Author: Adam Kubach
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __VRV_ANIMATE_KEY_FRAME_PATH_H__
+#define __VRV_ANIMATE_KEY_FRAME_PATH_H__
+
+#include "Helios/Plugins/Animate/Path.h"
+#include "Helios/Plugins/Animate/Frame.h"
+
+#include "Usul/Base/Object.h"
+
+#include "Serialize/XML/Macros.h"
+
+#include "GN/Config/UsulConfig.h"
+#include "GN/Splines/Curve.h"
+#include "GN/Evaluate/Point.h"
+#include "GN/Write/XML.h"
+
+#include <vector>
+
+namespace Usul { namespace Interfaces { struct IUnknown; } }
+
+namespace Animate {
+
+class KeyFramePath : public Animate::Path
+{
+public:
+  typedef Animate::Path                                          BaseClass;
+  typedef std::vector < Frame::RefPtr >                           Frames;
+  typedef GN::Config::UsulConfig < double, double, unsigned int > DoubleConfig;
+  typedef GN::Splines::Curve < DoubleConfig >                     DoubleCurve;
+  typedef DoubleCurve::IndependentSequence                        IndependentSequence;
+  typedef DoubleCurve::DependentContainer                         DependentContainer;
+  typedef DoubleCurve::DependentSequence                          DependentSequence;
+  typedef DoubleCurve::IndependentType                            Parameter;
+
+  USUL_DECLARE_REF_POINTERS ( KeyFramePath );
+
+  KeyFramePath ();
+
+  /// Clear.
+  virtual void      clear ();
+
+  /// Start the animation from beginning.
+  virtual void      start ( Usul::Interfaces::IUnknown * caller );
+
+  /// Animate one step.
+  virtual void      updateNotify ( Usul::Interfaces::IUnknown * caller );
+
+  /// Get/Set the number of animation steps.
+  void              steps ( unsigned int value );
+  unsigned int      steps ( ) const;
+
+protected:
+  virtual ~KeyFramePath ();
+
+  void              _interpolate ( );
+
+  /// Append a frame.
+  virtual void      _append ( Frame* frame );
+
+private:
+  Frames              _frames;
+  IndependentSequence _params;
+  DependentContainer  _rotations;
+  DoubleCurve         _curve;
+  unsigned int        _numberSteps;
+  unsigned int        _currentStep;
+
+  SERIALIZE_XML_DEFINE_MEMBERS ( KeyFramePath );
+};
+
+
+}
+
+
+#endif // __VRV_ANIMATE_PATH_H__

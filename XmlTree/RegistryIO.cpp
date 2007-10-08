c860792919aaf656ff06115183e31580756f2732
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-10-08 07:49:55 +0000

    -- Adding classes for working with Usul's registry. -- Reading and writing the registry from/to XML now.

---
+++ XmlTree/RegistryIO.cpp
@@ -0,0 +1,67 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2005, Perry L Miller IV
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Class that reads and writes Usul's registry.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include "XmlTree/RegistryIO.h"
+#include "XmlTree/Document.h"
+#include "XmlTree/Loader.h"
+#include "XmlTree/RegistryBuilder.h"
+#include "XmlTree/RegistryVisitor.h"
+#include "XmlTree/Writer.h"
+
+#include "Usul/Registry/Database.h"
+#include "Usul/Trace/Trace.h"
+
+using namespace XmlTree;
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Read the registry.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+void RegistryIO::read ( const std::string &file )
+{
+  USUL_TRACE_SCOPE;
+
+  // Read xml tree.
+  XmlTree::Loader loader;
+  XmlTree::Document::RefPtr document ( new XmlTree::Document );
+  loader.load ( file, document.get() );
+
+  // Build registry.
+  XmlTree::RegistryBuilder::RefPtr builder ( new XmlTree::RegistryBuilder );
+  builder->build ( document.get() );
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Write the registry.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+void RegistryIO::write ( const std::string &file )
+{
+  USUL_TRACE_SCOPE;
+
+  // Build xml tree.
+  XmlTree::RegistryVisitor::RefPtr visitor ( new XmlTree::RegistryVisitor );
+  Usul::Registry::Database::instance().accept ( visitor.get() );
+
+  // Write it to disk.
+  XmlTree::Writer writer;
+  writer.write ( visitor->document(), file );
+}
---
+++ XmlTree/RegistryIO.h
@@ -0,0 +1,38 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2005, Perry L Miller IV
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Class that reads and writes Usul's registry.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _XML_TREE_REGISTRY_IO_H_
+#define _XML_TREE_REGISTRY_IO_H_
+
+#include "XmlTree/Export.h"
+
+#include <string>
+
+
+namespace XmlTree {
+
+
+struct XML_TREE_EXPORT RegistryIO
+{
+  // Read/write the registry.
+  static void    read  ( const std::string &file );
+  static void    write ( const std::string &file );
+};
+
+
+} // namespace XmlTree
+
+
+#endif // _XML_TREE_REGISTRY_IO_H_

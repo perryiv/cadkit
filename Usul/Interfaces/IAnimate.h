e1480bc4db74cb5586dd7b1fa8ce7fd1e7908fa5
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-10-12 20:15:54 +0000

    -Removing unused functions from interface.

--- Usul/Interfaces/IAnimate.h
+++ Usul/Interfaces/IAnimate.h
@@ -7,16 +7,16 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-
 #ifndef __USUL_INTERFACES_ANIMATE_H__
 #define __USUL_INTERFACES_ANIMATE_H__
 
 #include "Usul/Interfaces/IUnknown.h"
 
-namespace osg { class Quat; class Vec3f; typedef Vec3f Vec3; };
+namespace osg { class Quat; class Vec3f; }
 
 namespace Usul {
 namespace Interfaces {
+
 
 struct IAnimate : public Usul::Interfaces::IUnknown
 {
@@ -27,20 +27,12 @@ struct IAnimate : public Usul::Interfaces::IUnknown
   enum { IID = 3673709138u };
 
   //Animate from current position to given position
-  virtual void animate ( const osg::Vec3& center, float distance, const osg::Quat& rotation ) = 0;
-  
-  //Animate from one position and orientation to another.
-  virtual void animate ( const osg::Vec3 &t1, const osg::Quat &r1, const osg::Vec3 &t2, const osg::Quat &r2, double duration ) = 0;
-
-  //Animate from one orientation to another.
-  virtual void animate ( const osg::Quat &r1, const osg::Quat &r2, double duration ) = 0;
-
+  virtual void animate ( const osg::Vec3f& center, float distance, const osg::Quat& rotation, Usul::Interfaces::IUnknown *caller = 0x0 ) = 0;
 }; 
 
-}
 
 }
-
+}
 
 #endif // __USUL_INTERFACES_ANIMATE_H__
 

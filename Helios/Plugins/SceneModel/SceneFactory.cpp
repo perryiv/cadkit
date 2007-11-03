fb88eaff08f4284ca1016b1cf00359e15749f1a2
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-11-03 17:53:52 +0000

    -Using Usul::Components::Factory

--- Helios/Plugins/SceneModel/SceneFactory.cpp
+++ Helios/Plugins/SceneModel/SceneFactory.cpp
@@ -12,12 +12,9 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-#include "SceneFactory.h"
-#include "SceneComponent.h"
+#include "Helios/Plugins/SceneModel/SceneComponent.h"
 
-#include "Usul/Components/Create.h"
-
-USUL_IMPLEMENT_IUNKNOWN_MEMBERS ( SceneFactory, SceneFactory::BaseClass );
+#include "Usul/Components/Factory.h"
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -44,74 +41,6 @@ extern "C" bool usul_is_debug_build()
 
 extern "C" Usul::Interfaces::IClassFactory *usul_get_class_factory()
 {
-  Usul::Interfaces::IClassFactory::ValidRefPtr factory ( new SceneFactory() );
+  Usul::Interfaces::IClassFactory::ValidRefPtr factory ( new Usul::Components::Factory<SceneComponent>() );
   return factory.release();
-}
-
-
-///////////////////////////////////////////////////////////////////////////////
-//
-//  Constructor.
-//
-///////////////////////////////////////////////////////////////////////////////
-
-SceneFactory::SceneFactory() : BaseClass()
-{
-}
-
-
-///////////////////////////////////////////////////////////////////////////////
-//
-//  Destructor.
-//
-///////////////////////////////////////////////////////////////////////////////
-
-SceneFactory::~SceneFactory()
-{
-}
-
-
-///////////////////////////////////////////////////////////////////////////////
-//
-//  Query for the interface.
-//
-///////////////////////////////////////////////////////////////////////////////
-
-Usul::Interfaces::IUnknown *SceneFactory::queryInterface ( unsigned long iid )
-{
-  switch ( iid )
-  {
-  case Usul::Interfaces::IClassFactory::IID:
-    return static_cast<Usul::Interfaces::IClassFactory*>(this);
-  case Usul::Interfaces::IUnknown::IID:
-    return static_cast<Usul::Interfaces::IUnknown*>(this);
-  default:
-    return 0x0;
-  }
-}
-
-
-///////////////////////////////////////////////////////////////////////////////
-//
-//  Create a component of the given interface type (defined by iid).
-//  Return an unknown pointer to this new component.
-//
-///////////////////////////////////////////////////////////////////////////////
-
-Usul::Interfaces::IUnknown *SceneFactory::createInstance ( unsigned long iid )
-{
-  // Require this to construct.
-  SceneComponent::ValidAccessRefPtr component ( new SceneComponent() );
-
-  // Do not require this to work.
-  Usul::Interfaces::IUnknown::RefPtr unknown ( component->queryInterface ( iid ) );
-
-  // Decrement this now so that we control the order of unreferencing.
-  // Reference count should go from 2 -> 1. If query failed above then 
-  // it's deleted here.
-  component = 0x0;
-
-  // Return the released pointer to the component.
-  // Reference count should go from 1 -> 0, but it should not delete.
-  return unknown.release();
 }
--- Helios/Plugins/TriangleModel/TriangleFactory.cpp
+++ Helios/Plugins/TriangleModel/TriangleFactory.cpp
@@ -13,13 +13,9 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-#include "TriangleFactory.h"
-#include "TriangleComponent.h"
+#include "Helios/Plugins/TriangleModel/TriangleComponent.h"
 
-#include "Usul/Components/Create.h"
-
-USUL_IMPLEMENT_IUNKNOWN_MEMBERS ( TriangleFactory, TriangleFactory::BaseClass );
-USUL_IMPLEMENT_TYPE_ID ( TriangleFactory );
+#include "Usul/Components/Factory.h"
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -46,74 +42,6 @@ extern "C" bool usul_is_debug_build()
 
 extern "C" Usul::Interfaces::IClassFactory *usul_get_class_factory()
 {
-  Usul::Interfaces::IClassFactory::ValidRefPtr factory ( new TriangleFactory() );
+  Usul::Interfaces::IClassFactory::ValidRefPtr factory ( new Usul::Components::Factory<TriangleComponent>() );
   return factory.release();
-}
-
-
-///////////////////////////////////////////////////////////////////////////////
-//
-//  Constructor.
-//
-///////////////////////////////////////////////////////////////////////////////
-
-TriangleFactory::TriangleFactory() : BaseClass()
-{
-}
-
-
-///////////////////////////////////////////////////////////////////////////////
-//
-//  Destructor.
-//
-///////////////////////////////////////////////////////////////////////////////
-
-TriangleFactory::~TriangleFactory()
-{
-}
-
-
-///////////////////////////////////////////////////////////////////////////////
-//
-//  Query for the interface.
-//
-///////////////////////////////////////////////////////////////////////////////
-
-Usul::Interfaces::IUnknown *TriangleFactory::queryInterface ( unsigned long iid )
-{
-  switch ( iid )
-  {
-  case Usul::Interfaces::IClassFactory::IID:
-    return static_cast<Usul::Interfaces::IClassFactory*>(this);
-  case Usul::Interfaces::IUnknown::IID:
-    return static_cast<Usul::Interfaces::IUnknown*>(this);
-  default:
-    return 0x0;
-  }
-}
-
-
-///////////////////////////////////////////////////////////////////////////////
-//
-//  Create a component of the given interface type (defined by iid).
-//  Return an unknown pointer to this new component.
-//
-///////////////////////////////////////////////////////////////////////////////
-
-Usul::Interfaces::IUnknown *TriangleFactory::createInstance ( unsigned long iid )
-{
-  // Require this to construct.
-  TriangleComponent::ValidAccessRefPtr component ( new TriangleComponent() );
-
-  // Do not require this to work.
-  Usul::Interfaces::IUnknown::RefPtr unknown ( component->queryInterface ( iid ) );
-
-  // Decrement this now so that we control the order of unreferencing.
-  // Reference count should go from 2 -> 1. If query failed above then 
-  // it's deleted here.
-  component = 0x0;
-
-  // Return the released pointer to the component.
-  // Reference count should go from 1 -> 0, but it should not delete.
-  return unknown.release();
 }

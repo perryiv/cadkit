b796b0d21deccc5f78604686dd7ba3f3195409aa
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-11-08 19:11:33 +0000

    -Adding base Delegate class for QtViewer.

---
+++ trunk/Helios/Qt/Views/OSG/Delegate.cpp
@@ -0,0 +1,108 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2007, Arizona State University
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//  Author(s): Adam Kubach
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  The delegate class.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include "Helios/Qt/Views/OSG/Delegate.h"
+#include "Helios/Qt/Views/OSG/Viewer.h"
+#include "Helios/Qt/Views/OSG/Format.h"
+
+#include "Usul/Documents/Document.h"
+#include "Usul/Interfaces/IBuildScene.h"
+#include "Usul/Interfaces/Qt/IWorkspace.h"
+
+#include "QtGui/QWorkspace"
+
+using namespace CadKit::Helios::Views::OSG;
+
+USUL_IMPLEMENT_IUNKNOWN_MEMBERS ( Delegate, Delegate::BaseClass );
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Constructor.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+Delegate::Delegate() : BaseClass()
+{
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Destructor.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+Delegate::~Delegate()
+{
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Query for the interface.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+Usul::Interfaces::IUnknown *Delegate::queryInterface ( unsigned long iid )
+{
+  switch ( iid )
+  {
+  case Usul::Interfaces::IUnknown::IID:
+  case Usul::Interfaces::IGUIDelegate::IID:
+    return static_cast < Usul::Interfaces::IGUIDelegate*>(this);
+  default:
+    return 0x0;
+  }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+//
+//  Build the default GUI
+//
+/////////////////////////////////////////////////////////////////////////////
+
+void Delegate::createDefaultGUI ( Usul::Documents::Document *document, Usul::Interfaces::IUnknown* caller )
+{
+  typedef CadKit::Helios::Views::OSG::Viewer QtViewer;
+
+  // Non-ref'ing smart-pointers that throw if given null.
+  typedef Usul::Pointers::Configs::NoRefCountingNullThrows Policy;
+  typedef Usul::Pointers::SmartPointer < QtViewer, Policy > QtViewerPtr;
+
+  Usul::Interfaces::Qt::IWorkspace::QueryPtr workspace ( caller );
+
+  if ( workspace.valid() )
+  {
+    QWorkspace *parent ( workspace->workspace() );
+    QtViewerPtr viewer ( new QtViewer ( document, CadKit::Helios::Views::OSG::defaultFormat(), parent ) );
+    parent->addWindow ( viewer.get() );
+
+    // We want mouse-move events even when there are no mouse buttons pressed.
+    viewer->setMouseTracking ( true );
+
+    // Build the scene.
+    Usul::Interfaces::IBuildScene::QueryPtr build ( document );
+    if ( build.valid () )
+    {
+      viewer->viewer()->scene ( build->buildScene ( document->options(), caller ) );
+    }
+
+    // Show the window.
+    viewer->show();
+  }
+}

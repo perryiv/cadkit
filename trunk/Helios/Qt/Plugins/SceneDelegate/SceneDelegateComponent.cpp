ad45503d5c57e0ad9133016ff33d9e464819d67a
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-11-10 05:46:19 +0000

    -Inheriting from new Delegate class.

--- trunk/Helios/Qt/Plugins/SceneDelegate/SceneDelegateComponent.cpp
+++ trunk/Helios/Qt/Plugins/SceneDelegate/SceneDelegateComponent.cpp
@@ -15,18 +15,6 @@
 
 #include "SceneDelegateComponent.h"
 
-#include "Usul/Documents/Document.h"
-#include "Usul/Interfaces/IBuildScene.h"
-#include "Usul/Interfaces/Qt/IWorkspace.h"
-
-#include "Usul/Shared/Preferences.h"
-#include "Usul/Registry/Constants.h"
-
-#include "Helios/Qt/Views/OSG/Viewer.h"
-#include "Helios/Qt/Views/OSG/Format.h"
-
-#include "QtGui/QWorkspace"
-
 USUL_IMPLEMENT_IUNKNOWN_MEMBERS ( SceneDelegateComponent, SceneDelegateComponent::BaseClass );
 
 
@@ -65,10 +53,8 @@ Usul::Interfaces::IUnknown *SceneDelegateComponent::queryInterface ( unsigned lo
   case Usul::Interfaces::IUnknown::IID:
   case Usul::Interfaces::IPlugin::IID:
     return static_cast < Usul::Interfaces::IPlugin*>(this);
-  case Usul::Interfaces::IGUIDelegate::IID:
-    return static_cast < Usul::Interfaces::IGUIDelegate*>(this);
   default:
-    return 0x0;
+    return BaseClass::queryInterface ( iid );
   }
 }
 
@@ -88,39 +74,4 @@ bool SceneDelegateComponent::doesHandle( const std::string& token ) const
            token ==  "Minerva Document" ||
            token == "Triangle Document" ||
            token == "Model Presentation Document" );
-}
-
-
-/////////////////////////////////////////////////////////////////////////////
-//
-//  Build the default GUI
-//
-/////////////////////////////////////////////////////////////////////////////
-
-void SceneDelegateComponent::createDefaultGUI ( Usul::Documents::Document *document, Usul::Interfaces::IUnknown* caller )
-{
-  typedef CadKit::Helios::Views::OSG::Viewer QtViewer;
-
-  // Non-ref'ing smart-pointers that throw if given null.
-  typedef Usul::Pointers::Configs::NoRefCountingNullThrows Policy;
-  typedef Usul::Pointers::SmartPointer < QtViewer, Policy > QtViewerPtr;
-
-  Usul::Interfaces::Qt::IWorkspace::QueryPtr workspace ( caller );
-
-  if ( workspace.valid() )
-  {
-    QWorkspace *parent ( workspace->workspace() );
-    QtViewerPtr viewer ( new QtViewer ( document, CadKit::Helios::Views::OSG::defaultFormat(), parent ) );
-    parent->addWindow ( viewer.get() );
-
-    // Build the scene.
-    Usul::Interfaces::IBuildScene::QueryPtr build ( document );
-    if ( build.valid () )
-    {
-      viewer->viewer()->scene ( build->buildScene ( document->options(), caller ) );
-    }
-
-    // Show the window.
-    viewer->show();
-  }
 }

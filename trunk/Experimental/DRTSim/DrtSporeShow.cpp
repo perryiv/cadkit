5548170b296e0b4f88f05da7340ad32775bf956d
jsconner <jsconner@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-12-04 18:52:09 +0000

    - Code for the Santanam Serv project.

---
+++ trunk/Experimental/DRTSim/DrtSporeShow.cpp
@@ -0,0 +1,82 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2007, Arizona State University
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//  Author(s): Jeff Conner
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include "DrtSporeShow.h"
+
+#include "Usul/Trace/Trace.h"
+#include "Usul/Interfaces/IDrtCommands.h"
+
+USUL_IMPLEMENT_COMMAND ( DrtSporeShow );
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Constructor.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+DrtSporeShow::DrtSporeShow ( Usul::Interfaces::IUnknown * caller ) :
+  BaseClass ( caller )
+{
+  USUL_TRACE_SCOPE;
+  this->text ( "Show Spores" );
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Destructor.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+DrtSporeShow::~DrtSporeShow ()
+{
+  USUL_TRACE_SCOPE;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Execute.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+void DrtSporeShow::_execute ()
+{
+  USUL_TRACE_SCOPE;
+  //this->_dummy();
+#if 1
+  Usul::Interfaces::IDrtCommands::QueryPtr nav ( this->caller() );
+  if ( nav.valid () )
+  {
+	if( true == nav->sporeShow() )
+		nav->sporeShow( false );
+	else
+		nav->sporeShow( true );
+  }
+#endif
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Update the radio button.  Return true to have radio checked.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+bool DrtSporeShow::updateCheck () const
+{
+  USUL_TRACE_SCOPE;
+
+  // Implement me to update radio state.
+  Usul::Interfaces::IDrtCommands::QueryPtr nav ( const_cast < Usul::Interfaces::IUnknown * > ( this->caller() ) );
+
+  return nav->sporeShow();
+}
+

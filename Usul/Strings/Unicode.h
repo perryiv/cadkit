2fc034d0fe30bcdaa8a66e657573b43e4670c088
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2007-12-19 18:25:21 +0000

    -Implenting conversion functions on unix platforms.

--- Usul/Strings/Unicode.h
+++ Usul/Strings/Unicode.h
@@ -29,34 +29,26 @@ namespace Unicode {
 
 inline void convert ( const std::string &s, std::wstring &w )
 {
-  #ifdef _MSC_VER
-
   size_t numConvertedChars ( 0 );
   std::vector<wchar_t> target ( s.size() + 1 );
+  #ifdef _MSC_VER
   ::mbstowcs_s ( &numConvertedChars, &target[0], target.size(), s.c_str(), _TRUNCATE );
-  w = &target[0];
-
   #else
-
-  TODO
-
+  numConvertedChars = ::mbstowcs ( &target[0], s.c_str(), target.size() );
   #endif
+  w = &target[0];
 }
 
 inline void convert ( const std::wstring &w, std::string &s )
 {
-  #ifdef _MSC_VER
-
   size_t numConvertedChars ( 0 );
   std::vector<char> target ( 2 * w.size() + 1 );
+  #ifdef _MSC_VER
   ::wcstombs_s ( &numConvertedChars, &target[0], target.size(), w.c_str(), _TRUNCATE );
-  s = &target[0];
-
   #else
-
-  TODO
-
+  numConvertedChars = ::wcstombs ( &target[0], w.c_str(), target.size() );
   #endif
+  s = &target[0];
 }
 
 inline void convert ( const char *s, std::wstring &w )

3f1630a2394984eb9dff2d6da9c746eb56991bc8
tfurlong <tfurlong@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2008-01-29 19:39:17 +0000

    Added rotation interfaces

--- CadKit/Database/Base/DbBaseSource.h
+++ CadKit/Database/Base/DbBaseSource.h
@@ -20,6 +20,7 @@
 
 #include "Interfaces/IDataSource.h"
 #include "Interfaces/IScale.h"
+#include "Interfaces/IRotate.h"
 
 #include "Standard/SlRefPtr.h"
 #include "Standard/SlMatrix44.h"
@@ -30,7 +31,9 @@ namespace CadKit
 class DB_BASE_API DbBaseSource : public DbBaseObject, 
                                  public IDataSource,
                                  public IScaleDouble,
-                                 public IScaleFloat
+                                 public IScaleFloat,
+                                 public IRotateDouble,
+                                 public IRotateFloat
 {
 public:
 
@@ -63,28 +66,54 @@ public:
 
   /////////////////////////////////////////////////////////////////////////////
   //
-  //  IScaleDouble interface.
+  //  IScaleFloat interface.
   //
   /////////////////////////////////////////////////////////////////////////////
 
   // Modify the current scale.
   virtual void            scale ( float x, float y, float z );
 
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  IRotateDouble interface.
+  //
+  /////////////////////////////////////////////////////////////////////////////
+
+  // Modify the current rotation.
+  virtual void            rotate ( double x, double y, double z, double deg );
+
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  IRotateFloat interface.
+  //
+  /////////////////////////////////////////////////////////////////////////////
+
+  // Modify the current rotation.
+  virtual void            rotate ( float x, float y, float z, float deg );
+
 protected:
 
   SlRefPtr<IUnknown> _target;
+
   SlMatrix44d _scale;
   mutable bool _needToScale;
+
+  SlMatrix44d _rotate;
+  mutable bool _needToRotate;
 
   DbBaseSource();
   virtual ~DbBaseSource();
 
   void                    _applyScaleOnce ( SlMatrix44d &scale ) const;
   void                    _applyScaleOnce ( SlMatrix44f &scale ) const;
-
   bool                    _doWeNeedToScale() const { return _needToScale; }
-
   void                    _setNeedToScale ( bool state ) { _needToScale = state; }
+
+  void                    _applyRotateOnce ( SlMatrix44d &rot ) const;
+  void                    _applyRotateOnce ( SlMatrix44f &rot ) const;
+  bool                    _doWeNeedToRotate() const { return _needToRotate; }
+  void                    _setNeedToRotate ( bool state ) { _needToRotate = state; }
+
 
   SL_DECLARE_REFERENCE_POINTER ( DbBaseSource );
   SL_DECLARE_CLASS ( DbBaseSource, 1033073996 );

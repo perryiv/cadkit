a6a72402078bf38e48f8927a6347d582f9acef72
jsconner <jsconner@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2008-02-19 01:24:52 +0000

    - Plugin to read Fieldview CFD files.

---
+++ trunk/Experimental/FieldViewReader/FieldViewReaderDelegate/CompileGuard.h
@@ -0,0 +1,24 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2008, Jeff Conner
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  This header file should not be included in client code.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _FIELD_VIEW_READER_DELEGATE_COMPILE_GUARD_H_
+#define _FIELD_VIEW_READER_DELEGATE_COMPILE_GUARD_H_
+
+#ifndef _COMPILING_FIELD_VIEW_READER_DELEGATE
+# error "This header file is only used for compiling the library."
+# error "It should not be included in client code."
+#endif
+
+#endif // _CFIELD_VIEW_READER_DELEGATE_COMPILE_GUARD_H_

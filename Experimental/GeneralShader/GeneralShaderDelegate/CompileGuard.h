42ee3423f9a69ecb615e9e8467ed35bca401725a
jsconner <jsconner@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2008-03-19 19:00:30 +0000

    - Class to apply an arbitrary shader to an arbitrary object given the correct format.
    Note: Originally developed for a school project but I thought it might be useful with some modification.

---
+++ Experimental/GeneralShader/GeneralShaderDelegate/CompileGuard.h
@@ -0,0 +1,24 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2008, Jeff Conner
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  This header file should not be included in client code.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef _GENERAL_SHADER_DELEGATE_COMPILE_GUARD_H_
+#define _GENERAL_SHADER_DELEGATE_COMPILE_GUARD_H_
+
+#ifndef _COMPILING_GENERAL_SHADER_DELEGATE
+# error "This header file is only used for compiling the library."
+# error "It should not be included in client code."
+#endif
+
+#endif // _CGENERAL_SHADER_DELEGATE_COMPILE_GUARD_H_

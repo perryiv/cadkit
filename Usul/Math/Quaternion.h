e4d307e329ce89c067b47011fefc7e6cbaa6f32b
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2008-05-12 04:15:05 +0000

    -Adding very basic quaternion class.

---
+++ Usul/Math/Quaternion.h
@@ -0,0 +1,176 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2008, Adam Kubach
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Quaternion class.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __USUL_MATH_QUATERNION_H__
+#define __USUL_MATH_QUATERNION_H__
+
+#include "Usul/Math/Functions.h"
+#include "Usul/Math/Matrix44.h"
+#include "Usul/Math/Vector3.h"
+#include "Usul/Math/Vector4.h"
+
+namespace Usul {
+namespace Math {
+
+
+template
+<
+  typename T,
+  typename Index_ = unsigned int,
+  typename ErrorChecker_ = Usul::Errors::AssertPolicy
+>
+class Quaternion
+{
+public:
+
+  typedef Quaternion<T,Index_,ErrorChecker_> ThisType;
+  typedef T value_type;
+  typedef Index_ size_type;
+  typedef ErrorChecker_ ErrorChecker;
+  typedef Usul::Math::Vector3<T,Index_,ErrorChecker_> Vector3;
+  typedef Usul::Math::Vector4<T,Index_,ErrorChecker_> Vector4;
+  typedef Usul::Math::Matrix44<T,Index_,ErrorChecker_> Matrix44;
+
+  /// Constructor.
+  Quaternion() : _v ( 0.0, 0.0, 0.0, 1.0 )
+  {
+  }
+
+  /// Constructor.
+  Quaternion( T x, T y, T z, T w ) : _v ( x, y, z, w )
+  {
+  }
+
+  /// Constructor.
+  Quaternion ( const Vector3& axis, T angle ) : _v()
+  {
+    // Should we normalize the axis, or assume it is?
+    _v[0] = axis[0];
+	  _v[1] = axis[1];
+	  _v[2] = axis[2];
+
+    const double scale ( Usul::Math::sin ( angle * 0.5f ) );
+
+    _v[0] *= scale;
+    _v[1] *= scale;
+    _v[2] *= scale;
+    _v[3] = Usul::Math::cos ( angle * 0.5f);
+  }
+
+  /// Constructor.
+  // From http://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion/index.htm
+  Quaternion ( const Matrix44& m ) : _v()
+  {
+    _v[3] = Usul::Math::sqrt ( 1 + m ( 0, 0 ) + m ( 1, 1 ) + m( 2, 2 ) ) / 2.0;
+    _v[0] = ( m ( 2, 1 ) - m ( 1, 2 ) ) / ( 4 * _v[3] );
+    _v[1] = ( m ( 0, 2 ) - m ( 2, 0 ) ) / ( 4 * _v[3] );
+    _v[2] = ( m ( 1, 0 ) - m ( 0, 1 ) ) / ( 4 * _v[3] );
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  Bracket operators.
+  //
+  /////////////////////////////////////////////////////////////////////////////
+
+  T &operator [] ( size_type i )
+  {
+    return _v[i];
+  }
+  const T &operator [] ( size_type i ) const
+  {
+    return _v[i];
+  }
+
+  /// Multiply by another quaternion.
+  ThisType operator* ( const ThisType& rhs ) const
+  {
+    // See equation 23 on http://mathworld.wolfram.com/Quaternion.html
+    return ThisType( rhs._v[3]*_v[0] + rhs._v[0]*_v[3] + rhs._v[1]*_v[2] - rhs._v[2]*_v[1],
+                     rhs._v[3]*_v[1] - rhs._v[0]*_v[2] + rhs._v[1]*_v[3] + rhs._v[2]*_v[0],
+                     rhs._v[3]*_v[2] + rhs._v[0]*_v[1] - rhs._v[1]*_v[0] + rhs._v[2]*_v[3],
+                     rhs._v[3]*_v[3] - rhs._v[0]*_v[0] - rhs._v[1]*_v[1] - rhs._v[2]*_v[2] );
+  }
+
+  /// Divide by a scalar.
+  ThisType operator/ ( value_type v ) const
+  {
+    const value_type denom ( 1.0 / v );
+    ThisType quat ( *this );
+    quat[0] *= denom;
+    quat[1] *= denom;
+    quat[2] *= denom;
+    quat[3] *= denom;
+    return quat;
+  }
+
+  /// Return the conjugate.
+  ThisType conjugate() const
+  {
+    return ThisType ( -_v[0], -_v[1], -_v[2], _v[3] );
+  }
+
+  /// Return the inverse.
+  ThisType inverse() const
+  {
+    return this->conjugate() / this->length2();
+  }
+
+  /// Return the length.
+  size_type length() const
+  {
+    return Usul::Math::sqrt ( _v[0] * _v[0] + _v[1] * _v[1] + _v[2] * _v[2] + _v[3] * _v[3] );
+  }
+
+  /// Return the length before applying square root.
+  size_type length2() const
+  {
+    return _v[0] * _v[0] + _v[1] * _v[1] + _v[2] * _v[2] + _v[3] * _v[3];
+  }
+
+  /// Get a Matrix.
+  void getMatrix ( Matrix44& R ) const
+  {
+    R[0] = 1.0f  - 2.0f * (_v[1] * _v[1] + _v[2] * _v[2]);
+    R[1] = 2.0f         * (_v[0] * _v[1] - _v[2] * _v[3]);
+    R[2] = 2.0f         * (_v[2] * _v[0] + _v[1] * _v[3]);
+    R[3] = 0.0f;
+
+    R[4] = 2.0f         * (_v[0] * _v[1] + _v[2] * _v[3]);
+    R[5] = 1.0f  - 2.0f * (_v[2] * _v[2] + _v[0] * _v[0]);
+    R[6] = 2.0f         * (_v[1] * _v[2] - _v[0] * _v[3]);
+    R[7] = 0.0f;
+
+    R[8] = 2.0f         * (_v[2] * _v[0] - _v[1] * _v[3]);
+    R[9] = 2.0f         * (_v[1] * _v[2] + _v[0] * _v[3]);
+    R[10] = 1.0f - 2.0f * (_v[1] * _v[1] + _v[0] * _v[0]);
+    R[11] = 0.0f;
+
+    R[12] = 0.0f;
+    R[13] = 0.0f;
+    R[14] = 0.0f;
+    R[15] = 1.0f;
+  }
+
+private:
+
+  Vector4 _v;
+};
+
+
+}
+}
+
+#endif // __USUL_MATH_QUATERNION_H__

59256467122f9da57a9776ff20854ec9b67d0ac7
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2008-06-02 05:15:01 +0000

    -Adding transparent and background color. -Adding choosing different image formats.

--- Minerva/Core/Layers/RasterLayerArcIMS.cpp
+++ Minerva/Core/Layers/RasterLayerArcIMS.cpp
@@ -26,11 +26,15 @@
 #include "XmlTree/Document.h"
 #include "XmlTree/Writer.h"
 
+#include "Serialize/XML/TypeWrapper.h"
+
 #include "Usul/Convert/Convert.h"
+#include "Usul/Convert/Vector3.h"
 #include "Usul/Factory/RegisterCreator.h"
 #include "Usul/File/Temp.h"
 #include "Usul/Network/Curl.h"
 #include "Usul/Scope/RemoveFile.h"
+#include "Usul/Strings/Format.h"
 #include "Usul/Threads/Safe.h"
 #include "Usul/Trace/Trace.h"
 
@@ -39,7 +43,7 @@
 using namespace Minerva::Core::Layers;
 
 USUL_FACTORY_REGISTER_CREATOR ( RasterLayerArcIMS );
-
+SERIALIZE_XML_DECLARE_TYPE_WRAPPER ( Usul::Math::Vec3uc );
 
 ///////////////////////////////////////////////////////////////////////////////
 //
@@ -48,9 +52,13 @@ USUL_FACTORY_REGISTER_CREATOR ( RasterLayerArcIMS );
 ///////////////////////////////////////////////////////////////////////////////
 
 RasterLayerArcIMS::RasterLayerArcIMS() : 
-  BaseClass()
+  BaseClass(),
+  _type ( "jpg" ),
+  _background ( 255, 255, 255 ),
+  _transparent ( 255, 255, 255 )
 {
   USUL_TRACE_SCOPE;
+  this->_registerMembers();
 }
 
 
@@ -61,8 +69,13 @@ RasterLayerArcIMS::RasterLayerArcIMS() :
 ///////////////////////////////////////////////////////////////////////////////
 
 RasterLayerArcIMS::RasterLayerArcIMS ( const RasterLayerArcIMS& rhs ) :
-  BaseClass ( rhs )
+  BaseClass ( rhs ),
+  _type ( rhs._type ),
+  _background ( 255, 255, 255 ),
+  _transparent ( 255, 255, 255 )
 {
+  USUL_TRACE_SCOPE;
+  this->_registerMembers();
 }
 
 
@@ -86,8 +99,24 @@ RasterLayerArcIMS::~RasterLayerArcIMS()
 
 Usul::Interfaces::IUnknown* RasterLayerArcIMS::clone() const
 {
+  USUL_TRACE_SCOPE;
   Usul::Interfaces::IUnknown::QueryPtr clone ( new RasterLayerArcIMS ( *this ) );
   return clone.release();
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Register members for serialization.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+void RasterLayerArcIMS::_registerMembers()
+{
+  USUL_TRACE_SCOPE;
+  this->_addMember ( "type", _type );
+  this->_addMember ( "background", _background );
+  this->_addMember ( "transparent", _transparent );
 }
 
 
@@ -168,16 +197,21 @@ std::string RasterLayerArcIMS::_createRequestXml ( const Extents& extents, unsig
   arcXml->attributes().insert ( Attributes::value_type ( "version", "1.1" ) );
 
   // REQUEST node.
-  XmlTree::Node::RefPtr request ( arcXml->child ( "REQUEST" ) );
+  Node::RefPtr request ( arcXml->child ( "REQUEST" ) );
 
   // GET_IMAGE node.
-  XmlTree::Node::RefPtr getImage ( request->child ( "GET_IMAGE" ) );
+  Node::RefPtr getImage ( request->child ( "GET_IMAGE" ) );
 
   // PROPERTIES node.
-  XmlTree::Node::RefPtr properties ( getImage->child ( "PROPERTIES" ) );
+  Node::RefPtr properties ( getImage->child ( "PROPERTIES" ) );
 
-  // ENVELOPE node.
-  XmlTree::Node::RefPtr envelope ( properties->child ( "ENVELOPE" ) );
+  // Property nodes.
+  Node::RefPtr envelope ( properties->child ( "ENVELOPE" ) );
+  Node::RefPtr imageSize ( properties->child ( "IMAGESIZE" ) );
+  Node::RefPtr featureCoordSys ( properties->child ( "FEATURECOORDSYS" ) );
+  Node::RefPtr filterCoordSys ( properties->child ( "FILTERCOORDSYS" ) );
+  Node::RefPtr output ( properties->child ( "OUTPUT" ) );
+  Node::RefPtr background ( properties->child ( "OUTPUT" ) );
 
   // Add envelope.  Use Usul convert for full precision.
   envelope->attributes().insert ( Attributes::value_type ( "minx", Usul::Convert::Type<Extents::ValueType,std::string>::convert ( extents.minimum()[0] ) ) );
@@ -185,20 +219,23 @@ std::string RasterLayerArcIMS::_createRequestXml ( const Extents& extents, unsig
   envelope->attributes().insert ( Attributes::value_type ( "maxx", Usul::Convert::Type<Extents::ValueType,std::string>::convert ( extents.maximum()[0] ) ) );
   envelope->attributes().insert ( Attributes::value_type ( "maxy", Usul::Convert::Type<Extents::ValueType,std::string>::convert ( extents.maximum()[1] ) ) );
 
-  // IMAGE_SIZE node.
-  Node::RefPtr imageSize ( properties->child ( "IMAGESIZE" ) );
-
   // Add width and height.
   imageSize->attributes().insert ( Attributes::value_type ( "width", Usul::Convert::Type<unsigned int,std::string>::convert ( width  ) ) );
   imageSize->attributes().insert ( Attributes::value_type ( "height", Usul::Convert::Type<unsigned int,std::string>::convert ( height ) ) );
 
-  // FEATURECOORDSYS node.
-  Node::RefPtr featureCoordSys ( properties->child ( "FEATURECOORDSYS" ) );
+  // Retrieve answer in WGS 84.
   featureCoordSys->attributes().insert ( Attributes::value_type ( "id", Usul::Convert::Type<unsigned int,std::string>::convert ( 4326 ) ) );
-
-  // FILTERCOORDSYS node.
-  Node::RefPtr filterCoordSys ( properties->child ( "FILTERCOORDSYS" ) );
   filterCoordSys->attributes().insert ( Attributes::value_type ( "id", Usul::Convert::Type<unsigned int,std::string>::convert ( 4326 ) ) );
+
+  // Add the format type.
+  output->attributes().insert ( Attributes::value_type ( "type", Usul::Threads::Safe::get ( this->mutex(), _type ) ) );
+
+  Usul::Math::Vec3d bgColor  ( Usul::Threads::Safe::get ( this->mutex(), _background  ) );
+  Usul::Math::Vec3d transparent ( Usul::Threads::Safe::get ( this->mutex(), _transparent ) );
+
+  // Add background color.
+  background->attributes().insert ( Attributes::value_type ( "color",      Usul::Strings::format ( bgColor[0], ",", bgColor[1], ",", bgColor[2] ) ) );
+  background->attributes().insert ( Attributes::value_type ( "transcolor", Usul::Strings::format ( transparent[0], ",", transparent[1], ",", transparent[2] ) ) );
 
   // Write the file to an ostringstream.
   std::ostringstream os;
@@ -216,5 +253,7 @@ std::string RasterLayerArcIMS::_createRequestXml ( const Extents& extents, unsig
 
 std::string RasterLayerArcIMS::_cacheFileExtension() const
 {
-  return "jpg";
+  USUL_TRACE_SCOPE;
+  Guard guard ( this->mutex() );
+  return _type;
 }

258946a462ad4a8cfb4d96f8e28fa654508f0418
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2008-07-13 05:47:17 +0000

    -This shouldn't have been committed.

--- trunk/Helios/Qt/Core/Program.cpp
+++ trunk/Helios/Qt/Core/Program.cpp
@@ -280,15 +280,6 @@ void Program::run ( int argc, char **argv,
 
     // Declare application.
     CadKit::Helios::Core::Application app ( argc, argv );
-    
-#ifdef __APPLE__
-    QDir dir(QApplication::applicationDirPath());
-    dir.cdUp();
-    dir.cd("PlugIns");
-    const std::string path ( dir.absolutePath().toStdString() );
-    std::cout << path << std::endl;
-    QApplication::addLibraryPath ( path.c_str() );
-#endif
 
     // Call quit when the last window closes. (Not sure if this is needed...)
     app.connect ( &app, SIGNAL ( lastWindowClosed() ), &app, SLOT ( quit() ) );

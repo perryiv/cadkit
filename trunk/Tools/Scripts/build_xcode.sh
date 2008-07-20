f038548a8eb6f7f4bc9ae0f4b9012625964607e7
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2008-07-20 03:15:30 +0000

    -- Adding another build script -- Fixing mistage in previous build scripts

---
+++ trunk/Tools/Scripts/build_xcode.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd $HOME/src/cadkit/Tools/Scripts
+./build_xcode_release.sh
+./build_xcode_debug.sh
--- trunk/Tools/Scripts/build_xcode_debug.sh
+++ trunk/Tools/Scripts/build_xcode_debug.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd $HOME/src/cadkit/Tools/Scripts
-./build_xcode_release.sh
-./build_xcode_debug.sh
+cd $HOME/src/cadkit/Xcode
+xcodebuild -project Helios_xcode3.xcodeproj -configuration Debug -target HeliosQtApp OBJROOT=$HOME/src/bin SYMROOT=$HOME/src/bin
+xcodebuild -project Helios_xcode3.xcodeproj -configuration Debug -target MinervaQt   OBJROOT=$HOME/src/bin SYMROOT=$HOME/src/bin

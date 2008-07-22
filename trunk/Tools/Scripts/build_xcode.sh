99ef59f9ec0d15036b6f1a9896266ce3c1bc47b0
pep4 <pep4@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2008-07-22 22:46:44 +0000

    Modified the command-line build scripts for Xcode.

--- trunk/Tools/Scripts/build_xcode.sh
+++ trunk/Tools/Scripts/build_xcode.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 cd $HOME/src/cadkit/Tools/Scripts
-./build_xcode_release.sh
-./build_xcode_debug.sh
+./build_xcode_helios_release.sh
+./build_xcode_minerva_release.sh
+./build_xcode_helios_debug.sh
+./build_xcode_minerva_debug.sh

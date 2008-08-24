3d3bcd163bf2631b24d5008798ed94884d1dc11a
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2008-08-24 08:26:06 +0000

    -Adding simple filtering of Items.

--- Minerva/Plugins/GeoRSSQt/AddGeoRSSLayerWidget.cpp
+++ Minerva/Plugins/GeoRSSQt/AddGeoRSSLayerWidget.cpp
@@ -54,10 +54,25 @@ void AddGeoRSSLayerWidget::apply ( Usul::Interfaces::IUnknown* parent, Usul::Int
     return;
   
   const std::string url ( _urlEdit->text().toStdString() );
+	const double refreshRate ( _refreshRate->value() );
+	const bool enableFiltering ( Qt::Checked == _enableFiltering->checkState() );
+	const std::string element ( _elementFilter->text().toStdString() );
+	const std::string value ( _valueFilter->text().toStdString() );
+
   if ( false == url.empty() )
   {
     Minerva::Layers::GeoRSS::GeoRSSLayer::RefPtr layer ( new Minerva::Layers::GeoRSS::GeoRSSLayer );
     layer->url ( url );
+		layer->refreshRate ( refreshRate );
+		layer->filteringEnabled ( enableFiltering );
+
+		if ( false == element.empty() && false == value.empty() )
+			layer->filter ( Minerva::Layers::GeoRSS::GeoRSSLayer::Filter ( element, value ) );
+
+		// Start the download.
+		layer->downloadFeed();
+
+		// Add the layer.
     al->addLayer ( Usul::Interfaces::IUnknown::QueryPtr ( layer ) );
   }
 }

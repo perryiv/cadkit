7cb207ad9609998ab1ffef4ddc29ab71f4ed645d
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2008-10-01 22:58:33 +0000

    -Changing function name from paste to addToClipboard.

--- trunk/Usul/System/ClipBoard.cpp
+++ trunk/Usul/System/ClipBoard.cpp
@@ -47,34 +47,34 @@ bool ClipBoard::isClipBoardSupported()
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-void ClipBoard::paste ( const std::string& text )
+void ClipBoard::addToClipboard ( const std::string& text )
 {
 #ifdef __APPLE__
 
   // Get the reference to the global clipboard;
   PasteboardRef gClipboard;
-  if ( noErr != PasteboardCreate( kPasteboardClipboard, &gClipboard ) )
+  if ( noErr != ::PasteboardCreate ( kPasteboardClipboard, &gClipboard ) )
   {
     throw std::runtime_error ( "Error 2724349028: Could not create handle to clipboard." );
   }
   
   // We need to clear the pasteboard of it's current contents so that this application can
 	// own it and add it's own gClipboard.
-	if ( noErr != PasteboardClear( gClipboard ) )
+	if ( noErr != ::PasteboardClear ( gClipboard ) )
   {
     throw std::runtime_error ( "Error 1404747496: Could not clear clipboard." );
   }
   
   // Allocate data for the given text.
-	CFDataRef data ( CFDataCreate ( kCFAllocatorSystemDefault, (UInt8*) text.c_str(), text.size() ) );
+	CFDataRef data ( ::CFDataCreate ( kCFAllocatorSystemDefault, (UInt8*) text.c_str(), text.size() ) );
   if ( 0x0 == data )
   {
     throw std::runtime_error ( "Error 1989938963: Could not create data to paste to the clipboard." );
   }
 
 	// Paste to the clipboard.
-	if ( noErr != PasteboardPutItemFlavor( gClipboard, (PasteboardItemID)1,
-                                         CFSTR("com.apple.traditional-mac-plain-text"), data, 0 ) )
+	if ( noErr != ::PasteboardPutItemFlavor ( gClipboard, (PasteboardItemID)1,
+                                            CFSTR("com.apple.traditional-mac-plain-text"), data, 0 ) )
   {
     throw std::runtime_error ( "Error 1104512946: Could not paste to this clipboard." );
   }

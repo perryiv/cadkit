ecdaf01641ff6dfdf52bfd226a4da53bc9f57104
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2008-10-02 04:00:40 +0000

    -Implementing addToClipboard for Windows.

--- trunk/Usul/System/ClipBoard.cpp
+++ trunk/Usul/System/ClipBoard.cpp
@@ -16,8 +16,16 @@
 
 #include "Usul/System/ClipBoard.h"
 
+#include "Usul/Scope/Caller.h"
+
 #ifdef __APPLE__
 #include <Carbon/Carbon.h>
+#endif
+
+#ifdef WIN32
+# define NOMINMAX
+# define WIN32_LEAN_AND_MEAN
+# include <windows.h>
 #endif
 
 #include <stdexcept>
@@ -35,6 +43,8 @@ bool ClipBoard::isClipBoardSupported()
 {
 #ifdef __APPLE__
   return true;
+#elif WIN32
+	return true;
 #else
   return false;
 #endif
@@ -80,8 +90,40 @@ void ClipBoard::addToClipboard ( const std::string& text )
   }
   
 #elif WIN32
-  
-  // TODO.
-  
+
+	// Windows clipboard reference: http://msdn.microsoft.com/en-us/library/ms648709(VS.85).aspx
+
+	// Get the top window.
+	HWND hwnd ( GetTopWindow ( GetDesktopWindow() ) );
+	if ( FALSE == ::OpenClipboard ( hwnd ) )
+		throw std::runtime_error ( "Error 3764112647: Could not open clipboard." );
+
+	// Make sure it's closed.
+	Usul::Scope::Caller::RefPtr close ( Usul::Scope::makeCaller ( ::CloseClipboard ) );
+
+	// Empty the clipboard.  This will give us ownership.
+	::EmptyClipboard();
+
+	// Allocate data.
+	HGLOBAL data ( ::GlobalAlloc ( GMEM_MOVEABLE, text.size() + 1 * sizeof ( std::string::value_type ) ) );
+  if ( 0x0 == data )
+		throw std::runtime_error ( "Error 3142978366: Could not allocate data. " );
+
+  // Lock the handle.
+	LPTSTR copy ( (LPTSTR) ::GlobalLock( data ) );
+
+	// Copy the text to the buffer.
+	::memcpy ( copy, text.c_str(), text.size() * sizeof( std::string::value_type ) );
+
+	// Add the NULL terminator.
+	copy[text.size()] = 0x0;
+
+	// Unlock the handle.
+	::GlobalUnlock ( data );
+
+  // Place the handle on the clipboard. 
+	if ( 0x0 == ::SetClipboardData ( CF_TEXT, data ) )
+		throw std::runtime_error ( "Error 1119617596: Could not copy data to the clipboard." );
+
 #endif
 }

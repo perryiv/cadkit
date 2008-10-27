80151d497c25950b25ecbc83a6b32c54804f4477
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2008-10-27 23:15:01 +0000

    -Fixing compile warnings when using Visual Studio 2008.

--- trunk/Usul/CommandLine/Arguments.cpp
+++ trunk/Usul/CommandLine/Arguments.cpp
@@ -17,7 +17,7 @@
 #include "Usul/File/Path.h"
 
 // Disable deprecated warning in Visual Studio 8 for strdup
-#if defined ( _MSC_VER ) && _MSC_VER == 1400
+#if defined ( _MSC_VER ) && _MSC_VER >= 1400
 #pragma warning ( disable : 4996 )
 #endif
 

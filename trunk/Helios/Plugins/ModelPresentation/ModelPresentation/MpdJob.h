ae405d04201f643ccc75c63ee236a17e616ed5c7
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2009-01-21 20:00:14 +0000

    -Removing occurances of IDataSync.  These were not being used. -Fixing indentation.

--- trunk/Helios/Plugins/ModelPresentation/ModelPresentation/MpdJob.h
+++ trunk/Helios/Plugins/ModelPresentation/ModelPresentation/MpdJob.h
@@ -17,7 +17,6 @@
 #include "Usul/Documents/Document.h"
 #include "Usul/Jobs/Job.h"
 #include "Usul/Jobs/Manager.h"
-#include "Usul/Interfaces/IDataSync.h"
 #include "Usul/File/Path.h"
 #include "Usul/File/Find.h"
 #include "Usul/File/Slash.h"
@@ -29,52 +28,52 @@
 namespace osg { class Node; }
 
 class MpdJob : public Usul::Jobs::Job
-  {
-    public:
-      typedef Usul::Jobs::Job                           BaseClass;
+{
+public:
+  typedef Usul::Jobs::Job                           BaseClass;
 
-      USUL_DECLARE_REF_POINTERS ( MpdJob );
-      typedef Usul::Documents::Document::RefPtr DocumentPtr;
-      typedef Usul::Interfaces::IDataSync IDataSync;
-      typedef Usul::Interfaces::IUnknown IUnknown;
-      typedef std::vector < std::string > Files;
-      typedef Usul::Documents::Manager DocManager;
-      typedef DocManager::DocumentInfo Info;
-      
-      typedef MpdDefinitions::MpdDynamicSets MpdDynamicSets;
+  USUL_DECLARE_REF_POINTERS ( MpdJob );
+  typedef Usul::Documents::Document::RefPtr DocumentPtr;
 
-      MpdJob ( Usul::Interfaces::IUnknown *caller,
-               const std::string &workingDir,
-               const std::string &searchDir, 
-               const std::string &prefix, 
-               const std::string &extension,
-               Files current );
+  typedef Usul::Interfaces::IUnknown IUnknown;
+  typedef std::vector < std::string > Files;
+  typedef Usul::Documents::Manager DocManager;
+  typedef DocManager::DocumentInfo Info;
+  
+  typedef MpdDefinitions::MpdDynamicSets MpdDynamicSets;
 
-      MpdDefinitions::Groups              getData();
-      MpdDefinitions::MpdDynamicSetHeader getHeader();
-      void                                setHeader( MpdDefinitions::MpdDynamicSetHeader h );
-      bool                                foundNewData();
-      void                                foundNewData( bool state );
+  MpdJob ( Usul::Interfaces::IUnknown *caller,
+           const std::string &workingDir,
+           const std::string &searchDir, 
+           const std::string &prefix, 
+           const std::string &extension,
+           Files current );
 
-    protected:
-      virtual ~MpdJob();
-      virtual void                        _started ();
-      void                                _findFiles();
-      void                                _loadNewDynamicFiles( std::string filename, IUnknown *caller, IUnknown *progress );
-      void                                _parseNewFiles( Files files, IUnknown *caller, IUnknown *progress );
-      osg::Node*                          _loadFile( const std::string& filename, IUnknown *caller, IUnknown *progress );
+  MpdDefinitions::Groups              getData();
+  MpdDefinitions::MpdDynamicSetHeader getHeader();
+  void                                setHeader( MpdDefinitions::MpdDynamicSetHeader h );
+  bool                                foundNewData();
+  void                                foundNewData( bool state );
 
-      void                                _openDocument ( const std::string &file, Usul::Documents::Document *document, Usul::Interfaces::IUnknown *caller, IUnknown *progress );
-        
-    private:
-      MpdDefinitions::Groups              _root;
-      std::string                         _workingDir;
-      std::string                         _searchDir;
-      std::string                         _prefix;
-      std::string                         _extension;
-      Files                               _currentFiles;
-      bool                                _foundNewData;
-  };
+protected:
+  virtual ~MpdJob();
+  virtual void                        _started ();
+  void                                _findFiles();
+  void                                _loadNewDynamicFiles( std::string filename, IUnknown *caller, IUnknown *progress );
+  void                                _parseNewFiles( Files files, IUnknown *caller, IUnknown *progress );
+  osg::Node*                          _loadFile( const std::string& filename, IUnknown *caller, IUnknown *progress );
+
+  void                                _openDocument ( const std::string &file, Usul::Documents::Document *document, Usul::Interfaces::IUnknown *caller, IUnknown *progress );
+    
+private:
+  MpdDefinitions::Groups              _root;
+  std::string                         _workingDir;
+  std::string                         _searchDir;
+  std::string                         _prefix;
+  std::string                         _extension;
+  Files                               _currentFiles;
+  bool                                _foundNewData;
+};
 
 
 #endif // __MODELPRESENTATION_JOB_LOADING_H__

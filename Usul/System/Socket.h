dcdb734a263a1255b791688df4101ee3207ceaf1
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2009-03-06 20:42:18 +0000

    -Adding cmake file to find Google unit test framework. -Adding CMakeLists for unit tests. -Fixing compile errors when using cmake generated Visual Studio 2008 files.

--- Usul/System/Socket.h
+++ Usul/System/Socket.h
@@ -19,7 +19,7 @@
 #include "Usul/Export/Export.h"
 
 // Disable deprecated warning in Visual Studio 8 for sprintf
-#if defined ( _MSC_VER ) && _MSC_VER == 1400
+#if defined ( _MSC_VER ) && _MSC_VER >= 1400
 #pragma warning ( disable : 4996 )
 #endif
 

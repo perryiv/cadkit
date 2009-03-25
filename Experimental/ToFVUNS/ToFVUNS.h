17c5282e2896b9214fe156271534226fc72dfdd7
jsconner <jsconner@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2009-03-25 23:37:19 +0000

    - Fixed the grid writing. - Results still not working correctly

--- Experimental/ToFVUNS/ToFVUNS.h
+++ Experimental/ToFVUNS/ToFVUNS.h
@@ -33,6 +33,9 @@ public:
   typedef std::vector< Grid1D > Grid2D;
   typedef std::vector< Grid2D > GridSpace;
 
+  typedef Usul::Types::Uint32 IndexValue;
+  typedef std::vector < std::vector < std::vector < IndexValue > > > IndexGrid;
+
   USUL_DECLARE_QUERY_POINTERS ( ToFVUNS );
   USUL_DECLARE_IUNKNOWN_MEMBERS;
 
@@ -48,8 +51,18 @@ protected:
   virtual void  updateProgressBar ( unsigned int value );
   virtual void  hideProgressBar(){}
 
+  void          _calculateYGrid();
+
+  void          _processArguments();
+  void          _processFiles();
+  void          _processFile ( const std::string &file, Grid2D &grid );
+  void          _procesGridFile( const std::string &file, Grid1D &grid );
+
   void          _writeResultsFile( const std::string &name );
-  void          _writeGrid( const std::string &name, std::ofstream* ofs );
+  void          _writeResults( const std::string &name, std::ofstream* ofs );
+  void          _writeGridFile( const std::string &name );
+  void          _writeElementNodes( std::ofstream* ofs );
+  void          _writeElementNodeHeader( std::ofstream* ofs );
 
   virtual ~ToFVUNS();
 
@@ -59,9 +72,7 @@ private:
 
   void          _openDocument ( const std::string &file, Usul::Documents::Document *document );
 
-  void          _processArguments();
-  void          _processFiles();
-  void          _processFile ( const std::string &file, Grid2D &grid );
+  
 
   void          _saveDocument ( const std::string &file, Usul::Documents::Document *document );
 
@@ -71,7 +82,10 @@ private:
   Files                       _files;
 
   Usul::Math::Vec3ui          _dimensions;
-  GridSpace                   _grid;
+  GridSpace                   _values;
   std::string                 _workingDirectory;
   std::string                 _baseFilename;
+  std::vector< Grid1D >       _grid;
+  IndexGrid                   _indexGrid;
+  
 };

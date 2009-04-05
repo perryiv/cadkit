f804082520f81b281afeb3132b7da9400bc35985
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2009-04-05 06:32:06 +0000

    -Removing XercesLife.

--- trunk/Minerva/Application/main.cpp
+++ trunk/Minerva/Application/main.cpp
@@ -16,8 +16,6 @@
 
 #include "Helios/Qt/Core/Program.h"
 
-#include "XmlTree/XercesLife.h"
-
 #include "Threads/OpenThreads/Thread.h"
 
 #include "Usul/Functions/SafeCall.h"
@@ -54,9 +52,6 @@ int main ( int argc, char **argv )
 
   try
   {
-    // We make heavy use of xerces, so initialize now.
-    XmlTree::XercesLife life;
-
     // Default trace behavior.
     Usul::Trace::Print::printing ( false );
 

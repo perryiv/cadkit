4c278ea113809fdc4dda05fd252d37afe49b0522
jsconner <jsconner@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2009-06-30 00:41:09 +0000

    - adding missing classes - adding stub for contaminant editing

---
+++ Experimental/VaporIntrusionGUI/VaporIntrusionDelegate/BuildingDialog.h
@@ -0,0 +1,58 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2009, Arizona State University
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//  Author: Jeff Conner
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Input dialog for files.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __VAPORINTRUSION_BUILDING_DIALOG_H__
+#define __VAPORINTRUSION_BUILDING_DIALOG_H__
+
+#include "VaporIntrusionGUI/Interfaces/IVaporIntrusionGUI.h"
+
+#include "Usul/Documents/Manager.h"
+#include "Usul/Math/Vector3.h"
+
+#include "ui_BuildingDialog.h"
+
+#include "QtGui/QDialog"
+
+
+class BuildingDialog : public QDialog,
+                             private Ui::BuildingDialog
+{
+  Q_OBJECT;
+public:
+  typedef QDialog BaseClass;
+
+  // Useful typedefs
+  typedef VaporIntrusionGUI::Interfaces::IVaporIntrusionGUI::Building Building;
+
+  BuildingDialog ( QWidget *parent = 0x0 );
+  virtual ~BuildingDialog();
+
+  Building building();
+  bool     useBuilding();
+
+
+protected:
+
+private:
+  
+
+private slots:
+
+};
+
+
+
+#endif // __VAPORINTRUSION_BUILDING_DIALOG_H__

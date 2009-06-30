11b1b0952320b5d1a3756fea3a27d8b92c67c679
jsconner <jsconner@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2009-06-30 00:41:09 +0000

    - adding missing classes - adding stub for contaminant editing

---
+++ trunk/Experimental/VaporIntrusionGUI/VaporIntrusionDelegate/ContaminantDialog.h
@@ -0,0 +1,62 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2009, Arizona State University
+//  All rights reserved.
+//  BSD License: http://www.opensource.org/licenses/bsd-license.html
+//  Author: Jeff Conner
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Input dialog for files.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __VAPORINTRUSION_CONTAMINANT_DIALOG_H__
+#define __VAPORINTRUSION_CONTAMINANT_DIALOG_H__
+
+#include "VaporIntrusionGUI/Interfaces/IVaporIntrusionGUI.h"
+
+#include "Usul/Documents/Manager.h"
+#include "Usul/Math/Vector3.h"
+
+#include "ui_ContaminantDialog.h"
+
+#include "QtGui/QDialog"
+
+
+class ContaminantDialog : public QDialog,
+                             private Ui::ContaminantDialog
+{
+  Q_OBJECT;
+public:
+  typedef QDialog BaseClass;
+  typedef VaporIntrusionGUI::Interfaces::IVaporIntrusionGUI IVPI;
+  typedef IVPI::Contaminant  Contaminant;
+  typedef IVPI::Contaminants Contaminants;
+
+  // Useful typedefs  
+
+  ContaminantDialog ( QWidget *parent = 0x0 );
+  virtual ~ContaminantDialog();
+
+  void                initialize();
+
+protected:
+
+  void                _initialize();
+
+private:
+  Contaminants        _contaminants;
+
+private slots:
+  void on_addButton_clicked();
+  void on_removeButton_clicked();
+
+};
+
+
+
+#endif // __VAPORINTRUSION_CONTAMINANT_DIALOG_H__

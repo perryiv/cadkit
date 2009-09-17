719bb6fa8139336ac0315b216dfcdb0b5a97658f
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2009-09-17 05:11:45 +0000

    -Checking an environment variable for the location of needed gdal data files.

--- Minerva/Config.h
+++ Minerva/Config.h
@@ -11,10 +11,14 @@
 #ifndef __MINERVA_CONFIG_H__
 #define __MINERVA_CONFIG_H__
 
+#include <string>
+
 // Should we compile for distributed communication?
 #define USE_DISTRIBUTED 0
 
 // Use new planetary navigator.
 #define USE_PLANETARY_NAVIGATOR 0
+
+const std::string MINERVA_DATA_DIR_VARIABLE ( "MINERVA_DATA_DIR" );
 
 #endif // __MINERVA_CONFIG_H__

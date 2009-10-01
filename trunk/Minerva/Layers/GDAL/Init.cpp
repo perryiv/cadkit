90421ff213a5f4cd7b113be3f4866a8030a3c522
wsuchewie <wsuchewie@73d323f7-f32a-0410-a0ec-c7cf9bc3a937>
2009-10-01 06:50:34 +0000

    -Setting GDAL_DATA variable with MINERVA_DATA_DIR, or to path relative to Minerva executable.

--- trunk/Minerva/Layers/GDAL/Init.cpp
+++ trunk/Minerva/Layers/GDAL/Init.cpp
@@ -10,20 +10,16 @@
 
 #include "Minerva/Config.h"
 
-#include "Usul/Adaptors/MemberFunction.h"
 #include "Usul/CommandLine/Arguments.h"
-#include "Usul/Strings/Format.h"
 #include "Usul/System/Environment.h"
-#include "Usul/Threads/Mutex.h"
-#include "Usul/Threads/Guard.h"
 
 #include "gdal.h"
 #include "ogr_api.h"
 #include "cpl_csv.h"
 
-#include <map>
+#include "boost/filesystem.hpp"
+
 #include <string>
-#include <vector>
 
 ///////////////////////////////////////////////////////////////////////////////
 //
@@ -33,16 +29,36 @@
 
 namespace
 {
-  const char * buildFilename ( const char* base );
-  
   struct Init
   {
-    typedef Usul::Threads::Mutex Mutex;
-    typedef Usul::Threads::Guard<Mutex> Guard;
-    
     ///  Constructor.
-    Init() : _filenames(), _mutex()
+    Init()
     {
+      boost::filesystem::path gdalDataDir;
+
+      if ( Usul::System::Environment::has ( MINERVA_DATA_DIR_VARIABLE ) )
+      {
+        gdalDataDir = Usul::System::Environment::get ( MINERVA_DATA_DIR_VARIABLE );
+      }
+
+      else
+      {
+#if __APPLE__
+        const std::string path ( "/../Frameworks/GDAL.framework/Resources/gdal/" );
+#elif _MSC_VER
+        const std::string path ( "/proj4/" );
+#else 
+        const std::string path ( "../share/minerva/gdal" );
+#endif
+
+        boost::filesystem::path programDir ( Usul::CommandLine::Arguments::instance().directory(), boost::filesystem::no_check );
+        gdalDataDir = programDir / path;
+      }
+      if ( false == Usul::System::Environment::has ( "GDAL_DATA" ) && boost::filesystem::is_directory ( gdalDataDir ) )
+      {
+        Usul::System::Environment::set ( "GDAL_DATA", gdalDataDir.native_directory_string() );
+      }
+
       /// If there are no drivers for gdal, assume that it hasn't been initialized yet.
       if ( 0 == ::GDALGetDriverCount() )
       {
@@ -50,12 +66,6 @@ namespace
       }
 
       ::OGRRegisterAll();
-      
-      // Set the callback to create a full path for given filename.
-      // Linux works fine, so don't do it there.
-      #ifndef __GNUC__
-      ::SetCSVFilenameHook ( buildFilename );
-      #endif
     }
     
     ///  Destructor.
@@ -65,68 +75,5 @@ namespace
       ::OGRCleanupAll();
     }
     
-    
-    /// Build the filename.
-    const char * buildFilenameImpl ( const char* base )
-    {
-      Guard guard ( _mutex );
-      
-      // See if we have this filename already.
-      Filenames::iterator iter ( _filenames.begin() );
-      if ( iter == _filenames.end() )
-      {
-        String filename;
-
-        if ( Usul::System::Environment::has ( MINERVA_DATA_DIR_VARIABLE ) )
-        {
-          const std::string dataDir ( Usul::System::Environment::get ( MINERVA_DATA_DIR_VARIABLE ) );
-          const std::string path ( dataDir + base );
-          std::copy ( path.begin(), path.end(), std::back_inserter ( filename ) );
-        }
-        else
-        {
-        // Build the filename.
-#if __APPLE__
-          const std::string path ( "/../Frameworks/GDAL.framework/Resources/gdal/" );
-#elif _MSC_VER
-          const std::string path ( "/proj4/" );
-#else 
-          const std::string path ( "/" );
-#endif
-          const std::string directory ( Usul::Strings::format ( Usul::CommandLine::Arguments::instance().directory(), path, base ) );
-          
-          std::copy ( directory.begin(), directory.end(), std::back_inserter ( filename ) );
-        }
-        
-        filename.push_back ( '\0' );
-
-        // Insert the result into the map.
-        typedef std::pair<Filenames::iterator, bool> Result;
-        Result result ( _filenames.insert ( std::make_pair ( base, filename ) ) );
-        
-        if ( false == result.second )
-          return 0x0;
-        
-        // Re-set the iterator to our result.
-        iter = result.first;
-      }
-      
-      return &( iter->second[0] );
-    }
-    
-  private:
-    typedef std::vector<char> String;
-    typedef std::map<std::string, String> Filenames;
-    
-    Filenames _filenames;
-    Usul::Threads::Mutex _mutex;
-    
   } _init;
-  
-  
-  // Redirect.
-  const char* buildFilename ( const char* base )
-  {
-    return _init.buildFilenameImpl ( base );
-  }
 }
